a69401704519fb1ba268946b1a581a5a8a0678e6
saens <songsh5775@gmail.com>
2022-02-18 21:47:07 +0900

    BJ 11053

--- Practice.cpp
+++ Practice.cpp
@@ -3,42 +3,71 @@
 
 using namespace std;
 
+
+int DP[1001];
+
+
+int MAX(int a, int b)
+{
+    if(a > b)
+    {
+        return a;
+    }
+    else
+    {
+        return b;
+    }
+}
+
+int lis(int startIndex, int N, int* arr)
+{
+    if(DP[startIndex] != -1)
+    {
+        return DP[startIndex];
+    }
+
+    int last_lis = 1;
+    for(int i=startIndex+1; i<N ; ++i)
+    {
+        if(arr[startIndex] < arr[i])
+        {
+            int now_lis = 1 + lis(i, N, arr);
+            last_lis = MAX(last_lis, now_lis);
+        }
+    }
+    DP[startIndex] = last_lis;
+    return last_lis;
+}
+
+
 int main()
 {
     ios::sync_with_stdio(false);
     cin.tie(NULL);
+
+    for(int i=0 ; i<1001 ; ++i)
+    {
+        DP[i] = -1;
+    }
     
     int N;
 
     cin >> N;
 
-    // int* arr;
+    int arr[N];
 
-    // for(int i=0 ; i<N ; ++i)
-    // {
-    //     cin >> arr[i];
-    // }
+    for(int i=0 ; i<N ; ++i)
+    {
+        cin >> arr[i];
+    }
 
-    // int longest = 1;
-    // int now_length, lastIndex;
-    // for(int i=0 ; i<N ; ++i)
-    // {
-    //     now_length = 1;
-    //     lastIndex = i;
-    //     for(int j=i+1 ; j<N ; ++j)
-    //     {
-    //         if(arr[j]>arr[lastIndex])
-    //         {
-    //             now_length++;
-    //         }
-    //         lastIndex = j;
-    //     }
-    //     if(now_length > longest)
-    //     {
-    //         longest = now_length;
-    //     }
-    // }
+    int longest = 0;
 
-    cout << N;
+    for(int i=0 ; i<N ; ++i)
+    {
+        longest = MAX(lis(i, N, arr), longest);
+    }
+
+    cout << longest;
     
 }
