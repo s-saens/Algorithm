2755ef8bcd67aeb2ce9e1c4860a3d2d011f854c6
saens <songsh5775@gmail.com>
2022-02-18 22:50:56 +0900

    BJ 11054 - 시간초과 풀이 : 어떤 수 x를 시작으로 하는 바이오틱 수열을 구하는 함수를 정의했음. DP 사용 시 DP 값이 틀리게 저장됨.

--- Practice.cpp
+++ Practice.cpp
@@ -19,7 +19,7 @@ int MAX(int a, int b)
     }
 }
 
-int lis(int startIndex, int N, int* arr)
+int longestBiotic(int startIndex, int last_changeCnt, int last_increment, int N, int* arr)
 {
     if(DP[startIndex] != -1)
     {
@@ -27,15 +27,49 @@ int lis(int startIndex, int N, int* arr)
     }
 
     int last_lis = 1;
+
     for(int i=startIndex+1; i<N ; ++i)
     {
-        if(arr[startIndex] < arr[i])
+        // now_ : 현재 i에 대한 값
+        // last_ : 현재 함수 전체에 대한 값
+        int now_changeCnt = last_changeCnt;
+        int now_increment = last_increment;
+
+        // 감소한 경우
+        if(arr[startIndex] > arr[i])
         {
-            int now_lis = 1 + lis(i, N, arr);
-            last_lis = MAX(last_lis, now_lis);
+            now_increment = -1;
+            // 이전에 증가하고 있었던 경우
+            if(last_increment > 0)
+            {
+                now_changeCnt++;
+            }
         }
+        // 증가한 경우
+        else if(arr[startIndex] < arr[i])
+        {
+            now_increment = 1;
+            // 이전에 감소하고 있었던 경우 : ch에 관계 없이 바이오틱 X
+            if(last_increment < 0)
+            {
+                continue;
+            }
+        }
+        // 그대로인 경우 : ch에 관계 없이 바이오틱 X
+        else
+        {
+            continue;
+        }
+
+        if(now_changeCnt > 1)
+        {
+            continue;
+        }
+
+        last_lis = MAX(last_lis, 1 + longestBiotic(i, now_changeCnt, now_increment, N, arr));
     }
-    DP[startIndex] = last_lis;
+
+    DP[startIndex] = MAX(last_lis, startIndex);
     return last_lis;
 }
 
@@ -65,7 +99,7 @@ int main()
 
     for(int i=0 ; i<N ; ++i)
     {
-        longest = MAX(lis(i, N, arr), longest);
+        longest = MAX(longestBiotic(i, 0, 0, N, arr), longest);
     }
 
     cout << longest;
