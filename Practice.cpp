7967a1921a77bfc3296586ec578182d5682b38f9
Saens <songsh5775@gmail.com>
2022-02-18 12:41:13 +0900

    11054 test

--- Practice.cpp
+++ Practice.cpp
@@ -7,63 +7,38 @@ int main()
 {
     ios::sync_with_stdio(false);
     cin.tie(NULL);
-
-    int min = 99999;
-
-    string a, b;
-
-    cin >> a >> b;
-
-    string *longer;
-    string *shorter;
-
-    int aLen = a.length();
-    int bLen = b.length();
-
-    if (aLen > bLen)
-    {
-        longer = &a;
-        shorter = &b;
-    }
-    else
-    {
-        longer = &b;
-        shorter = &a;
-    }
-
-    int gap = (*longer).length() - (*shorter).length();
-
-    string sum("");
-
-    int buffer = 0;
-    for (int i = (*shorter).length() - 1; i >= 0; --i)
-    {
-        int s = ((*shorter)[i] - 48) + ((*longer)[i + gap] - 48) + buffer;
-
-        if(s>=10)
-        {
-            buffer = s / 10;
-        }
-        else
-        {
-            buffer = 0;
-        }
-
-        sum.insert(sum.begin(), (char)(s % 10 + 48));
-    }
-
-    for (int i = gap - 1; i >= 0; --i)
-    {
-        int s = ((*longer)[i] - 48) + buffer;
-        buffer = s/10;
-        sum.insert(sum.begin(), (char)(s % 10 + 48));
-    }
-
-    if (buffer > 0)
-    {
-        sum.insert(sum.begin(), (char)(buffer + 48));
-    }
     
-    cout << sum;
+    int N;
+
+    cin >> N;
+
+    // int* arr;
+
+    // for(int i=0 ; i<N ; ++i)
+    // {
+    //     cin >> arr[i];
+    // }
+
+    // int longest = 1;
+    // int now_length, lastIndex;
+    // for(int i=0 ; i<N ; ++i)
+    // {
+    //     now_length = 1;
+    //     lastIndex = i;
+    //     for(int j=i+1 ; j<N ; ++j)
+    //     {
+    //         if(arr[j]>arr[lastIndex])
+    //         {
+    //             now_length++;
+    //         }
+    //         lastIndex = j;
+    //     }
+    //     if(now_length > longest)
+    //     {
+    //         longest = now_length;
+    //     }
+    // }
+
+    cout << N;
     
 }
