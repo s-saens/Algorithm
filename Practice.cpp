64a39a4472e70bf012724e48d7e223a0ca8ab3a4
saens <songsh5775@gmail.com>
2022-02-19 18:52:45 +0900

    BJ 1018

--- Practice.cpp
+++ Practice.cpp
@@ -3,71 +3,12 @@
 
 using namespace std;
 
-int DPLeft[1001];
-int DPRight[1001];
-
-int MAX(int a, int b)
+int MIN(int a, int b)
 {
-    if (a > b)
-    {
+    if (a < b)
         return a;
-    }
     else
-    {
         return b;
-    }
-}
-
-int longestLeft(int startIndex, int N, int *arr) // 사실 여기서 N은 의미 없지만, 코드 헷갈리니까 그냥 넣기로 함.
-{
-    if (DPLeft[startIndex] != -1)
-    {
-        return DPLeft[startIndex];
-    }
-
-    int lastLongest = 1;
-    for (int i = startIndex - 1; i >= 0; --i)
-    {
-        // 왼쪽으로 감소해야 함
-        if (arr[startIndex] > arr[i])
-        {
-            lastLongest = MAX(lastLongest, 1 + longestLeft(i, N, arr));
-        }
-    }
-
-    DPLeft[startIndex] = lastLongest;
-    return lastLongest;
-}
-
-int longestRight(int startIndex, int N, int *arr)
-{
-    if (DPRight[startIndex] != -1)
-    {
-        return DPRight[startIndex];
-    }
-
-
-    int lastLongest = 1;
-    for (int i = startIndex + 1; i < N; ++i)
-    {
-        // 왼쪽으로 감소해야 함
-        if (arr[startIndex] > arr[i])
-        {
-            lastLongest = MAX(lastLongest, 1 + longestRight(i, N, arr));
-        }
-    }
-
-    DPRight[startIndex] = lastLongest;
-    return lastLongest;
-}
-
-int longestBiotic(int midIndex, int N, int *arr)
-{
-    // 왼쪽 오른쪽 셀 때, mid도 세게 되는데, 이 때 같은 값이 두 번 더해지기 때문에, 양쪽 longest 더하고 하나 빼줌.
-    int l = longestLeft(midIndex, N, arr);
-    int r = longestRight(midIndex, N, arr);
-    int b = l + r - 1;
-    return b;
 }
 
 int main()
@@ -75,30 +16,58 @@ int main()
     ios::sync_with_stdio(false);
     cin.tie(NULL);
 
-    for (int i = 0; i < 1001; ++i)
-    {
-        DPLeft[i] = -1;
-        DPRight[i] = -1;
-    }
+    int N, M; // N:세로, M:가로
 
-    int N;
+    cin >> N >> M;
 
-    cin >> N;
-
-    int arr[N];
-
+    string pieces[N];
     for (int i = 0; i < N; ++i)
     {
-        cin >> arr[i];
+        cin >> pieces[i];
     }
 
-    int longest = 0;
+    int minChangeCount = 5000;
 
-    for (int i = 0; i < N; ++i)
+    for (int i = 0; i <= N - 8; ++i)
     {
-        int a = longestBiotic(i, N, arr);
-        longest = MAX(a, longest);
+        for (int j = 0; j <= M - 8; ++j)
+        {
+            int changeCount_B = 0; // 맨 왼쪽 위가 Black이어야 한다고 가정할 때, 바뀌는 칸의 수
+            int changeCount_W = 0; // 맨 왼쪽 위가 White~
+
+            for (int k = 0; k < 8; ++k)
+            {
+                for (int l = 0; l < 8; ++l)
+                {
+                    // 맨 왼쪽 위(k+l = 0)가 B여야 할 때
+                    // k+l 더한 값이 짝수인 (0포함) 곳에서 'W'인 경우,
+                    // 홀수인 곳에서 'B'인 경우 바꿈
+                    if (pieces[i + k][j + l] == 'W' && (k + l) % 2 == 0)
+                    {
+                        changeCount_B++;
+                    }
+                    if (pieces[i + k][j + l] == 'B' && (k + l) % 2 == 1)
+                    {
+                        changeCount_B++;
+                    }
+
+                    // 맨 왼쪽 위(k+l = 0)가 W여야 할 때
+                    // k+l 더한 값이 짝수인 (0포함) 곳에서 'B'인 경우,
+                    // 홀수인 곳에서 'W'인 경우 바꿈
+                    if (pieces[i + k][j + l] == 'B' && (k + l) % 2 == 0)
+                    {
+                        changeCount_W++;
+                    }
+                    if (pieces[i + k][j + l] == 'W' && (k + l) % 2 == 1)
+                    {
+                        changeCount_W++;
+                    }
+                }
+            }
+
+            minChangeCount = MIN(minChangeCount, MIN(changeCount_B, changeCount_W));
+        }
     }
 
-    cout << longest;
+    cout << minChangeCount;
 }
