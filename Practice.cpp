ac356f2b260e1e86bc1663c986c2b2661a1d542c
saens <songsh5775@gmail.com>
2022-02-21 15:55:46 +0900

    BJ 11650 - 이중 선택 정렬 - 시간초과

--- Practice.cpp
+++ Practice.cpp
@@ -17,36 +17,23 @@ struct Vector2
     int y;
 };
 
-Vector2* bigger(Vector2* v1, Vector2* v2)
+Vector2 *Bigger(Vector2 *v1, Vector2 *v2)
 {
-    if((*v1).x > (*v2).x)
-    {
+    if ((*v1).x > (*v2).x)
         return v1;
-    }
-    else if((*v1).x < (*v2).x)
-    {
+    else if ((*v1).x < (*v2).x)
         return v2;
-    }
+    else if ((*v1).y > (*v2).y)
+        return v1;
     else
-    {
-        if((*v1).y > (*v2).y)
-        {
-            return v1;
-        }
-        else
-        {
-            return v2;
-        }
-    }
-
+        return v2;
 }
 
-void swapVector2(Vector2* arr, int i1, int i2)
+void swapVector2(Vector2 *arr, int i1, int i2)
 {
     Vector2 temp = arr[i1];
     arr[i1] = arr[i2];
     arr[i2] = temp;
-
 }
 
 int main()
@@ -60,27 +47,35 @@ int main()
 
     Vector2 pos[N];
 
-    for(int i=0 ; i<N ; ++i)
+    for (int i = 0; i < N; ++i)
     {
         int x, y;
         cin >> x >> y;
         pos[i].x = x;
         pos[i].y = y;
     }
-    
-    for(int i=0 ; i<N-1 ; ++i)
+
+    // sort : 이중 선택 정렬
+    int cnt = 0; // 최대/최소가 정해진 횟수
+    while(cnt <= N/2)
     {
-        for(int j=i ; j<N-1 ; ++j)
+        for(int i=cnt ; i<N-cnt ; ++i)
         {
-            if(bigger(&pos[j], &pos[j+1]) == &pos[j])
+            // 맨 왼쪽거가 더 크다면 SWAP
+            if(Bigger(&pos[cnt], &pos[i]) == &pos[cnt])
             {
-                swapVector2(pos, j, j+1);
+                swapVector2(pos, cnt, i);
+            }
+            if(Bigger(&pos[N-cnt-1], &pos[i]) == &pos[i])
+            {
+                swapVector2(pos, N-cnt-1, i);
             }
         }
+        cnt++;
     }
 
     cout << "\n";
-    for(int i=0 ; i<N ; ++i)
+    for (int i = 0; i < N; ++i)
     {
         cout << pos[i].x << " " << pos[i].y << "\n";
     }
