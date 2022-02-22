37c55e7b7083fcc713046b984de762ec2a8b0454
saens <songsh5775@gmail.com>
2022-02-22 22:43:16 +0900

    BJ 10951

--- Practice.cpp
+++ Practice.cpp
@@ -7,33 +7,9 @@ int main()
     ios::sync_with_stdio(false);
     cin.tie(NULL);
 
-    int number, tmp = 0;
-
-    int lastNumber = 0;
-    for(int i=0 ; i<8 ; ++i)
+    int A, B;
+    while(!(cin >> A >> B).eof())
     {
-        cin >> number;
-        if(lastNumber < number)
-        {
-            tmp++;
-        }
-        else
-        {
-            tmp--;
-        }
-        lastNumber = number;
-    }
-
-    if(tmp >= 8)
-    {
-        cout << "ascending";
-    }
-    else if(tmp <= -5)
-    {
-        cout << "descending";
-    }
-    else
-    {
-        cout << "mixed";
+        cout << A + B << "\n";
     }
 }
