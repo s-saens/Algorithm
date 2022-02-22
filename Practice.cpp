6f080174dfaac89c5c73518d7c42013c8f0a7d38
saens <songsh5775@gmail.com>
2022-02-22 22:29:58 +0900

    BJ 2920

--- Practice.cpp
+++ Practice.cpp
@@ -1,68 +1,39 @@
 #include <iostream>
-#include <vector>
-#include <algorithm>
 
 using namespace std;
-
-class Vector2
-{
-    public:
-    int x=0;
-    int y=0;
-
-    Vector2 (int _x, int _y)
-    {
-        x = _x;
-        y = _y;
-    }
-};
-
-bool compare(Vector2& v1, Vector2& v2)
-{
-    if((v1).y > (v2).y)
-    {
-        return false;
-    }
-    else if((v1).y < (v2).y)
-    {
-        return true;
-    }
-    // x가 같을 때
-    else if((v1).x > (v2).x)
-    {
-        return false;
-    }
-    else
-    {
-        return true;
-    }
-
-}
 
 int main()
 {
     ios::sync_with_stdio(false);
     cin.tie(NULL);
 
-    int N; // N:세로, M:가로
+    int number, tmp = 0;
 
-    cin >> N;
-
-    vector<Vector2> pos;
-
-    for (int i = 0; i < N; ++i)
+    int lastNumber = 0;
+    for(int i=0 ; i<8 ; ++i)
     {
-        int x, y;
-        cin >> x >> y;
-        Vector2 v = Vector2(x, y);
-        pos.push_back(v);
+        cin >> number;
+        if(lastNumber < number)
+        {
+            tmp++;
+        }
+        else
+        {
+            tmp--;
+        }
+        lastNumber = number;
     }
 
-    sort(pos.begin(), pos.end(), compare);
-
-    cout << "\n";
-    for (int i = 0; i < N; ++i)
+    if(tmp >= 8)
     {
-        cout << pos[i].x << " " << pos[i].y << "\n";
+        cout << "ascending";
+    }
+    else if(tmp <= -5)
+    {
+        cout << "descending";
+    }
+    else
+    {
+        cout << "mixed";
     }
 }
