4aa0fe320e8e3c057d907806e84dd8dceee591c9
saens <songsh5775@gmail.com>
2022-02-23 00:42:36 +0900

    BJ 1874

--- Practice.cpp
+++ Practice.cpp
@@ -1,35 +1,58 @@
 #include <iostream>
 #include <algorithm>
+#include <stack>
+#include <queue>
 
 using namespace std;
-
-int reverseStringToInt(string n)
-{
-    reverse(n.begin(), n.end());
-    return stoi(n);
-}
-
-int max(int a, int b)
-{
-    if(a > b)
-    {
-        return a;
-    }
-    else
-    {
-        return b;
-    }
-}
 
 int main()
 {
     ios::sync_with_stdio(false);
     cin.tie(NULL);
 
-    string a, b;
-    cin >> a >> b;
+    int n;
+    cin >> n;
 
-    int c = max(reverseStringToInt(a), reverseStringToInt(b));
+    int lastNatural = 1;
+    stack<int> stack1234; // 1부터 순차적으로 쌓아올릴 친구
+    queue<int> seq;
 
-    cout << c;
+    string history;
+
+    for(int i=0 ; i<n ; ++i)
+    {
+        int number;
+        cin >> number;
+        seq.push(number);
+    }
+
+    int whileCount = 0;
+    while(seq.empty() == false)
+    {
+        whileCount++;
+        int popped = seq.front();
+        seq.pop();
+
+        while(lastNatural <= popped)
+        {
+            stack1234.push(lastNatural);
+            history += "+\n";
+            lastNatural++;
+        }
+
+        int popped1234 = stack1234.top();
+
+        stack1234.pop();
+        history += "-\n";
+
+
+        if (popped1234 > popped)
+        {
+            cout << "NO";
+            return 0;
+        }
+    }
+
+    cout << history;
+
 }
