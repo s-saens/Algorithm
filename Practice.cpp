e020575d4a7999fdc9cf1d07ec650d7ed052674d
saens <songsh5775@gmail.com>
2022-02-25 01:50:55 +0900

    BJ 1003

--- Practice.cpp
+++ Practice.cpp
@@ -1,28 +1,53 @@
 #include <iostream>
-#include <cmath>
 
 using namespace std;
 
-int MIN(int a, int b)
-{
-    if(a > b)
-    {
-        return b;
-    }
-    else
-    {
-        return a;
-    }
-}
 
 int main()
 {
     ios::sync_with_stdio(false);
     cin.tie(NULL);
 
-    int x, y, w, h;
+    long long fibo[50];
+    fibo[0] = 0;
+    fibo[1] = 1;
+    fibo[2] = 1;
+    fibo[3] = 2;
+    for(int i=4 ; i<50 ; ++i)
+    {
+        fibo[i] = fibo[i-1] + fibo[i-2];
+    }
 
-    cin >> x >> y >> w >> h;
+    int T;
 
-    cout << MIN( MIN(x, y), MIN( abs(w-x), abs(h-y) ) );
+    cin >> T;
+
+    int N[T];
+
+    for(int i=0 ; i<T ; ++i)
+    {
+        cin >> N[i];
+    }
+    for(int i=0 ; i<T ; ++i)
+    {
+        switch (N[i])
+        {
+        case 0:
+            cout << 1 << " " << 0 << "\n";
+            break;
+        case 1:
+            cout << 0 << " " << 1 << "\n";
+            break;
+        case 2:
+            cout << 1 << " " << 1 << "\n";
+            break;
+        case 3:
+            cout << 1 << " " << 2 << "\n";
+            break;
+
+        default:
+            cout << fibo[N[i]-1] << " " << fibo[N[i]] << "\n";
+            break;
+        }
+    }
 }
