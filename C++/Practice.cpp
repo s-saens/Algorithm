fed4632df1847c33eb37805405868e8c3859ae90
saens <songsh5775@gmail.com>
2022-03-03 23:11:37 +0900

    BJ 2751 - STL sort

--- C++/Practice.cpp
+++ C++/Practice.cpp
@@ -1,67 +1,28 @@
 #include <iostream>
-#include <vector>
 #include <algorithm>
 
 using namespace std;
-
-vector<int> arr;
-
-int BinarySearch(int num) // This function returns the first index of the number. If num is not in the container, it returns -1.
-{
-    int len = arr.size();
-    int l = 0;
-    int r = len - 1;
-
-    while(l < r)
-    {
-        int m = (l+r)/2;
-        if(arr[m] >= num)
-        {
-            r = m;
-        }
-        else
-        {
-            l = m+1;
-        }
-    }
-    if(arr[l] == num)
-    {
-        return l;
-    }
-
-    return -1;
-}
 
 int main()
 {
     ios::sync_with_stdio(false);
     cin.tie(NULL);
+    cout.tie(NULL);
 
     int N;
-    cout << "Array size : ";
     cin >> N;
 
-    cout << "Array members(in sorted order) : ";
-    for (int i = 0; i < N; ++i)
+    int arr[N];
+
+    for(int i=0 ; i<N ; ++i)
     {
-        int num;
-        cin >> num;
-        arr.push_back(num);
+        cin >> arr[i];
     }
 
-    int S;
-    cout << "Number to search : ";
-    cin >> S;
+    sort(arr, arr+N);
 
-    int index = BinarySearch(S);
-
-    cout << "\nIndex : " << index;
-    if(index > -1)
+    for(int i=0 ; i<N ; ++i)
     {
-        cout << "\nNumber : " << arr[index];
-    }
-    else
-    {
-        cout << "\nNumber is not in the array\n";
+        cout << arr[i] << "\n";
     }
 }
