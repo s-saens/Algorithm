9f1955e783653eba7049ec908d7b6d75eae51f13
saens <songsh5775@gmail.com>
2022-03-05 03:06:58 +0900

    BJ 10816
    시간초과 풀이 : 이분탐색으로 처음 나오는 원소의 인덱스만 찾고, 그로부터 오른쪽 끝 도달할때까지 순차 탐색했음.
    시간통과 풀이 : 처음, 마지막 나오는 원소의 인덱스를 찾고, 그 두 값의 차이 + 1

--- C++/Practice.cpp
+++ C++/Practice.cpp
@@ -8,6 +8,12 @@ int SearchFirstIndex(int* cards, int length, int number)
     int l, r, m;
     l = 0;
     r = length-1;
+
+    if (cards[l] == number)
+    {
+        return l;
+    }
+
     while(l < r)
     {
         m = (l+r)/2;
@@ -20,7 +26,42 @@ int SearchFirstIndex(int* cards, int length, int number)
             l = m+1;
         }
     }
+
+    if(cards[l] != number)
+    {
+        return -1;
+    }
     return l;
+}
+
+int SearchLastIndex(int *cards, int length, int number)
+{
+    int l, r, m;
+    l = 0;
+    r = length - 1;
+
+    if (cards[r] == number)
+    {
+        return r;
+    }
+
+    while (l < r)
+    {
+        m = (l + r) / 2;
+        if (cards[m] <= number)
+        {
+            l = m+1;
+        }
+        else
+        {
+            r = m;
+        }
+    }
+    if (cards[l-1] != number)
+    {
+        return -1;
+    }
+    return l-1;
 }
 
 int main()
@@ -53,20 +94,14 @@ int main()
     for(int i=0 ; i<M ; ++i)
     {
         int num = numbers[i];
-        int searchIndex = SearchFirstIndex(cards, N, num);
-        int cnt = 0;
-        for(int j=searchIndex ; j<N ; ++j)
+        int firstIndex = SearchFirstIndex(cards, N, num);
+        int lastIndex = SearchLastIndex(cards, N, num);
+
+        counts[i] = lastIndex-firstIndex + 1;
+        if (firstIndex == -1)
         {
-            if(cards[j] == num)
-            {
-                cnt++;
-            }
-            else
-            {
-                break;;
-            }
+            counts[i]--;
         }
-        counts[i] = cnt;
     }
 
     for(int i=0 ; i<M ; ++i)
