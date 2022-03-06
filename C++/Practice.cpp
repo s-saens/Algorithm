19d90773983ad2822047afc537269e78da5a6b95
saens <songsh5775@gmail.com>
2022-03-06 21:11:25 +0900

    BJ 1037

--- C++/Practice.cpp
+++ C++/Practice.cpp
@@ -1,26 +1,8 @@
 #include <iostream>
 #include <algorithm>
+#include <climits>
 
 using namespace std;
-
-int DP[31][31]; // DP[x][y] 에 있는 원소는 F(x, y)를 의미함.
-
-int Calc(int x, int y)
-{
-    if(DP[x][y] != -1)
-    {
-        return DP[x][y];
-    }
-
-    int sum = 0;
-    for(int i=y-1 ; i>=x-1 ; --i)
-    {
-        sum += Calc(x-1,i);
-    }
-
-    DP[x][y] = sum;
-    return sum;
-}
 
 int main()
 {
@@ -29,41 +11,20 @@ int main()
     cin.tie(NULL);
     cout.tie(NULL);
 
-    int T;
-    cin >> T;
+    int N;
+    cin >> N;
 
-    for(short i=0 ; i<31 ; ++i)
+    int minimum = INT_MAX;
+    int maximum = 0;
+
+    for(int i=0 ; i<N ; ++i)
     {
-        for(short j=0 ; j<31 ; ++j)
-        {
-            if(i == j)
-            {
-                DP[i][j] = 1;
-            }
-            else if(i==1)
-            {
-                DP[i][j] = j;
-            }
-            else
-            {
-                DP[i][j] = -1;
-            }
-        }
+        int a;
+        cin >> a;
+        if(minimum > a) minimum = a;
+        if(maximum < a) maximum = a;
     }
 
-    int answers[T];
-
-    for(int i=0 ; i<T ; ++i)
-    {
-        int x, y;
-        cin >> x >> y;
-        answers[i] = Calc(x, y);
-    }
-
-    for(int i=0; i<T ; ++i)
-    {
-        cout << answers[i] << "\n";
-    }
-
+    cout << minimum * maximum;
     return 0;
 }
