960cb104b56513c528d6cb652ae26a58da3920c5
saens <songsh5775@gmail.com>
2022-03-09 01:33:22 +0900

    BJ 1308

--- C++/Practice.cpp
+++ C++/Practice.cpp
@@ -2,6 +2,60 @@
 
 using namespace std;
 
+bool isYoonYear(int year) // 2월29일이 추가됨.
+{
+    bool ret = true;
+    if (year % 4 == 0)
+    {
+        if (year % 100 == 0)
+        {
+            if (year % 400 == 0)
+            {
+                return true;
+            }
+            return false;
+        }
+        return true;
+    }
+    return false;
+}
+
+int YearToDays(int year)
+{
+    int sum = 0;
+    for (int i = 1; i < year; ++i)
+    {
+        if (isYoonYear(i))
+        {
+            sum += 366;
+        }
+        else
+        {
+            sum += 365;
+        }
+    }
+    return sum;
+}
+
+int mDays[13] = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+int MonthToDays(int month, int year)
+{
+    int sum = 0;
+    for(int i=1 ; i<month ; ++i)
+    {
+        if (i == 2 && isYoonYear(year))
+        {
+            sum += 29;
+        }
+        else
+        {
+            sum += mDays[i];
+        }
+    }
+    return sum;
+}
+
 int main()
 {
     ios::sync_with_stdio(false);
@@ -9,47 +63,37 @@ int main()
     cin.tie(NULL);
     cout.tie(NULL);
 
-    int N, K;
-    cin >> N >> K;
+    int Y1, M1, D1;
+    int Y2, M2, D2;
+    cin >> Y1 >> M1 >> D1;
+    cin >> Y2 >> M2 >> D2;
 
-    int jose[N];
-    int peop[N];
-
-    for(int i=0 ; i<N ; ++i)
+    if(Y2-Y1 > 1000)
     {
-        peop[i] = i+1;
-        jose[i] = -1;
+        cout << "gg";
+        return 0;
     }
-
-    int lastIndex = -1;
-    for(int i=0 ; i<N ; ++i)
+    else if(Y2 - Y1 == 1000)
     {
-        int remainMove = K;
-
-        while(remainMove>0)
+        if(M2 > M1)
         {
-            lastIndex = (lastIndex + 1) % N;
-            if (peop[lastIndex] == -1)
+            cout << "gg";
+            return 0;
+        }
+        else if(M2 == M1)
+        {
+            if(D2 >= D1)
             {
-                remainMove++;
+                cout << "gg";
+                return 0;
             }
-            remainMove--;
-        }
-        jose[i] = peop[lastIndex];
-        peop[lastIndex] = -1;
-    }
-
-    cout << '<';
-    for(int i=0 ; i<N ; ++i)
-    {
-        cout << jose[i];
-        if(i < N-1)
-        {
-            cout << ", ";
         }
     }
-    cout << '>';
 
+    int days1 = YearToDays(Y1) + MonthToDays(M1, Y1) + D1;
+    int days2 = YearToDays(Y2) + MonthToDays(M2, Y2) + D2;
+
+    cout << "D-" << days2 - days1;
 
     return 0;
 }
