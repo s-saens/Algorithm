0b51449aef1101c01c82247eec74ba603aa80f32
saens <songsh5775@gmail.com>
2022-03-10 00:05:16 +0900

    BJ 1251

--- C++/Practice.cpp
+++ C++/Practice.cpp
@@ -1,60 +1,7 @@
 #include <iostream>
+#include <algorithm>
 
 using namespace std;
-
-bool isYoonYear(int year) // 2월29일이 추가됨.
-{
-    bool ret = true;
-    if (year % 4 == 0)
-    {
-        if (year % 100 == 0)
-        {
-            if (year % 400 == 0)
-            {
-                return true;
-            }
-            return false;
-        }
-        return true;
-    }
-    return false;
-}
-
-int YearToDays(int year)
-{
-    int sum = 0;
-    for (int i = 1; i < year; ++i)
-    {
-        if (isYoonYear(i))
-        {
-            sum += 366;
-        }
-        else
-        {
-            sum += 365;
-        }
-    }
-    return sum;
-}
-
-int mDays[13] = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-
-int MonthToDays(int month, int year)
-{
-    int sum = 0;
-    for(int i=1 ; i<month ; ++i)
-    {
-        if (i == 2 && isYoonYear(year))
-        {
-            sum += 29;
-        }
-        else
-        {
-            sum += mDays[i];
-        }
-    }
-    return sum;
-}
 
 int main()
 {
@@ -63,37 +10,38 @@ int main()
     cin.tie(NULL);
     cout.tie(NULL);
 
-    int Y1, M1, D1;
-    int Y2, M2, D2;
-    cin >> Y1 >> M1 >> D1;
-    cin >> Y2 >> M2 >> D2;
+    string s;
+    cin >> s;
 
-    if(Y2-Y1 > 1000)
+    int len = s.length();
+
+    string answer = "{";
+
+    for(int i=0 ; i<len-2 ; ++i)
     {
-        cout << "gg";
-        return 0;
-    }
-    else if(Y2 - Y1 == 1000)
-    {
-        if(M2 > M1)
+        string newS;
+        string fragments[3];
+        for(int j=i+1 ; j<len-1 ; ++j)
         {
-            cout << "gg";
-            return 0;
-        }
-        else if(M2 == M1)
-        {
-            if(D2 >= D1)
+            // (시작위치, 길이)
+            fragments[0] = s.substr(0, i+1);
+            reverse(fragments[0].begin(), fragments[0].end());
+            fragments[1] = s.substr(i + 1, j - i);
+            reverse(fragments[1].begin(), fragments[1].end());
+            fragments[2] = s.substr(j + 1, len - j - 1);
+            reverse(fragments[2].begin(), fragments[2].end());
+            
+            newS = fragments[0] + fragments[1] + fragments[2];
+
+            if (answer.compare(newS) > 0)
             {
-                cout << "gg";
-                return 0;
+                answer = newS;
             }
         }
+
     }
 
-    int days1 = YearToDays(Y1) + MonthToDays(M1, Y1) + D1;
-    int days2 = YearToDays(Y2) + MonthToDays(M2, Y2) + D2;
-
-    cout << "D-" << days2 - days1;
+    cout << answer;
 
     return 0;
 }
