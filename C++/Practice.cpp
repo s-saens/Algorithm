3f2f14d7b972d44b7a6b11c2ac59589bb1d742dd
saens <songsh5775@gmail.com>
2022-03-10 02:50:38 +0900

    BJ 1340
    C++ iostream 입출력 속도 빠르게 하는 구문 때문에 scanf를 사용하면 안됨.
    다음 링크 참고
    https://hegosumluxmundij.tistory.com/54

--- C++/Practice.cpp
+++ C++/Practice.cpp
@@ -1,90 +1,124 @@
 #include <iostream>
+#include <stdio.h>
 
 using namespace std;
 
-struct Point
+int mDays[13] = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+bool isYoonYear(int year) // 2월29일이 추가됨.
 {
-    int column;
-    short row;
-};
+    if (year % 4 == 0)
+    {
+        if (year % 100 == 0)
+        {
+            if (year % 400 == 0)
+            {
+                return true;
+            }
+            return false;
+        }
+        return true;
+    }
+    return false;
+}
 
-bool visited[6][6];
-
-int main()
+int DaysOfYear(int year)
 {
-    ios::sync_with_stdio(false);
-    ios_base::sync_with_stdio(0);
-    cin.tie(NULL);
-    cout.tie(NULL);
-
-    for (int i = 0; i < 6; ++i)
+    if (isYoonYear(year))
     {
-        for (int j = 0; j < 6; ++j)
-        {
-            visited[i][j] = false;
-        }
-    }
-
-    Point firstPoint, lastPoint;
-
-    string input;
-    cin >> input;
-
-    lastPoint.column = (int)input[0] - (int)'A';
-    lastPoint.row = (int)input[1] - (int)'1';
-
-    visited[lastPoint.column][lastPoint.row] = true;
-
-    firstPoint.column = lastPoint.column;
-    firstPoint.row = lastPoint.row;
-
-    bool valid = true;
-
-    for (int i = 1; i < 36; ++i)
-    {
-        Point p;
-        cin >> input;
-        p.column = (int)input[0] - (int)'A';
-        p.row = (int)input[1] - (int)'1';
-
-        // cout << p.column << ", " << p.row << "\n";
-
-        if (visited[p.column][p.row])
-        {
-            valid = false;
-        }
-
-        int deltaColumn = abs(p.column - lastPoint.column);
-        int deltaRow = abs(p.row - lastPoint.row);
-
-        if (deltaRow + deltaColumn != 3 || deltaRow * deltaColumn != 2)
-        {
-            valid = false;
-        }
-
-        lastPoint.column = p.column;
-        lastPoint.row = p.row;
-        
-        visited[p.column][p.row] = true;
-    }
-
-    int dC = abs(lastPoint.column - firstPoint.column);
-    int dR = abs(lastPoint.row - firstPoint.row);
-
-        if (dC + dR != 3 || dC * dR != 2)
-    {
-        valid = false;
-    }
-
-
-    if (valid)
-    {
-        cout << "Valid";
+        mDays[2] = 29;
+        return 366;
     }
     else
     {
-        cout << "Invalid";
+        mDays[2] = 28;
+        return 365;
     }
+}
+
+int MonthToDays(int month)
+{
+    int sum = 0;
+    for (int i = 1; i < month; ++i)
+    {
+        sum += mDays[i];
+    }
+    return sum;
+}
+
+int MonthStringToInt(string s)
+{
+    if (s == "January")
+        return 1;
+    if (s == "February")
+        return 2;
+    if (s == "March")
+        return 3;
+    if (s == "April")
+        return 4;
+    if (s == "May")
+        return 5;
+    if (s == "June")
+        return 6;
+    if (s == "July")
+        return 7;
+    if (s == "August")
+        return 8;
+    if (s == "September")
+        return 9;
+    if (s == "October")
+        return 10;
+    if (s == "November")
+        return 11;
+    if (s == "December")
+        return 12;
+    return -1;
+}
+
+int TimeMinutes(int h, int m)
+{
+    return (h * 60) + m;
+}
+
+int DaysToMinutes(int days)
+{
+    return days * 60 * 24;
+}
+
+int main()
+{
+    // ios::sync_with_stdio(false);
+    // ios_base::sync_with_stdio(0);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cout << fixed;
+    cout.precision(15);
+
+    // inputs
+    string M;
+    cin >> M;
+    int months = MonthStringToInt(M);
+
+    int D;
+    scanf("%02d,", &D);
+
+    int year;
+    cin >> year;
+    int daysOfYear = DaysOfYear(year);
+
+    int h, m;
+    scanf("%02d:%02d", &h, &m);
+    // input end
+
+    // 모두 분으로 환산
+    int days = MonthToDays(months) + D;
+
+    int minutes = TimeMinutes(h, m) + DaysToMinutes(days - 1);
+
+    int minutesOfYear = DaysToMinutes(daysOfYear);
+
+    cout << (minutes * 100.0) / minutesOfYear << "\n";
 
     return 0;
 }
