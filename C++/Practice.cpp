c5dacba5d5e936254e25bca0d78ff2e91d55ccbf
saens <songsh5775@gmail.com>
2022-03-10 15:12:33 +0900

    BJ 1417

--- C++/Practice.cpp
+++ C++/Practice.cpp
@@ -1,52 +1,55 @@
 #include <iostream>
-#include <string>
-
-#define ull unsigned long long int
+#include <algorithm>
 
 using namespace std;
 
-ull TotalSum(int n)
-{
-    return (ull)n*(n+1);
-}
-
 int main()
 {
-    // ios_base::sync_with_stdio(false);
-    // cin.tie(NULL);
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
 
-    int N = 1;
-    int senario = 0;
-    while (N>0)
+    int N;
+    cin >> N;
+
+    if(N <= 1)
     {
-        senario++;
-        cin >> N;
-        if(N == 0) break;
-
-        string names[N+1];
-
-        string buffer;
-
-        getline(cin, buffer);
-
-        for(int i=0 ; i<N ; ++i)
-        {
-            getline(cin, names[i]);
-        }
-
-        ull sum = 0;
-        for(int i=0 ; i<(2*N)-1 ; ++i)
-        {
-            int number;
-            char ab;
-            cin >> number >> ab;
-            sum += number;
-        }
-
-        int lostIndex = TotalSum(N)-sum;
-
-        cout << senario << ' ' << names[lostIndex-1] << endl;
+        cout << 0;
+        return 0;
     }
+    N--;
+
+    int dasom;
+    int votes[N];
+    cin >> dasom;
+
+    int cnt = 0;
+
+    for(int i=0 ; i<N ; ++i)
+    {
+        cin >> votes[i];
+    }
+
+    sort(votes, votes+N);
+
+    while(true)
+    {
+        if(votes[N-1] >= dasom)
+        {
+            votes[N-1]--;
+            dasom++;
+            cnt++;
+            if(votes[N-2] > votes[N-1] && votes[N-2] >= dasom)
+            {
+                sort(votes, votes+N);
+            }
+        }
+        else
+        {
+            break;
+        }
+    }
+
+    cout << cnt;
 
     return 0;
 }
