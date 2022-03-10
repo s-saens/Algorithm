5ab73ce7e8d70d118012440f4f10feb57b0b3eaa
saens <songsh5775@gmail.com>
2022-03-10 14:31:17 +0900

    BJ 1380

--- C++/Practice.cpp
+++ C++/Practice.cpp
@@ -1,124 +1,52 @@
 #include <iostream>
-#include <stdio.h>
+#include <string>
+
+#define ull unsigned long long int
 
 using namespace std;
 
-int mDays[13] = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-
-bool isYoonYear(int year) // 2월29일이 추가됨.
+ull TotalSum(int n)
 {
-    if (year % 4 == 0)
-    {
-        if (year % 100 == 0)
-        {
-            if (year % 400 == 0)
-            {
-                return true;
-            }
-            return false;
-        }
-        return true;
-    }
-    return false;
-}
-
-int DaysOfYear(int year)
-{
-    if (isYoonYear(year))
-    {
-        mDays[2] = 29;
-        return 366;
-    }
-    else
-    {
-        mDays[2] = 28;
-        return 365;
-    }
-}
-
-int MonthToDays(int month)
-{
-    int sum = 0;
-    for (int i = 1; i < month; ++i)
-    {
-        sum += mDays[i];
-    }
-    return sum;
-}
-
-int MonthStringToInt(string s)
-{
-    if (s == "January")
-        return 1;
-    if (s == "February")
-        return 2;
-    if (s == "March")
-        return 3;
-    if (s == "April")
-        return 4;
-    if (s == "May")
-        return 5;
-    if (s == "June")
-        return 6;
-    if (s == "July")
-        return 7;
-    if (s == "August")
-        return 8;
-    if (s == "September")
-        return 9;
-    if (s == "October")
-        return 10;
-    if (s == "November")
-        return 11;
-    if (s == "December")
-        return 12;
-    return -1;
-}
-
-int TimeMinutes(int h, int m)
-{
-    return (h * 60) + m;
-}
-
-int DaysToMinutes(int days)
-{
-    return days * 60 * 24;
+    return (ull)n*(n+1);
 }
 
 int main()
 {
-    // ios::sync_with_stdio(false);
-    // ios_base::sync_with_stdio(0);
-    cin.tie(NULL);
-    cout.tie(NULL);
+    // ios_base::sync_with_stdio(false);
+    // cin.tie(NULL);
 
-    cout << fixed;
-    cout.precision(15);
+    int N = 1;
+    int senario = 0;
+    while (N>0)
+    {
+        senario++;
+        cin >> N;
+        if(N == 0) break;
 
-    // inputs
-    string M;
-    cin >> M;
-    int months = MonthStringToInt(M);
+        string names[N+1];
 
-    int D;
-    scanf("%02d,", &D);
+        string buffer;
 
-    int year;
-    cin >> year;
-    int daysOfYear = DaysOfYear(year);
+        getline(cin, buffer);
 
-    int h, m;
-    scanf("%02d:%02d", &h, &m);
-    // input end
+        for(int i=0 ; i<N ; ++i)
+        {
+            getline(cin, names[i]);
+        }
 
-    // 모두 분으로 환산
-    int days = MonthToDays(months) + D;
+        ull sum = 0;
+        for(int i=0 ; i<(2*N)-1 ; ++i)
+        {
+            int number;
+            char ab;
+            cin >> number >> ab;
+            sum += number;
+        }
 
-    int minutes = TimeMinutes(h, m) + DaysToMinutes(days - 1);
+        int lostIndex = TotalSum(N)-sum;
 
-    int minutesOfYear = DaysToMinutes(daysOfYear);
-
-    cout << (minutes * 100.0) / minutesOfYear << "\n";
+        cout << senario << ' ' << names[lostIndex-1] << endl;
+    }
 
     return 0;
 }
