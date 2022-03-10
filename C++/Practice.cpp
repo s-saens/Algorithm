39523e19b046ba2440a1eb7a834c17e6d8f5cfdc
saens <songsh5775@gmail.com>
2022-03-10 17:52:37 +0900

    Delete all

--- C++/Practice.cpp
+++ C++/Practice.cpp
@@ -1,55 +1,13 @@
 #include <iostream>
-#include <algorithm>
 
 using namespace std;
 
 int main()
 {
-    ios_base::sync_with_stdio(false);
+    ios_base::sync_with_stdio(0);
     cin.tie(NULL);
 
-    int N;
-    cin >> N;
 
-    if(N <= 1)
-    {
-        cout << 0;
-        return 0;
-    }
-    N--;
-
-    int dasom;
-    int votes[N];
-    cin >> dasom;
-
-    int cnt = 0;
-
-    for(int i=0 ; i<N ; ++i)
-    {
-        cin >> votes[i];
-    }
-
-    sort(votes, votes+N);
-
-    while(true)
-    {
-        if(votes[N-1] >= dasom)
-        {
-            votes[N-1]--;
-            dasom++;
-            cnt++;
-            if(votes[N-2] > votes[N-1] && votes[N-2] >= dasom)
-            {
-                sort(votes, votes+N);
-            }
-        }
-        else
-        {
-            break;
-        }
-    }
-
-    cout << cnt;
 
     return 0;
 }
