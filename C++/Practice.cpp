ea8013aa3015a9eebd48cdd4fef26305b52a4d23
saens <songsh5775@gmail.com>
2022-03-11 23:16:31 +0900

    [C++] BJ 11866

--- C++/Practice.cpp
+++ C++/Practice.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <deque>
 #include <vector>
 
 using namespace std;
@@ -9,72 +8,49 @@ int main()
     ios_base::sync_with_stdio(0);
     cin.tie(NULL);
 
-    deque<int> dq;
-    vector<int> answers;
+    int N, K;
+    cin >> N >> K;
 
-    int N;
-    cin >> N;
-    
+    vector<int> josephus;
+    int people[N];
+    bool selected[N];
+
     for(int i=0 ; i<N ; ++i)
     {
-        string op;
-        cin >> op;
-        int X = 0;
-
-        switch (op[1])
-        {
-            case 'u': // push
-                if(op[5] == 'f') // push front
-                {
-                    cin >> X;
-                    dq.push_front(X);
-                }
-                else if(op[5] == 'b') // push back
-                {
-                    cin >> X;
-                    dq.push_back(X);
-                }
-                break;
-            case 'o': // pop
-                if(dq.empty())
-                {
-                    answers.push_back(-1);
-                }
-                else
-                {
-                    if(op[4] == 'f')
-                    {
-                        answers.push_back(dq.front());
-                        dq.pop_front();
-                    }
-                    else if(op[4] == 'b')
-                    {
-                        answers.push_back(dq.back());
-                        dq.pop_back();
-                    }
-                }
-                break;
-            case 'i': // size
-                answers.push_back(dq.size());
-                break;
-            case 'm': // empty
-                answers.push_back(dq.empty());
-                break;
-            case 'r': // front
-                answers.push_back(dq.empty() ? -1 : dq.front());
-                break;
-            case 'a': // back
-                answers.push_back(dq.empty() ? -1 : dq.back());
-                break;
-            default:
-                break;
-        }
+        people[i] = i+1;
+        selected[i] = false;
     }
 
-    for(unsigned int i=0 ; i<answers.size() ; ++i)
+    int lastIndex = 0;
+
+    int cnt = K;
+    for (int i = 0; i < N; ++i)
     {
-        cout << answers[i] << "\n";
+        int cnt = K;
+        while(cnt > 0)
+        {
+            if (!selected[lastIndex])
+            {
+                cnt--;
+            }
+
+            if(cnt > 0)
+            {
+                lastIndex = (lastIndex + 1) % N;
+            }
+        }
+        josephus.push_back(people[lastIndex]);
+        selected[lastIndex] = true;
+        lastIndex = (lastIndex + 1) % N;
     }
+
+    string answer = "<";
+    cout << '<';
+    for(int i=0 ; i<N-1 ; ++i)
+    {
+        cout << josephus[i] << ", ";
+    }
+    cout << josephus[N-1] << '>';
 
     return 0;
 }
