6f1c9861c2355e998441ae49a2f535e26fd82f0c
saens <songsh5775@gmail.com>
2022-03-11 15:22:42 +0900

    [C++] BJ 10845

--- C++/Practice.cpp
+++ C++/Practice.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <queue>
+#include <vector>
 
 using namespace std;
 
@@ -6,39 +8,58 @@ int main()
 {
     ios_base::sync_with_stdio(0);
     cin.tie(NULL);
+
+    queue<int> q;
+    vector<int> answers;
+
+    int N;
+    cin >> N;
     
-    int requiredCnt[9];
-
-    for(int i=0 ; i<9 ; ++i)
+    for(int i=0 ; i<N ; ++i)
     {
-        requiredCnt[i] = 0;
-    }
+        string op;
+        cin >> op;
+        int X = 0;
 
-    string s;
-    cin >> s;
-    int len = s.length();
-
-    int maxRequireCnt = 0;
-
-    for(int i=0 ; i<len ; ++i)
-    {
-        if(s[i] == '9')
+        switch (op[1])
         {
-            requiredCnt[6]++;
-        }
-        else
-        {
-            requiredCnt[s[i] - '0']++;
+            case 'u': // push
+                cin >> X;
+                q.push(X);
+                break;
+            case 'o': // pop
+                if(!q.empty())
+                {
+                    answers.push_back(q.front());
+                    q.pop();
+                }
+                else
+                {
+                    answers.push_back(-1);
+                }
+                
+                break;
+            case 'i': // size
+                answers.push_back(q.size());
+                break;
+            case 'm': // empty
+                answers.push_back(q.empty());
+                break;
+            case 'r': // front
+                answers.push_back(q.empty() ? -1 : q.front());
+                break;
+            case 'a': // back
+                answers.push_back(q.empty() ? -1 : q.back());
+                break;
+            default:
+                break;
         }
     }
-    requiredCnt[6] = (requiredCnt[6] % 2) + requiredCnt[6] / 2;
 
-    for(int i=0 ; i<9 ; ++i)
+    for(unsigned int i=0 ; i<answers.size() ; ++i)
     {
-        maxRequireCnt = max(maxRequireCnt, requiredCnt[i]);
+        cout << answers[i] << "\n";
     }
-
-    cout << maxRequireCnt;
 
     return 0;
 }
