7f8d1097f730897c8a202ab77032f4ea2fa7054d
saens <songsh5775@gmail.com>
2022-03-11 13:43:24 +0900

    [C++] BJ 1427

--- C++/Practice.cpp
+++ C++/Practice.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <algorithm>
 
 using namespace std;
 
@@ -7,6 +8,25 @@ int main()
     ios_base::sync_with_stdio(0);
     cin.tie(NULL);
 
+    string s;
+    cin >> s;
+
+    int len = s.length();
+
+    int numbers[len];
+
+
+    for(int i=0 ; i<len ; ++i)
+    {
+        numbers[i] = (int)s[i]-(int)'0';
+    }
+
+    sort(numbers, numbers + len);
+
+    for(int i=len-1 ; i>=0 ; --i)
+    {
+        cout << numbers[i];
+    }
 
 
     return 0;
