481de4f44c6e3cd72b53abe78034fba3b5d649af
saens <songsh5775@gmail.com>
2022-03-12 23:19:28 +0900

    [C++] BJ 10989

--- C++/Practice.cpp
+++ C++/Practice.cpp
@@ -1,27 +1,36 @@
 #include <iostream>
-#include <cmath>
-using namespace std;
-#define ll long long int
 
+using namespace std;
+
+int numbers[10001];
 
 int main()
 {
     ios_base::sync_with_stdio(0);
     cin.tie(NULL);
 
-    ll A, B;
-    cin >> A >> B;
-
-    ll answer;
-    if(A > B)
+    for (int i = 0; i < 10001; ++i)
     {
-        answer = A - B;
-    }
-    else
-    {
-        answer = B - A;
+        numbers[i] = 0;
     }
 
-    cout << answer;
+    int N;
+    cin >> N;
+    for (int i = 0; i < N; ++i)
+    {
+        int n;
+        cin >> n;
+        numbers[n]++;
+    }
+
+    for (int i = 0; i < 10001; ++i)
+    {
+        while (numbers[i] > 0)
+        {
+            cout << i << "\n";
+            numbers[i]--;
+        }
+    }
+
     return 0;
 }
