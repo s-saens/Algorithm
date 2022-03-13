62bdef8c561945a8e77b802d1bb28a5509a7bdc5
saens <songsh5775@gmail.com>
2022-03-13 14:32:12 +0900

    [C++] BJ 7568

--- C++/Practice.cpp
+++ C++/Practice.cpp
@@ -2,34 +2,65 @@
 
 using namespace std;
 
-int numbers[10001];
+struct Person
+{
+    int weight;
+    int height;
+    int rank;
+};
+
+short Sign(int x)
+{
+    if(x > 0)
+    {
+        return 1;
+    }
+    else if(x < 0)
+    {
+        return -1;
+    }
+    return 0;
+}
+
+int Dungchi(Person p1, Person p2) // 0이면 덩치 판정 불가, 2이면 p1이 덩치 큼, -2이면 p2가 덩치 큼
+{
+    int deltaW = p1.weight - p2.weight;
+    int deltaH = p1.height - p2.height;
+    int signDW = Sign(deltaW);
+    int signDH = Sign(deltaH);
+    return signDW + signDH;
+}
 
 int main()
 {
     ios_base::sync_with_stdio(0);
     cin.tie(NULL);
 
-    for (int i = 0; i < 10001; ++i)
-    {
-        numbers[i] = 0;
-    }
-
     int N;
     cin >> N;
+
+    Person people[N];
+
     for (int i = 0; i < N; ++i)
     {
-        int n;
-        cin >> n;
-        numbers[n]++;
+        cin >> people[i].weight >> people[i].height;
+        people[i].rank = 1;
     }
 
-    for (int i = 0; i < 10001; ++i)
+    for (int i = 0; i < N; ++i)
     {
-        while (numbers[i] > 0)
+        for(int j=0 ; j<N ; ++j)
         {
-            cout << i << "\n";
-            numbers[i]--;
+            if( Dungchi(people[i], people[j]) == -2 )
+            {
+                people[i].rank += 1;
+            }
         }
+    }
+
+    for(int i=0 ; i<N ; ++i)
+    {
+        cout << people[i].rank << "\n";
     }
 
     return 0;
