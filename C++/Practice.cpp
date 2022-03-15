d0ce97d0ddc202f223e6f487357b2574a849784c
saens <songsh5775@gmail.com>
2022-03-15 17:38:14 +0900

    [C++] BJ 1966

--- C++/Practice.cpp
+++ C++/Practice.cpp
@@ -1,37 +1,75 @@
 #include <iostream>
-#include <stack>
+#include <queue>
+#include <algorithm>
 
 using namespace std;
+
+int GetAnswer(int N, int M)
+{
+    int pivotIndex = M;
+    queue<int> q;
+    int arr[N];
+
+    int maximum = 0;
+
+    for(int i=0 ; i<N ; ++i)
+    {
+        int a;
+        cin >> a;
+
+        q.push(a);
+        arr[i] = a;
+    }
+    sort(arr, arr+N);
+
+    int maxIndex = N-1;
+
+    int cnt = 0;
+    while(maxIndex >= 0)
+    {
+        if(arr[maxIndex] <= q.front()) // dequeue
+        {
+            cnt++;
+            if(pivotIndex == 0)
+            {
+                return cnt;
+            }
+            q.pop();
+            maxIndex--;
+        }
+        else // 맨 뒤로
+        {
+            q.push(q.front());
+            q.pop();
+        }
+        pivotIndex = (pivotIndex + q.size() - 1) % q.size();
+    }
+
+    return cnt;
+}
 
 int main()
 {
     ios_base::sync_with_stdio(0);
     cin.tie(NULL);
     
-    int K;
-    cin >> K;
+    int T;
+    cin >> T;
 
-    int sum = 0;
-    stack<int> numbers;
+    int answer[T];
 
-    for(int i=0 ; i<K ; ++i)
+    for(int i=0 ; i<T ; ++i)
     {
-        int input;
-        cin >> input;
+        int N, M;
+        cin >> N >> M;
 
-        if(input == 0)
-        {
-            sum -= numbers.top();
-            numbers.pop();
-        }
-        else
-        {
-            sum += input;
-            numbers.push(input);
-        }
+        answer[i] = GetAnswer(N, M);
     }
 
-    cout << sum;
+    for(int i=0 ; i<T ; ++i)
+    {
+        cout << answer[i] << "\n";
+    }
 
     return 0;
 }
