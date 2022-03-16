e4299654ff3e501c2f69df13947041ac69a893b9
saens <songsh5775@gmail.com>
2022-03-17 02:16:33 +0900

    [C++] BJ 1260

--- C++/Practice.cpp
+++ C++/Practice.cpp
@@ -1,90 +1,99 @@
 #include <iostream>
 #include <algorithm>
-#define ull unsigned long long int
+#include <queue>
+#include <vector>
 
 using namespace std;
 
-// 블럭 제거 2초
-// 블럭 놓기 1초
+struct Node
+{
+    bool visited;
+    vector<int> linkedNodeIndex;
+};
+
+Node nodes[1001];
+
+void InitiateFlag()
+{
+    for(int i=0 ; i<1001 ; ++i)
+    {
+        nodes[i].visited = false;
+    }
+}
+
+void DFS(int nodeIndex)
+{
+    vector<int>& linked = nodes[nodeIndex].linkedNodeIndex;
+
+    nodes[nodeIndex].visited = true;
+
+    cout << nodeIndex << ' ';
+
+    for(int i=0 ; i<linked.size() ; ++i)
+    {
+        if(!nodes[linked[i]].visited)
+        {
+            DFS(linked[i]);
+        }
+    }
+    
+}
+
+queue<int> q;
+
+void BFS(int nodeIndex)
+{
+    q.push(nodeIndex);
+    while(q.size() > 0)
+    {
+        int pop = q.front(); q.pop();
+        if(nodes[pop].visited)
+        {
+            continue;
+        }
+        cout << pop << ' ';
+        nodes[pop].visited = true;
+        vector<int>& linked = nodes[pop].linkedNodeIndex;
+        for(int i=0 ; i<linked.size() ; ++i)
+        {
+            if(!nodes[linked[i]].visited)
+            {
+                q.push(linked[i]);
+            }
+        }
+    }
+}
 
 int main()
 {
     ios_base::sync_with_stdio(0);
     cin.tie(NULL);
 
-    int X, Y;
-    cin >> X >> Y;
+    int N, M, V;
+    cin >> N >> M >> V;
 
-    int floorSize = X * Y; // floorSize
-
-    ull B;
-    cin >> B;
-
-    int blocks[floorSize];
-    int maxHeight = 0;
-    int minHeight = 256;
-    int diggables[floorSize];
-    int empties[floorSize];
-
-    for (int i = 0; i < floorSize; ++i)
+    for(int i=0 ; i<M ; ++i)
     {
-        cin >> blocks[i];
-        maxHeight = max(maxHeight, blocks[i]);
-        minHeight = min(minHeight, blocks[i]);
+        int a, b;
+        cin >> a >> b;
+        nodes[a].linkedNodeIndex.push_back(b);
+        nodes[b].linkedNodeIndex.push_back(a);
     }
 
-
-    for(int i=0 ; i<floorSize ; ++i)
+    for(int i=0 ; i<1001 ; ++i)
     {
-        empties[i] = maxHeight - blocks[i];
-    }
-    for(int i=floorSize-1 ; i>=0 ; --i)
-    {
-        diggables[i] = blocks[i] - minHeight;
+        vector<int>& linked = nodes[i].linkedNodeIndex;
+        sort(linked.begin(), linked.end());
     }
 
-    int minTime = 500 * 500 * 256;
-    int answerHeight = 0;
+    InitiateFlag();
 
-    // sort(blocks, blocks + floorSize);
+    DFS(V);
+    cout << "\n";
 
+    InitiateFlag();
 
-    for(int cuttingHeight = minHeight ; cuttingHeight <= maxHeight ; ++cuttingHeight)
-    {
-        int diggableCnt = 0;
-        int emptyCnt = 0;
-        for(int i=0 ; i<floorSize ; ++i)
-        {
-            if(blocks[i] < cuttingHeight) // 왼쪽에 있는 경우 : 채워야됨
-            {
-                emptyCnt += cuttingHeight - blocks[i];
-            }
-            else// 오른쪽에 있는 경우 : 파야됨
-            {
-                diggableCnt += blocks[i] - cuttingHeight;
-            }
-        }
-
-        if(diggableCnt + B >= emptyCnt) // 채울 수 있음, 즉 height 만족 가능.
-        {
-            int t = diggableCnt * 2 + emptyCnt;
-            if(minTime > t)
-            {
-                minTime = t;
-                answerHeight = cuttingHeight;
-            }
-            else if(minTime == t)
-            {
-                answerHeight = max(cuttingHeight, answerHeight);
-            }
-        }
-        else
-        {
-            break;
-        }
-    }
-
-    cout << minTime << ' ' << answerHeight;
+    BFS(V);
 
     return 0;
 }
