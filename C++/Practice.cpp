4f7a4c9d5c17f12faacf65d00d1e2b736910a934
saens <songsh5775@gmail.com>
2022-03-17 16:45:21 +0900

    [C++] BJ 1645
    문제 설명이 문제가 있는 문제.

--- C++/Practice.cpp
+++ C++/Practice.cpp
@@ -1,99 +1,37 @@
 #include <iostream>
 #include <algorithm>
-#include <queue>
-#include <vector>
 
 using namespace std;
-
-struct Node
-{
-    bool visited;
-    vector<int> linkedNodeIndex;
-};
-
-Node nodes[1001];
-
-void InitiateFlag()
-{
-    for(int i=0 ; i<1001 ; ++i)
-    {
-        nodes[i].visited = false;
-    }
-}
-
-void DFS(int nodeIndex)
-{
-    vector<int>& linked = nodes[nodeIndex].linkedNodeIndex;
-
-    nodes[nodeIndex].visited = true;
-
-    cout << nodeIndex << ' ';
-
-    for(int i=0 ; i<linked.size() ; ++i)
-    {
-        if(!nodes[linked[i]].visited)
-        {
-            DFS(linked[i]);
-        }
-    }
-    
-}
-
-queue<int> q;
-
-void BFS(int nodeIndex)
-{
-    q.push(nodeIndex);
-    while(q.size() > 0)
-    {
-        int pop = q.front(); q.pop();
-        if(nodes[pop].visited)
-        {
-            continue;
-        }
-        cout << pop << ' ';
-        nodes[pop].visited = true;
-        vector<int>& linked = nodes[pop].linkedNodeIndex;
-        for(int i=0 ; i<linked.size() ; ++i)
-        {
-            if(!nodes[linked[i]].visited)
-            {
-                q.push(linked[i]);
-            }
-        }
-    }
-}
 
 int main()
 {
     ios_base::sync_with_stdio(0);
     cin.tie(NULL);
 
-    int N, M, V;
-    cin >> N >> M >> V;
+    int N;
+    cin >> N;
+    int k[N];
 
-    for(int i=0 ; i<M ; ++i)
+    for(int i=0 ; i<N ; ++i)
     {
-        int a, b;
-        cin >> a >> b;
-        nodes[a].linkedNodeIndex.push_back(b);
-        nodes[b].linkedNodeIndex.push_back(a);
+        cin >> k[i];
     }
 
-    for(int i=0 ; i<1001 ; ++i)
+    sort(k, k+N);
+
+    int requiredCnt = 0;
+    int answer = 0;
+    for(int i=0 ; i<N ; ++i)
     {
-        vector<int>& linked = nodes[i].linkedNodeIndex;
-        sort(linked.begin(), linked.end());
+        requiredCnt = k[i];
+        if(i >= requiredCnt)
+        {
+            answer = i+1;
+            break;
+        }
     }
 
-    InitiateFlag();
-
-    DFS(V);
-    cout << "\n";
-
-    InitiateFlag();
-
-    BFS(V);
+    cout <<  answer;
 
     return 0;
 }
