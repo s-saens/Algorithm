80a3574ba8fa2ed83d849d116a1ff754a9d7b681
saens <songsh5775@gmail.com>
2022-03-24 03:43:47 +0900

    [C++] BJ 15650

--- C++/Practice.cpp
+++ C++/Practice.cpp
@@ -1,69 +1,37 @@
 #include <iostream>
-#include <vector>
 
 using namespace std;
+
+int N, M;
+
+void Permutation(int start, int cnt, string s)
+{
+    if(cnt >= M-1)
+    {
+        cout << s + to_string(start) << "\n";
+        return;
+    }
+
+    s += to_string(start) + ' ';
+
+    for(int i=start+1 ; i<=N ; ++i)
+    {
+        Permutation(i, cnt+1, s);
+    }
+    return;
+}
 
 int main()
 {
     ios::sync_with_stdio(false);
     cin.tie(NULL);
 
-    int N, len;
-    cin >> N >> len;
-
-    vector<int> possiblesLengths;
-
-    string ioioi;
-    cin >> ioioi;
-
-    int cnt = 0;
-    int answer = 0;
-
-    for (int i = 0; i < len; ++i)
+    cin >> N >> M;
+    
+    for(int i=1 ; i<=N ; ++i)
     {
-        if (cnt <= 0) // seq의 처음 시작은 I여야 함.
-        {
-            if (ioioi[i] == 'I')
-            {
-                cnt++;
-            }
-        }
-        else if (i > 0) // seq가 시작됨.
-        {
-            if (ioioi[i - 1] == 'I' && ioioi[i] == 'O' && i < len - 1)
-            {
-                cnt++; // 마지막이 O일 경우, 다음 I가 나올 수 없으므로, 빼야 함.
-            }
-            else if (ioioi[i - 1] == 'O' && ioioi[i] == 'I')
-            {
-                cnt++;
-            }
-            else // seq 깨짐. cnt = 0
-            {
-                if (cnt >= 2 * N + 1)
-                {
-                    int l = (cnt - 1) / 2; // seq에서 O의 개수
-                    answer += l - N + 1;
-                }
-
-                if (ioioi[i] == 'O')
-                {
-                    cnt = 0;
-                }
-                else if (ioioi[i] == 'I')
-                {
-                    cnt = 1;
-                }
-            }
-        }
+        Permutation(i, 0, "");
     }
-    if (cnt >= 2 * N + 1)
-    {
-        int l = (cnt - 1) / 2; // seq에서 O의 개수
-        answer += l - N + 1;
-    }
-
-    cout << answer;
 
     return 0;
 }
