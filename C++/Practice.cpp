83e0151b51469afa78c40276ceb2a87f064c8b22
saens <songsh5775@gmail.com>
2022-03-24 17:11:48 +0900

    [C++] BJ 2448

--- C++/Practice.cpp
+++ C++/Practice.cpp
@@ -1,48 +1,79 @@
 #include <iostream>
-#include <vector>
-#include <algorithm>
 
 using namespace std;
 
-int N, M;
-vector<int> v;
+/* 연습장
 
+     o     
+    o o    
+   ooooo   
+  o     o  
+ o o   o o 
+ooooo ooooo
 
-void Permutation(int startIndex, int cnt, string s)
+s1 = GetStars()
+
+*/
+
+string* map;
+
+void F(int x, int y, int length)
 {
-    if(cnt >= M-1)
+    if(length == 3)
     {
-        cout << s + to_string(v[startIndex]) << "\n";
+        for(int i=y ; i>y-3 ; --i)
+        {
+            for(int j=x ; j<=x+4 ; ++j)
+            {
+                if(i % 3 == 2)
+                {
+                    map[i][j] = '*';
+                }
+                else if(i % 3 == 1 && (j == x+1 || j == x+3))
+                {
+                    map[i][j] = '*';
+                }
+                else if(i % 3 == 0 && j == x+2)
+                {
+                    map[i][j] = '*';
+                }
+            }
+        }
         return;
     }
 
-    s += to_string(v[startIndex]) + ' ';
-
-    for(int i=startIndex ; i<N ; ++i)
-    {
-        Permutation(i, cnt+1, s);
-    }
-    return;
+    F(x, y, length/2); // 왼쪽 아래
+    F(x + length, y, length/2); // 오른쪽 아래
+    F(x + (length/2), y - (length/2), length/2); // 위
 }
+
 
 int main()
 {
     ios::sync_with_stdio(false);
     cin.tie(NULL);
 
-    cin >> N >> M;
+    int N;
+    cin >> N;
+
+    string m[N];
+    map = m;
+
     for(int i=0 ; i<N ; ++i)
     {
-        int a;
-        cin >> a;
-        v.push_back(a);
+        for(int j=0 ; j<N*2-1 ; ++j)
+        {
+            map[i] += ' ';
+        }
     }
-    sort(v.begin(), v.end());
-    
+
+    F(0, N-1, N);
+
     for(int i=0 ; i<N ; ++i)
     {
-        Permutation(i, 0, "");
+        cout << m[i] << "\n";
     }
+
 
     return 0;
 }
