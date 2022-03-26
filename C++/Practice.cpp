669da87743acfb2ccfeaeb604d7f85d43e125d1a
saens <songsh5775@gmail.com>
2022-03-26 22:50:32 +0900

    [C++] BJ 1934

--- C++/Practice.cpp
+++ C++/Practice.cpp
@@ -1,63 +1,45 @@
 #include <iostream>
 #include <algorithm>
 
-#define ull __uint128_t
-
 using namespace std;
-
-ull comb[101][101];
-
-ull Comb(int n, int r)
-{
-    if(comb[n][r] > 0)
-    {
-        return comb[n][r];
-    }
-    if(r == 1 || r == n-1)
-    {
-        return n;
-    }
-    if(r == 0 || r == n)
-    {
-        return 1;
-    }
-
-    comb[n][r] = Comb(n - 1, r - 1) + Comb(n - 1, r);
-    return comb[n][r];
-}
-
-string Uint128ToString(ull a)
-{
-    string ret = "";
-
-    while(a > 0)
-    {
-        ret += to_string((int)(a % 10));
-        a /= 10;
-    }
-
-    reverse(ret.begin(), ret.end());
-
-    return ret;
-}
 
 int main()
 {
     ios::sync_with_stdio(false);
     cin.tie(NULL);
 
-    for(int i=0 ; i<101 ; ++i)
+    int T;
+    cin >> T;
+
+    int answers[T];
+
+    for(int i=0 ; i<T ; ++i)
     {
-        for(int j=0 ; j<101 ; ++j)
+        answers[i] = 1;
+
+        int number[2];
+        cin >> number[0] >> number[1];
+
+        sort(number, number+2);
+
+        for(int j=2 ; j<=number[0] ; ++j)
         {
-            comb[i][j] = 0;
+            if(number[0]%j == 0 && number[1]%j == 0)
+            {
+                answers[i] *= j;
+                number[0] /= j;
+                number[1] /= j;
+                j--;
+            }
         }
+
+        answers[i] *= number[0] * number[1];
     }
 
-    int n, r;
-    cin >> n >> r;
-
-    cout << Uint128ToString(Comb(n, r));
+    for(int i=0 ; i<T ; ++i)
+    {
+        cout << answers[i] << "\n";
+    }
 
     return 0;
 }
