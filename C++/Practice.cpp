13875fedb3c9dcc398e75ddef7003ed3b737b3a2
saens <songsh5775@gmail.com>
2022-04-01 02:35:59 +0900

    [C++] BJ 11725

--- C++/Practice.cpp
+++ C++/Practice.cpp
@@ -4,83 +4,26 @@
 
 using namespace std;
 
-int N, E, K;
-int INF = 2147483647;
-
-struct Edge
-{
-    int next;
-    int cost;
-};
-
 struct Node
 {
-    int index = 1;
-    bool visited = false;
-    vector<Edge> edges;
+    int parent = -1;
+    vector<int> linked;
 };
 
-Node *nodes;
-int *answer;
+int N;
+Node* nodes;
 
-void Init()
+void SetParent(int p)
 {
-    for (int i = 1; i <= N; ++i)
+    vector<int> &linked = nodes[p].linked;
+
+    for(int i=0 ; i<linked.size() ; ++i)
     {
-        if(i == K) answer[i] = 0;
-        else answer[i] = INF;
-
-        nodes[i].index = i;
-
-        Edge e;
-        e.cost = 0;
-        e.next = i;
-        nodes[i].edges.push_back(e);
-    }
-}
-
-void Dijkstra(int index)
-{
-    Init();
-
-    int cummCost = 0;
-    while(true)
-    {
-        vector<Edge>& edges = nodes[index].edges;
-
-        Edge minEdge;
-        minEdge.cost = INF;
-        minEdge.next = -1;
-        
-        nodes[index].visited = true;
-
-        for(int j=0 ; j<edges.size() ; ++j)
+        if(nodes[linked[i]].parent == -1)
         {
-            Edge e = edges[j];
-
-            if(nodes[e.next].visited)
-            {
-                continue;
-            }
-
-            if(answer[e.next] > e.cost + cummCost)
-            {
-                answer[e.next] = e.cost + cummCost;
-            }
-
-            if(minEdge.cost > e.cost)
-            {
-                minEdge = e;
-            }
+            nodes[linked[i]].parent = p;
+            SetParent(linked[i]);
         }
-
-        if(minEdge.next == -1)
-        {
-            break;
-        }
-
-        index = minEdge.next;
-        cummCost += minEdge.cost;
     }
 }
 
@@ -89,38 +32,25 @@ int main()
     ios::sync_with_stdio(false);
     cin.tie(NULL);
 
-    cin >> N >> E >> K;
-
-    Node n[N + 1];
+    cin >> N;
+    Node n[N+1];
     nodes = n;
-    int a[N + 1];
-    answer = a;
 
-    for (int i = 0; i < E; ++i)
+    n[1].parent = 0;
+
+    for(int i=2 ; i<=N ; ++i)
     {
-        int a, b, c;
-        cin >> a >> b >> c;
-
-        if(a > N || b > N) continue;
-        if(b == K) continue;
-        
-        Edge e;
-        e.cost = c;
-        e.next = b;
-
-        nodes[a].edges.push_back(e);
+        int a, b;
+        cin >> a >> b;
+        nodes[a].linked.push_back(b);
+        nodes[b].linked.push_back(a);
     }
 
-    Dijkstra(K);
+    SetParent(1);
 
-    for (int i = 1; i <= N; ++i)
+    for(int i=2 ; i<=N ; ++i)
     {
-        if (answer[i] == INF)
-        {
-            cout << "INF" << "\n";
-            continue;
-        }
-        cout << answer[i] << "\n";
+        cout << nodes[i].parent << "\n";
     }
 
     return 0;
