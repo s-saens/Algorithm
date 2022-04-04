4c73e12e4ebbaf27b50f6970cfde5d15e3b11ecc
saens <songsh5775@gmail.com>
2022-04-04 16:56:20 +0900

    [C++] BJ 11659

--- C++/Practice.cpp
+++ C++/Practice.cpp
@@ -1,99 +1,36 @@
 #include <iostream>
-#include <vector>
-#include <algorithm>
 
 using namespace std;
 
-struct Node
-{
-    short num = 0;
-    int dist = 0;    // distance from root node
-    short level = 0; // = parents.size();
-    short parent = 1;
-    vector<short> children;
-};
-
-Node *nodes;
-
-int FindSameParent(int a, int b)
-{
-    int longer;  // has more parents
-    int shorter; // has less parents
-    if (nodes[a].level > nodes[b].level)
-    {
-        longer = a;
-        shorter = b;
-    }
-    else
-    {
-        longer = b;
-        shorter = a;
-    }
-
-    int sParent = nodes[shorter].num;
-    int lParent = nodes[longer].num;
-
-    int cnt = nodes[lParent].level - nodes[sParent].level;
-    for(int i=0 ; i<cnt ; ++i)
-    {
-        lParent = nodes[lParent].parent;
-    }
-
-    while (sParent != lParent)
-    {
-        sParent = nodes[sParent].parent;
-        lParent = nodes[lParent].parent;
-    }
-
-    return lParent;
-}
 
 int main()
 {
     ios::sync_with_stdio(false);
     cin.tie(NULL);
 
-    int N;
-    cin >> N;
+    int N, M;
+    cin >> N >> M;
 
-    Node n[N + 1];
-    nodes = n;
+    int numbers[N];
 
-    for (int i = 0; i <= N; ++i)
+    for(int i=0 ; i<N ; ++i)
     {
-        nodes[i].num = i;
-    }
-
-    for (int i = 1; i < N; ++i)
-    {
-        int p, c, d;
-        cin >> p >> c >> d;
-        nodes[c].dist = d + nodes[p].dist;
-        nodes[c].level = nodes[p].level + 1;
-        nodes[c].parent = p;
-        nodes[p].children.push_back(c);
-    }
-
-    int maxDistNode = 0;
-    for (int i = 1; i <= N; ++i)
-    {
-        if (nodes[maxDistNode].dist < nodes[i].dist)
+        cin >> numbers[i];
+        if(i>0)
         {
-            maxDistNode = i;
+            numbers[i] += numbers[i-1];
         }
     }
 
-    int maximum = 0;
-    for (int i = 1; i <= N; ++i)
+    for(int i=0 ; i<M ; ++i)
     {
-        if (nodes[i].num == nodes[maxDistNode].num)
-            continue;
+        int l, r;
+        cin >> l >> r; l--; r--;
 
-        int distance = nodes[i].dist + nodes[maxDistNode].dist - (2 * nodes[FindSameParent(i, maxDistNode)].dist);
-        maximum = max(maximum, distance);
+        int answer = numbers[r];
+        if(l>0) answer -= numbers[l-1];
+        cout << answer << "\n";
     }
-
-    cout << maximum;
 
     return 0;
 }
