09864c8bad96337cb1eac5d93b89e04dc92bae62
saens <songsh5775@gmail.com>
2022-04-10 17:55:32 +0900

    [C++] BJ 1931
    <반례였던 것>
    3
    1 3
    8 8
    4 8
    answer : 3

--- C++/Practice.cpp
+++ C++/Practice.cpp
@@ -1,76 +1,50 @@
 #include <iostream>
-#include <cmath>
-#define ll unsigned long long int
+#include <algorithm>
 using namespace std;
 
-struct Position
+struct Conference
 {
-    ll x = 0;
-    ll y = 0;
-    Position(ll _x, ll _y)
-    {
-        x = _x;
-        y = _y;
-    }
-    bool operator == (Position& p)
-    {
-        return this->x == p.x && this->y == p.y;
-    }
+    int start = 0;
+    int end = 0;
 };
-
+bool cmp(Conference& c1, Conference& c2)
+{
+    if(c1.end < c2.end)
+    {
+        return true;
+    }
+    else if(c1.end == c2.end)
+    {
+        return c1.start < c2.start;
+    }
+    return false;
+}
 int main()
 {
     ios::sync_with_stdio(false);
     cin.tie(NULL);
 
-    int N, r, c;
-    cin >> N >> r >> c; // row:y좌표, column:x좌표
-    ll pN = pow(2,N);
+    int N; cin >>N;
+    Conference conferences[N];
 
-    Position lt = Position(0, 0);
-    Position rb = Position(pN-1, pN-1);
-
-    ll cnt = 0;
-    ll size = pN;
-
-    while(!(lt == rb))
+    for(int i=0 ; i<N ; ++i)
     {
-        ll size2D;
-        if(size >= 4) size2D = size / 4 * size;
-        else size2D = size * size / 4;
-        // 왼쪽
-        if(lt.x <= c && c <= rb.x - size/2)
+        cin >> conferences[i].start >> conferences[i].end;
+    }
+
+    sort(conferences, conferences + N, cmp);
+
+    int cnt = 1;
+    Conference lastC = conferences[0];
+    for(int i=1 ; i<N ; ++i)
+    {
+        Conference c = conferences[i];
+
+        if(c.start >= lastC.end)
         {
-            rb.x -= size/2;
-            // 위 : 1
-            if(lt.y <= r && r <= rb.y - size/2)
-            {
-                rb.y -= size/2;
-            }
-            // 아래 : 3
-            else
-            {
-                cnt += size2D * 2;
-                lt.y += size/2;
-            }
+            cnt++;
+            lastC = c;
         }
-        else
-        {
-            lt.x += size/2;
-            // 위 : 2
-            if (lt.y <= r && r <= rb.y - size/2)
-            {
-                cnt += size2D * 1;
-                rb.y -= size/2;
-            }
-            // 아래 : 4
-            else
-            {
-                cnt += size2D * 3;
-                lt.y += size/2;
-            }
-        }
-        size /= 2;
     }
 
     cout << cnt;
