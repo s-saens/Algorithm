d1ad34d0c5b5ea5ee73cc448a4e5327e520a567a
saens <songsh5775@gmail.com>
2022-04-10 23:16:18 +0900

    [C++] BJ 12852 1로만들기2

--- C++/Practice.cpp
+++ C++/Practice.cpp
@@ -1,35 +1,68 @@
 #include <iostream>
 #include <set>
+#include <algorithm>
 using namespace std;
+
+int dp[1000001];
+int answer[1000001];
+
+void Init()
+{
+    for(int i=0 ; i<1000001 ; ++i) dp[i] = -1;
+    dp[1] = 0;
+}
+
+int F(int x)
+{
+    if(dp[x] > -1)
+    {
+        return dp[x];
+    }
+
+    int minimum = 1 + F(x-1);
+    answer[x] = x-1;
+    if(x%2 == 0)
+    {
+        int a = 1 + F(x/2);
+        if (a < minimum)
+        {
+            minimum = a;
+            answer[x] = x/2;
+        }
+    }
+    if(x%3 == 0)
+    {
+        int a = 1 + F(x/3);
+        if (a < minimum)
+        {
+            minimum = a;
+            answer[x] = x/3;
+        }
+    }
+
+    dp[x] = minimum;
+    return minimum;
+}
 
 int main()
 {
     ios::sync_with_stdio(false);
     cin.tie(NULL);
 
-    int T; cin >> T;
+    Init();
 
-    for(int t=0 ; t<T ; ++t)
+    int X; cin >> X;
+
+    int m = F(X);
+    cout << m << "\n";
+
+    int k = X;
+    for(int i=0 ; i<m ; ++i)
     {
-        int N; cin >> N;
-        
-        multiset<int> dpq;
-
-        for(int i=0 ; i<N ; ++i)
-        {
-            char op; int num;
-            cin >> op >> num;
-            if (op == 'I') dpq.insert(num);
-            else if (op == 'D' && !dpq.empty())
-            {
-                if(num == 1) dpq.erase(--dpq.end());
-                if(num == -1) dpq.erase(dpq.begin());
-            }
-        }
-
-        if(dpq.empty()) cout << "EMPTY\n";
-        else cout << *(--dpq.end()) << ' ' << *dpq.begin() << "\n";
+        cout << k << ' ';
+        k = answer[k];
     }
+    cout << 1;
 
     return 0;
 }
