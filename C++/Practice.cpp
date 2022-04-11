1da6416c6b7aa97a3e0c1987c1726fc2296ba8b7
saens <songsh5775@gmail.com>
2022-04-11 10:48:20 +0900

    [C++] BJ 1389 케빈베이컨

--- C++/Practice.cpp
+++ C++/Practice.cpp
@@ -1,68 +1,51 @@
 #include <iostream>
-#include <set>
 #include <algorithm>
 using namespace std;
-
-int dp[1000001];
-int answer[1000001];
-
-void Init()
-{
-    for(int i=0 ; i<1000001 ; ++i) dp[i] = -1;
-    dp[1] = 0;
-}
-
-int F(int x)
-{
-    if(dp[x] > -1)
-    {
-        return dp[x];
-    }
-
-    int minimum = 1 + F(x-1);
-    answer[x] = x-1;
-    if(x%2 == 0)
-    {
-        int a = 1 + F(x/2);
-        if (a < minimum)
-        {
-            minimum = a;
-            answer[x] = x/2;
-        }
-    }
-    if(x%3 == 0)
-    {
-        int a = 1 + F(x/3);
-        if (a < minimum)
-        {
-            minimum = a;
-            answer[x] = x/3;
-        }
-    }
-
-    dp[x] = minimum;
-    return minimum;
-}
 
 int main()
 {
     ios::sync_with_stdio(false);
     cin.tie(NULL);
 
-    Init();
+    int N, M;
+    cin >> N >> M;
 
-    int X; cin >> X;
+    int nodes[N][N];
+    for(int i=0 ; i<N ; ++i)
+        for(int j=0 ; j<N ; ++j)
+            nodes[i][j] = i==j ? 0 : 50000;
 
-    int m = F(X);
-    cout << m << "\n";
-
-    int k = X;
-    for(int i=0 ; i<m ; ++i)
+    for(int i=0 ; i<M ; ++i)
     {
-        cout << k << ' ';
-        k = answer[k];
+        int a, b;
+        cin >> a >> b; a--; b--;
+        nodes[a][b] = 1;
+        nodes[b][a] = 1;
     }
-    cout << 1;
+
+    for(int i=0 ; i<N ; ++i)
+        for(int s=0 ; s<N ; ++s)
+            for(int e=0 ; e<N ; ++e)
+                nodes[s][e] = min(nodes[s][e], nodes[s][i] + nodes[i][e]);
+
+    int minimum = 50000;
+    int minIndex = 0;
+    for(int i=N-1 ; i>=0 ; --i)
+    {
+        int sum = 0;
+        for(int j=0 ; j<N ; ++j)
+        {
+            sum += nodes[i][j];
+        }
+        
+        if(minimum >= sum)
+        {
+            minimum = sum;
+            minIndex = i+1;
+        }
+    }
+
+    cout << minIndex;
 
     return 0;
 }
