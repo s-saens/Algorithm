511c4a5e6570b8b0cb876c7ff341d00a1ade3a16
saens <songsh5775@gmail.com>
2022-04-16 16:15:27 +0900

    [C++] BJ 10999 구간합2 (LazyPropagation)
    https://baeharam.github.io/posts/algorithm/lazy-propagation/

--- C++/Practice.cpp
+++ C++/Practice.cpp
@@ -1,57 +1,87 @@
 #include <iostream>
 #include <vector>
+#include <cmath>
 #define ll long long int
 using namespace std;
 
 struct Node
 {
-    ll sum, l, r, cL, cR;
+    ll sum, l, r, cL, cR, lazy;
     Node() {}
     Node(ll _s, ll _l, ll _r, ll _cL, ll _cR)
-    { sum = _s; l = _l; r = _r; cL = _cL; cR = _cR; }
+    {
+        sum = _s;
+        l = _l;
+        r = _r;
+        cL = _cL;
+        cR = _cR;
+        lazy = 0;
+    }
 };
 
-ll* numbers;
-Node* segTree;
+ll *numbers;
+Node *segTree;
+
+void Propagate(ll nI)
+{
+    Node *n = &segTree[nI];
+    Node *nL = &segTree[n->cL];
+    Node *nR = &segTree[n->cR];
+
+    n->sum += (n->r - n->l + 1) * n->lazy;
+
+    if (n->cL != n->cR)
+    {
+        nL->lazy += n->lazy;
+        nR->lazy += n->lazy;
+    }
+    n->lazy = 0;
+}
 
 ll Sum(ll l, ll r, ll nI)
 {
     Node *n = &segTree[nI];
-    if(r < n->l || l > n->r) return 0;
-    if(l <= n->l && r >= n->r) return n->sum;
+    Propagate(nI);
+    if (r < n->l || l > n->r) return 0;
+    if (l <= n->l && r >= n->r) return n->sum;
     return Sum(l, r, n->cL) + Sum(l, r, n->cR);
 }
 
-ll Modify(ll i, ll number, ll nI) // 변화량 반환
+void Modify(ll l, ll r, ll number, ll nI)
 {
     Node *n = &segTree[nI];
-    if(n->l == n->r)
+    Node *nL = &segTree[n->cL];
+    Node *nR = &segTree[n->cR];
+    Propagate(nI);
+    if (r < n->l || l > n->r) return;
+    if (l <= n->l && r >= n->r)
     {
-        ll lastNumber = n->sum;
-        n->sum = number;
-        return number - lastNumber; // 변화량
+        n->sum += (n->r - n->l + 1) * number;
+        if(n->l != n->r)
+        {
+            nL->lazy += number;
+            nR->lazy += number;
+        }
+        return;
     }
+    Modify(l, r, number, n->cL);
+    Modify(l, r, number, n->cR);
 
-    ll nMid = (n->l + n->r)/2;
-    ll delta = 0;
-    if(i > nMid) delta = Modify(i, number, n->cR);
-    else delta = Modify(i, number, n->cL);
-    n->sum += delta;
-    return delta;
+    n->sum = nL->sum + nR->sum;
 }
 
 ll SetSegTree(ll l, ll r, ll nI)
 {
-    if(l == r)
+    if (l == r)
     {
         segTree[nI] = Node(numbers[l], l, r, -1, -1);
         return numbers[l];
     }
 
-    ll mid = (l+r)/2;
-    ll cR = (nI+1)*2;
+    ll mid = (l + r) / 2;
+    ll cR = (nI + 1) * 2;
     ll cL = cR - 1;
-    ll sum = SetSegTree(l, mid, cL) + SetSegTree(mid+1, r, cR);
+    ll sum = SetSegTree(l, mid, cL) + SetSegTree(mid + 1, r, cR);
     segTree[nI] = Node(sum, l, r, cL, cR);
     return segTree[nI].sum;
 }
@@ -59,8 +89,22 @@ ll SetSegTree(ll l, ll r, ll nI)
 ll FindSize(ll N)
 {
     ll ret = 1;
-    while(ret < N) ret *= 2;
+    while (ret < N) ret *= 2;
     return ret * 2;
+}
+
+void PrintSegTree(int treeSize)
+{
+    int level = 1;
+    for(int i=0 ; i<treeSize-1 ; ++i)
+    {
+        cout << segTree[i].sum << ' ';
+        if(i == pow(2, level) - 2)
+        {
+            cout << "\n";
+            level++;
+        }
+    }
 }
 
 int main()
@@ -68,30 +112,34 @@ int main()
     ios::sync_with_stdio(false);
     cin.tie(NULL);
 
-    ll N, M, K; cin >> N >> M >> K;
+    ll N, M, K;
+    cin >> N >> M >> K;
     ll treeSize = FindSize(N);
     numbers = new ll[N];
     segTree = new Node[treeSize];
-    
-    for(int i=0 ; i<N ; ++i) cin >> numbers[i];
 
-    SetSegTree(0, N-1, 0);
+    for (int i = 0; i < N; ++i) cin >> numbers[i];
 
-    while(M > 0 || K > 0)
+    SetSegTree(0, N - 1, 0);
+
+    while (M > 0 || K > 0)
     {
-        short op;
-        ll a, b;
-        cin >> op >> a >> b;
+        short op; cin >> op;
 
-        if(op == 1)
+        if (op == 1)
         {
             M--;
-            Modify(a-1, b, 0);
+            ll l, r, c;
+            cin >> l >> r >> c;
+            Modify(l-1, r-1, c, 0);
+            // PrintSegTree(treeSize);
         }
-        else if(op == 2)
+        else if (op == 2)
         {
             K--;
-            cout << Sum(a-1, b-1, 0) << "\n";
+            ll a, b;
+            cin >> a >> b;
+            cout << Sum(a - 1, b - 1, 0) << "\n";
         }
     }
 
