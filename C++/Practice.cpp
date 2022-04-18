4b846bd9db3540d0ab4b6e6983843262b96db7ac
saens <songsh5775@gmail.com>
2022-04-18 16:36:46 +0900

    [C++] BJ 2822 set container 활용

--- C++/Practice.cpp
+++ C++/Practice.cpp
@@ -1,83 +1,58 @@
 #include <iostream>
-#include <queue>
-#include <vector>
-#include <algorithm>
-#define FOR(i, N) for(int i=0 ; i<N ; ++i)
+#include <set>
 
-#define ll long long int
 using namespace std;
 
-char dx[4] = {-1, 1, 0, 0};
-char dy[4] = {0, 0, -1, 1};
+#define FOR(i, N) for(int i=0 ; i<N ; ++i)
 
-int N;
-
-struct Point
+class Score
 {
-    int x, y;
-    bool h, v;
-    Point() {}
-    Point(int _x, int _y, bool _h, bool _v)
+public:
+    int value = 0;
+    int index = 0;
+
+    Score() {}
+    Score(int v, int i)
     {
-        x = _x ; y = _y ; h = _h ; v = _v;
+        value = v;
+        index = i;
+    }
+
+    bool operator < (const Score &s1) const
+    {
+        return s1.value < this->value;
     }
 };
-
-Point** points;
-
-int BFS(int x, int y)
-{
-    queue<Point> q;
-    q.push(points[y][x]);
-
-    int cnt = 0;
-    while(!q.empty())
-    {
-        Point f = q.front(); q.pop();
-
-        if(points[f.y][f.x].v) continue;
-        points[f.y][f.x].v = true;
-
-        cnt++;
-
-        FOR(i, 4)
-        {
-            int nx = f.x + dx[i];
-            int ny = f.y + dy[i];
-            if(nx < 0 || nx >= N || ny < 0 || ny >= N) continue;
-            if(!points[ny][nx].v && points[ny][nx].h) q.push(points[ny][nx]);
-        }
-    }
-
-    return cnt;
-}
 
 int main()
 {
     ios::sync_with_stdio(false);
     cin.tie(NULL);
 
-    cin >> N;
-    
-    points = new Point*[N];
-    FOR(y, N) points[y] = new Point[N];
-    
-    FOR(y, N)
+    set<Score> scores;
+    set<int> indexes;
+
+    FOR(i, 8)
     {
-        string input;
-        cin >> input;
-        FOR(x, N) points[y][x] = Point(x, y, input[x]-'0', false);
+        int v; cin >> v;
+        scores.insert(Score(v, i));
     }
 
-    vector<int> answers;
+    int sum =0;
+    FOR(i, 5)
+    {
+        Score s = (*scores.begin());
+        sum += s.value;
+        indexes.insert(s.index);
+        scores.erase(scores.begin());
+    }
+    cout << sum << "\n";
 
-    FOR(y, N) FOR(x, N) if(!points[y][x].v && points[y][x].h) answers.push_back(BFS(x, y));
-
-    sort(answers.begin(), answers.end());
-
-    cout << answers.size() << "\n";
-    FOR(i, answers.size()) cout << answers[i] << "\n";
-
+    FOR(i, 5)
+    {
+        cout << *indexes.begin() + 1 << ' ';
+        indexes.erase(indexes.begin());
+    }
 
     return 0;
 }
