6d9760ee2fc46318bb80b509492f921172de56cf
saens <songsh5775@gmail.com>
2022-04-18 15:49:23 +0900

    [C++] BJ 2667 단지번호

--- C++/Practice.cpp
+++ C++/Practice.cpp
@@ -1,80 +1,83 @@
 #include <iostream>
 #include <queue>
+#include <vector>
+#include <algorithm>
+#define FOR(i, N) for(int i=0 ; i<N ; ++i)
 
 #define ll long long int
 using namespace std;
 
-char dX[4] = {-1, 1, 0, 0};
-char dY[4] = {0, 0, -1, 1};
+char dx[4] = {-1, 1, 0, 0};
+char dy[4] = {0, 0, -1, 1};
+
+int N;
 
 struct Point
 {
-    short x, y, d;
+    int x, y;
+    bool h, v;
     Point() {}
-    Point(int _x, int _y, int _d)
+    Point(int _x, int _y, bool _h, bool _v)
     {
-        x = _x;
-        y = _y;
-        d = _d;
+        x = _x ; y = _y ; h = _h ; v = _v;
     }
 };
+
+Point** points;
+
+int BFS(int x, int y)
+{
+    queue<Point> q;
+    q.push(points[y][x]);
+
+    int cnt = 0;
+    while(!q.empty())
+    {
+        Point f = q.front(); q.pop();
+
+        if(points[f.y][f.x].v) continue;
+        points[f.y][f.x].v = true;
+
+        cnt++;
+
+        FOR(i, 4)
+        {
+            int nx = f.x + dx[i];
+            int ny = f.y + dy[i];
+            if(nx < 0 || nx >= N || ny < 0 || ny >= N) continue;
+            if(!points[ny][nx].v && points[ny][nx].h) q.push(points[ny][nx]);
+        }
+    }
+
+    return cnt;
+}
 
 int main()
 {
     ios::sync_with_stdio(false);
     cin.tie(NULL);
 
-    int X, Y;
-    cin >> Y >> X;
-
-    bool p[Y][X];
-    bool v[Y][X];
-
-    for(int y=0 ; y<Y ; ++y)
+    cin >> N;
+    
+    points = new Point*[N];
+    FOR(y, N) points[y] = new Point[N];
+    
+    FOR(y, N)
     {
-        for(int x=0 ; x<X ; ++x)
-        {
-            p[y][x] = false;
-            v[y][x] = false;
-        }
+        string input;
+        cin >> input;
+        FOR(x, N) points[y][x] = Point(x, y, input[x]-'0', false);
     }
 
-    for (int y = 0; y < Y; ++y)
-    {
-        string input; cin >> input;
-        for (int x = 0; x < X; ++x) p[y][x] = input[x] - '0';
-    }
+    vector<int> answers;
 
-    queue<Point> q;
-    q.push(Point(0,0,1));
+    FOR(y, N) FOR(x, N) if(!points[y][x].v && points[y][x].h) answers.push_back(BFS(x, y));
 
-    int d;
+    sort(answers.begin(), answers.end());
 
-    while(!q.empty())
-    {
-        Point f = q.front(); q.pop();
-        if(v[f.y][f.x]) continue;
-        v[f.y][f.x] = true;
-        d = f.d;
+    cout << answers.size() << "\n";
+    FOR(i, answers.size()) cout << answers[i] << "\n";
 
-        if(f.x == X-1 && f.y == Y-1) break;
-
-        for(int i=0 ; i<4 ; ++i)
-        {
-            int nX = f.x + dX[i];
-            int nY = f.y + dY[i];
-
-            if(nX < 0 || nX >= X || nY < 0 || nY >= Y) continue;
-
-
-            if(!v[nY][nX] && p[nY][nX])
-            {
-                q.push(Point(nX, nY, f.d+1));
-            }
-        }
-    }
-
-    cout << d;
 
     return 0;
 }
