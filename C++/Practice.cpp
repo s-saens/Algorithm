04d27f2f3f8c477429554aa2dda5da9b57ea2707
saens <songsh5775@gmail.com>
2022-04-22 16:40:53 +0900

    [C++] BJ 14500 테트로미노 - BF
    재귀호출로는 ㅗ 모양을 제외한 모든 모양을 얻을 수 있다.
    따라서 재귀호출로 다른 모든 모양들에 대해 최댓값 A를 얻고,
    ㅗ 모양에 대한 최댓값 B를 얻어서,
    max(A, B)를 반환하도록 한다.

--- C++/Practice.cpp
+++ C++/Practice.cpp
@@ -1,33 +1,79 @@
 #include <iostream>
 #include <algorithm>
-#define MIN -100000
 
 using namespace std;
 
-int N;
+#define FOR(i, N) for(int i=0 ; i<N ; ++i)
+
+int dx[4] = {-1, 1, 0, 0};
+int dy[4] = {0, 0, -1, 1};
+
+int Y, X;
+
+int** numbers;
+
+int GetFuckingMax(int x, int y)
+{
+    int maxSum = 0;
+    FOR(i, 4)
+    {
+        int sum = numbers[y][x];
+        FOR(j, 3)
+        {
+            int nd = (i+j)%4;
+            int nx = x + dx[nd];
+            int ny = y + dy[nd];
+            if(nx < 0 || nx >= X || ny < 0 || ny >= Y) continue;
+            sum += numbers[ny][nx];
+        }
+        maxSum = max(maxSum, sum);
+    }
+    return maxSum;
+}
+
+int GetMax(int x, int y, int cnt, int lastI)
+{
+    if(cnt == 3) 
+    {
+        return numbers[y][x];
+    }
+    int maximum = 0;
+    FOR(i, 4)
+    {
+        if(lastI%2 == 0 && i == lastI + 1) continue;
+        if(lastI%2 == 1 && i == lastI - 1) continue;
+
+        int nx = x + dx[i];
+        int ny = y + dy[i];
+        if(nx < 0 || nx >= X || ny < 0 || ny >= Y) continue;
+        maximum = max(maximum, numbers[y][x] + GetMax(nx, ny, cnt+1, i));
+    }
+    return maximum;
+}
 
 int main()
 {
     ios::sync_with_stdio(false);
     cin.tie(NULL);
 
-    cin >> N;
+    cin >> Y >> X;
 
-    int number;
-    cin >> number;
+    numbers = new int*[Y];
+    FOR(y, Y) FOR(x, X) numbers[y] = new int[X];
 
-    int seqSum = number;
-    int maxSum = seqSum;
-
-    for(int i=1 ; i<N ; ++i)
+    FOR(y, Y) FOR(x, X)
     {
-        cin >> number;
-        if(seqSum + number < number) seqSum = number;
-        else seqSum += number;
-        maxSum = max(maxSum, seqSum);
+        cin >> numbers[y][x];
     }
 
-    cout << maxSum;
+    int sum = 0;
+    FOR(y, Y) FOR(x, X)
+    {
+        sum = max(sum, GetMax(x, y, 0, -1));
+        sum = max(sum, GetFuckingMax(x, y));
+    }
+
+    cout << sum;
 
     return 0;
 }
