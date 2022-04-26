9f3c2136ee40bb47bc6ebcee407fc2aa30aed736
saens <songsh5775@gmail.com>
2022-04-26 17:12:10 +0900

    [C++] BJ 1708 볼록 껍질 Convex Hull - Graham's Scan

--- C++/Practice.cpp
+++ C++/Practice.cpp
@@ -1,52 +1,56 @@
 #include <iostream>
+#include <cmath>
+#include <algorithm>
+#include <stack>
 #define ll long long
+#define FOR(i, s, e) for(int i=s ; i<e ; ++i)
 
 using namespace std;
 
 struct Vector2
 {
     ll x, y;
-    ll size;
+    double size;
+    double theta;
 
-    Vector2(ll _x, ll _y)
+    Vector2()
+    {
+        x = 500000;
+        y = 500000;
+    }
+
+    Vector2(int _x, int _y)
     {
         x = _x;
         y = _y;
-        size = x*x + y*y;
-    }
-    
-    Vector2 operator - (Vector2 &v2)
-    {
-        return Vector2(x - v2.x, y - v2.y);
+        size = (x * x) + (y * y);
+        theta = x == 0 && y == 0 ? -1 : acos( (double)x / sqrt((double)size) );
     }
 
-    ll operator * (Vector2 &v2)
+    Vector2 operator - (Vector2 v)
     {
-        return (x * v2.x) + (y * v2.y);
+        return Vector2(x - v.x, y - v.y);
     }
-    
-    short operator >> (Vector2 &v2) // 외적한 값의 sign : -1 or 0 or 1
-    {
-        ll cross = (x * v2.y) - (y * v2.x);
 
-        if (cross < 0) return -1;
-        if (cross > 0) return 1;
+    ll operator * (Vector2 v)
+    {
+        return (x * v.x) + (y * v.y);
+    }
+
+    short operator >> (Vector2 v)  // 외적 z값의 sign 반환. 양수이면 왼쪽 벡터에 대해 오른쪽 벡터는 CCW (왼쪽)
+    {
+        ll c = (x * v.y) - (y * v.x);
+        if(c < 0) return -1;
+        if(c > 0) return 1;
         return 0;
-    }
-
-    bool operator <= (Vector2 &v2) // v가 v2의 왼쪽(반시계방향, CCW)에 있는 경우 true 반환
-    {
-        return (v2 >> *this) > 0;
     }
 };
 
-bool PisinL(Vector2 p, Vector2 X, Vector2 Y)
+bool CompareTheta(Vector2& v1, Vector2& v2)
 {
-    Vector2 XY = Y-X;
-    Vector2 XP = p-X;
-    if(XY.size < XP.size) return false;
-    if(XY * XP < 0) return false;
-    return XY >> XP == 0;
+    ll dot = v1 * v2;
+    if(dot * dot == v1.size * v2.size) return v1.size < v2.size;
+    return v1.theta < v2.theta;
 }
 
 int main()
@@ -54,41 +58,53 @@ int main()
     ios::sync_with_stdio(false);
     cin.tie(NULL);
 
-    ll x1, y1, x2, y2, x3, y3, x4, y4;
-    cin >> x1 >> y1 >> x2 >> y2 >> x3 >> y3 >> x4 >> y4;
+    int pointCnt; cin >> pointCnt;
 
-    Vector2 A = Vector2(x1, y1);
-    Vector2 B = Vector2(x2, y2);
-    Vector2 C = Vector2(x3, y3);
-    Vector2 D = Vector2(x4, y4);
+    Vector2 p[pointCnt];
+    Vector2 b = p[0];
 
-    Vector2 AB = B-A;
-    Vector2 AC = C-A;
-    Vector2 AD = D-A;
-    Vector2 CA = A-C;
-    Vector2 CB = B-C;
-    Vector2 CD = D-C;
-
-    int crossProduct = (AB >> AC) * (AB >> AD);
-
-    if (PisinL(C, A, B) || PisinL(D, A, B) || PisinL(A, C, D) || PisinL(B, C, D)) // 선에 포함되는 점이 단 한개라도 있다면 선분이 교차된 것을 의미.
+    FOR(i, 0, pointCnt)
     {
-        cout << 1; return 0;
+        int x, y;
+        cin >> x >> y;
+        p[i] = Vector2(x, y);
+        if(b.y > y) b = p[i];
+        else if(b.y == y && b.x < x) b = p[i];
     }
 
-    if(crossProduct < 0) // "직"선이 교차함.
+    FOR(i, 0, pointCnt) p[i] = p[i] - b;
+
+    sort(p, p+pointCnt, CompareTheta);
+
+    stack<int> s;
+    s.push(0);
+    s.push(1);
+
+    FOR(i, 2, pointCnt)
     {
-        if(AC <= AB) // C가 직선 AB의 왼쪽에 있음. (= D가 직선 AB의 오른쪽에 있음.)
+        while(s.size() > 1)
         {
-            cout << ((CB <= CD) && (CD <= CA)); return 0;
-        }
+            int second = s.top(); s.pop();
+            int first = s.top();
 
-        cout << ((CD <= CB) && (CA <= CD)); return 0;
+            if((p[second] - p[first]) >> (p[i] - p[first]) > 0) // p[i]가 왼쪽에 있으면 첫번째거 다시 집어넣고 break;
+            {
+                s.push(second);
+                break;
+            }
+        }
+        s.push(i);
     }
-    else // 직선 교차 안하면 당연히 선분도 교차 안함. 평행한 경우에도 첫번째 if문에서 안걸러졌으면 어차피 교차 못함.
+
+    int first = s.top(); s.pop();
+    int second = s.top();
+
+    if ((p[second] - p[first]) >> (p[0] - p[first]) != 0)
     {
-        cout << 0; return 0;
+        s.push(first);
     }
+
+    cout << s.size();
 
     return 0;
 }
