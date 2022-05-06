910848a7d694c458be76326838d762d26a386b7b
saens <songsh5775@gmail.com>
2022-05-06 15:29:19 +0900

    [C++] BJ 2467 두 용액 - Two Pointers

--- C++/Practice.cpp
+++ C++/Practice.cpp
@@ -1,52 +1,93 @@
 #include <iostream>
+#include <cmath>
 #include <algorithm>
 #define FOR(i, s, e) for(int i=s ; i<e ; ++i)
+#define ll long long int
 
 using namespace std;
+
+ll minimum = 2147483647;
+ll minL = 0;
+ll minR = 1;
+
+ll* numbers;
+
+ll Min(ll l, ll r)
+{
+    ll sum = numbers[l] + numbers[r];
+    if(abs(minimum) > abs(sum))
+    {
+        minimum = sum;
+        minL = l;
+        minR = r;
+    }
+    return sum;
+}
 
 int main()
 {
     ios::sync_with_stdio(false);
     cin.tie(NULL);
 
-    int MAX = 1000000000;
+    int N; cin >> N;
+    numbers = new ll[N];
 
-    int N, S;
-    cin >> N >> S;
-
-    int sums[N+1];
-    sums[0] = 0;
-
-    cin >> sums[1];
-
-    FOR(i, 2, N+1)
+    ll changingIndex = 0;
+    FOR(i, 0, N)
     {
-        int number;
-        cin >> number;
-        sums[i] = sums[i-1] + number;
+        cin >> numbers[i];
+        if(i > 0 && numbers[i] * numbers[i-1] <= 0) changingIndex = i; // 부호가 바뀌는 index
     }
 
-    int l = 1; // inclusive
-    int r = 1; // inclusive
-
-    int minRL = MAX;
-    
-    while(l <= r && r <= N && l >= 0)
+    if(N == 2)
     {
-        int sum = sums[r] - sums[l-1];
+        cout << numbers[0] << ' ' << numbers[1];
+        return 0;
+    }
 
-        // cout << '>' << l << ',' << r << ':' << sum << " ... " << minRL << "\n";
+    ll l = 0;
+    ll r = 1;
 
-        if(sum < S) r++;
-        else if(sum >= S)
+    if(changingIndex > 1)
+    {
+        l = changingIndex-2;
+        r = changingIndex-1;
+        Min(l, r);
+    }
+    if(changingIndex < N-1)
+    {
+        l = changingIndex;
+        r = changingIndex+1;
+        Min(l, r);
+    }
+    if (changingIndex > 0)
+    {
+        l = changingIndex-1;
+        r = changingIndex;
+        Min(l, r);
+    }
+    Min(0, 1);
+    Min(N-2, N-1);
+
+    while(l > 0 || r < N-1)
+    {
+        int sum = Min(l, r);
+        if(sum > 0)
         {
-            minRL = min(minRL, r - l);
-            l++;
+            if(l > 0) l--;
+            else if(r < N-1) r++;
         }
+        else if(sum < 0)
+        {
+            if(r < N-1) r++;
+            else if(l > 0) l--;
+        }
+        else break;
     }
 
-    if(minRL == MAX) cout << 0;
-    else cout << minRL + 1;
+    Min(l, r);
+
+    cout << numbers[minL] << ' ' << numbers[minR] ;
 
     return 0;
 }
