4e9cd359fa9e8eba1e1578346726eccee05dfaa3
saens <songsh5775@gmail.com>
2022-05-12 17:56:21 +0900

    [C++] BJ 16236 아기상어 - BFS + PriorityQueue

--- C++/Practice.cpp
+++ C++/Practice.cpp
@@ -1,31 +1,42 @@
 #include <iostream>
 #include <cmath>
+#include <queue>
 #include <algorithm>
 #define ll long long int
 #define FOR(i, s, e) for (int i = s; i < e; ++i)
-#define MAX 214748364700
 
 using namespace std;
 
-ll minimum = MAX;
-ll minL = 0;
-ll minC = 1;
-ll minR = 2;
+int dx[4] = {0, -1, 1, 0};
+int dy[4] = {-1, 0, 0, 1};
 
-ll *numbers;
-
-ll Min(ll l, ll c, ll r)
+struct Vector2
 {
-    ll sum = numbers[l] + numbers[r] + numbers[c];
-    if (abs(minimum) > abs(sum))
+    int x, y, t;
+    Vector2() {}
+    Vector2(int _x, int _y, int _t)
     {
-        minimum = sum;
-        minL = l;
-        minC = c;
-        minR = r;
+        x = _x;
+        y = _y;
+        t = _t;
     }
-    return sum;
-}
+};
+
+struct CompareVector2
+{
+    bool operator()(Vector2 &v1, Vector2 &v2)
+    {
+        if(v1.t > v2.t) return true;
+        else if(v1.t < v2.t) return false;
+
+        if (v1.y > v2.y) return true;
+        else if(v1.y < v2.y) return false;
+
+        if(v1.x > v2.x) return true;
+        else if(v1.x < v2.x) return false;
+        return false;
+    }
+};
 
 int main()
 {
@@ -34,45 +45,83 @@ int main()
 
     int N;
     cin >> N;
-    numbers = new ll[N];
 
-    FOR(i, 0, N) cin >> numbers[i];
+    int p[N][N];
+    int passFlag[N][N]; // passFlag < 2 - (nowSize + eatenCnt)
 
-    sort(numbers, numbers+N);
-
-    if (N == 3)
+    Vector2 start;
+    FOR(y, 0, N) FOR(x, 0, N)
     {
-        cout << numbers[0] << ' ' << numbers[1] << ' ' << numbers[2];
-        return 0;
+        passFlag[y][x] = 0;
+        cin >> p[y][x];
+        if(p[y][x] == 9) start = Vector2(x, y, 0);
     }
 
-    FOR(i, 1, N-1)
+    priority_queue<Vector2, vector<Vector2>, CompareVector2> q;
+    q.push(start);
+
+    passFlag[start.y][start.x] = -1;
+    p[start.y][start.x] = 0;
+
+    int nowSize = 2;
+    int eatenCnt = 0;
+    int totalEatenCnt = 0;
+
+    int time = 0;
+
+    while(true)
     {
-        ll c = i;
-        ll l = i-1;
-        ll r = i+1;
-        while (l > 0 || r < N - 1)
+        priority_queue<Vector2, vector<Vector2>, CompareVector2> fishes;
+
+        while(!q.empty())
         {
-            int sum = Min(l, c, r);
-            if (sum > 0)
+            Vector2 now = q.top(); q.pop();
+            int x = now.x;
+            int y = now.y;
+
+            FOR(i, 0, 4)
             {
-                if (l > 0) l--;
-                else if (r < N - 1) r++;
+                int nx = x + dx[i];
+                int ny = y + dy[i];
+
+                if(nx < 0 || nx >= N || ny < 0 || ny >= N || p[ny][nx] > nowSize) continue;
+                if(passFlag[ny][nx] < 2 - (nowSize + totalEatenCnt)) continue;
+
+                passFlag[ny][nx] = 1 - (nowSize + totalEatenCnt);
+
+                if(p[ny][nx] == 0 || p[ny][nx] == nowSize) // 지나가기!
+                {
+                    if(fishes.empty()) q.push(Vector2(nx, ny, now.t + 1));
+                }
+                else if(p[ny][nx] < nowSize) // 잡아먹기 후보 추가
+                {
+                    fishes.push(Vector2(nx, ny, now.t + 1));
+                }
             }
-            else if (sum < 0)
-            {
-                if (r < N - 1) r++;
-                else if (l > 0) l--;
-            }
-            else break;
         }
-        Min(l, c, r);
+
+        if(!fishes.empty())
+        {
+            Vector2 fish = fishes.top();
+            q.push(fish);
+
+            eatenCnt++;
+            totalEatenCnt++;
+            if (eatenCnt >= nowSize) // Size 갱신
+            {
+                eatenCnt = 0;
+                nowSize++;
+            }
+
+            passFlag[fish.y][fish.x]--;
+            p[fish.y][fish.x] = 0;
+
+            time = fish.t;
+        }
+        else break;
     }
 
-    ll answers[3] = {numbers[minL], numbers[minC], numbers[minR]};
-    sort(answers, answers+3);
-
-    FOR(i, 0, 3) cout << answers[i] << ' ';
+    cout << time;
 
     return 0;
 }
