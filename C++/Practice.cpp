fd0dc17be168b63b82d7b78c70fc3338ec39aecf
saens <songsh5775@gmail.com>
2022-05-22 01:34:30 +0900

    [C++] BJ 9655 - DP
    DP로 풀긴 했지만 간단한 홀짝 출력 프로그램도 AC 뜸

--- C++/Practice.cpp
+++ C++/Practice.cpp
@@ -1,34 +1,32 @@
 #include <iostream>
-#include <queue>
-#include <cmath>
-#include <vector>
-
-#define ll long long int
 #define FOR(i, s, e) for(int i=s ; i<e ; ++i)
 
 using namespace std;
 
-struct V2
+short result[1001]; // SK가 이기면 1, 아니면 2
+
+void Init()
 {
-    ll x = 0;
-    ll y = 0;
-
-    V2 operator-(V2 v)
+    FOR(i, 0, 1001)
     {
-        V2 newV;
-        newV.x = x - v.x;
-        newV.y = y - v.y;
-        return newV;
+        result[i] = 0;
     }
+}
 
-    short operator >> (V2 v) // 외적
-    {
-        ll c = (x * v.y) - (y * v.x);
-        if(c < 0) return -1;
-        if(c > 0) return 1;
-        else return 0;
-    }
-};
+bool Solve(int N, bool skTurn)
+{
+    if(result[N] > 0) return result[N] == 1;
+    bool nextTurn = !skTurn;
+
+    // 기저
+    if(N == 1 || N == 3) return skTurn;
+    if(N == 2) return !skTurn;
+
+    bool answer = (Solve(N - 1, !skTurn) || Solve(N - 3, !skTurn));
+    if(answer) result[N] = 1;
+    else result[N] = 2;
+    return answer;
+}
 
 int main()
 {
@@ -36,13 +34,11 @@ int main()
     cin.tie(NULL);
     cout.tie(NULL);
 
-    V2 P[3];
-    FOR(i, 0, 3)
-    {
-        cin >> P[i].x >> P[i].y;
-    }
+    int N;
+    cin >> N;
 
-    cout << ((P[1] - P[0]) >> (P[2] - P[1])) << "\n";
+    bool answer = Solve(N, true);
+    cout << (answer ? "SK" : "CY");
 
     return 0;
 }
