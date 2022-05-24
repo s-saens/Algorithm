47eda030c3ddfd5792c04337bf0b5a970d9babfd
saens <songsh5775@gmail.com>
2022-05-24 18:12:21 +0900

    [C++] BJ 9466 텀프로젝트 - DFS

--- C++/Practice.cpp
+++ C++/Practice.cpp
@@ -4,18 +4,57 @@
 
 using namespace std;
 
+int* nexts;
+bool* visited;
+bool* done;
+
+int cnt = 0;
+
+void Set(int n)
+{
+    visited[n] = true;
+    int next = nexts[n];
+    
+    if(!visited[next]) Set(next);
+    else if(!done[next])
+    {
+        for(int i=next ; i!=n ; i=nexts[i]) cnt++;
+        cnt++;
+    }
+
+    done[n] = true;
+}
+
 int main()
 {
     ios::sync_with_stdio(false);
     cin.tie(NULL);
-    cout.tie(NULL);
 
-    int N; cin >> N;
-    int numbers[N];
-    FOR(i, 0, N) cin >> numbers[i];
-    sort(numbers, numbers+N);
+    int T; cin >> T;
+    int answers[T];
 
-    FOR(i, 0, N) cout << numbers[N-1-i] << "\n";
+    FOR(t, 0, T)
+    {
+        cnt = 0;
+        int N; cin >> N;
+        nexts = new int[N];
+        visited = new bool[N];
+        done = new bool[N];
+
+        FOR(i, 0, N)
+        {
+            int n; cin >> n;
+            nexts[i] = n - 1;
+            visited[i] = 0;
+            done[i] = 0;
+        }
+
+        FOR(i, 0, N) if(!visited[i]) Set(i);
+
+        answers[t] = N - cnt;
+    }
+
+    FOR(t, 0, T) cout << answers[t] << "\n";
 
 
     return 0;
