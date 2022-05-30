6b328cf47c9d7082aea1db226f02d74ba60ef676
saens <songsh5775@gmail.com>
2022-05-30 11:00:29 +0900

    [C++] BJ 17114 하이퍼토마토 숏코딩

--- C++/Practice.cpp
+++ C++/Practice.cpp
@@ -1,63 +1,7 @@
 #include <iostream>
-#include <vector>
 #include <queue>
-
-#define FOR(i, s, e) for(int i=s ; i<e ; ++i)
-
+#include <cmath>
 using namespace std;
-
-struct Node 
-{
-    vector<int> nexts;
-    int inCnt = 0;
-};
-
-Node* nodes;
-
-void Connect(int a, int b)
-{
-    nodes[a].nexts.push_back(b);
-    nodes[b].inCnt++;
-}
-
-int main()
-{
-    ios::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-
-    int N, M; cin >> N >> M;
-    nodes = new Node[N];
-
-    int startNode = 0;
-
-    FOR(i, 0, M) 
-    {
-        int a, b; cin >> a >> b;
-        a--; b--;
-
-        Connect(a, b);
-    }
-
-    priority_queue<int, vector<int>, greater<int> > pq;
-    vector<int> answers;
-
-    FOR(i, 0, N) if (nodes[i].inCnt == 0) pq.push(i);
-
-    while(!pq.empty())
-    {
-        int now = pq.top(); pq.pop();
-        answers.push_back(now+1);
-
-        FOR(i, 0, nodes[now].nexts.size())
-        {
-            int next = nodes[now].nexts[i];
-            nodes[next].inCnt--;
-            if(nodes[next].inCnt == 0) pq.push(next);
-        }
-    }
-
-    FOR(i, 0, N) cout << answers[i] << ' ';
-
-    return 0;
-}
+#define F(i,N) for(int i=0;i<N;++i)
+struct T{int l=0,s=0,d=0;};
+int main(){int p=11,r=0,e=0,t=1,d=0,s[p];F(i,p){cin>>s[i];t*=s[i];}int w[p * 2];F(i,p){int m=1;F(j,i)m*=s[j];w[i*2]=m;w[i*2+1]=-m;}T y[t];queue<T> q;F(i,t){cin >> y[i].s;y[i].l=i;if(y[i].s==1) q.push(y[i]);else if(y[i].s == -1)e++;}while(!q.empty()){T f=q.front();q.pop();r++;d=f.d;F(i,p*2){bool n=i%2;int c=f.l+w[i],v=abs(w[i]),b=v*s[i/2],x=f.l%b,z=c%b;if(c<0||c>=t||abs(x-z)!=v||n&&z>x||!n&&z<x)continue;if(y[c].s == 0){y[c].s = 1;y[c].d = f.d + 1;q.push(y[c]);}}}if(r==t-e&&r>0)cout<<d;else cout<<-1;return 0;}
