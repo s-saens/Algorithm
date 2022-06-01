46402c6e8cc739b5b769080917f51fc34d314a45
saens <songsh5775@gmail.com>
2022-06-01 16:26:28 +0900

    [C++] BJ 1644 소수의연속합 - TP

--- C++/Practice.cpp
+++ C++/Practice.cpp
@@ -1,11 +1,30 @@
 #include <iostream>
-#include <algorithm>
 #include <cstring>
+#include <vector>
+#include <cmath>
 using namespace std;
-#define FOR(i, s, e) for(int i=s ; i<e ; ++i)
-#define FOR_R(i, s, e) for(int i=s ; i>=e ; --i) // both inclusive
 
-int N, M;
+#define FOR(i,s,e) for(int i=s; i<e; ++i)
+#define ll long long int
+
+int N;
+vector<int> primes;
+
+const int MAX = 4000000;
+bool visited[MAX+1];
+
+int l, r, cnt;
+ll sum=2;
+void L()
+{
+    sum -= primes[l];
+    l++;
+}
+void R()
+{
+    r++;
+    sum += primes[r];
+}
 
 int main()
 {
@@ -13,26 +32,41 @@ int main()
     cin.tie(NULL);
     cout.tie(NULL);
 
-    cin >> N >> M;
-    int memories[N];
-    int costs[N];
+    cin >> N;
+    memset(visited, 0, sizeof(visited));
 
-    int maxCostSum = 0;
-    FOR(i, 0, N) cin >> memories[i];
-    FOR(i, 0, N)
+    visited[0] = 1;
+    visited[1] = 1;
+    visited[2] = 0;
+    visited[3] = 0;
+
+    FOR(i, 2, MAX)
     {
-        cin >> costs[i];
-        maxCostSum += costs[i];
+        if(!visited[i])
+        {
+            visited[i] = 1;
+            primes.push_back(i);
+            for(int j=i+i ; j<=MAX ; j += i) visited[j] = 1;
+        }
     }
-    int maxMemories[maxCostSum+1];
-    memset(maxMemories, 0, sizeof(maxMemories));
 
-    FOR(i, 0, N) FOR_R(j, maxCostSum, costs[i])
-        maxMemories[j] = max(maxMemories[j], maxMemories[j-costs[i]] + memories[i]);
+    int s = primes.size();
 
-    FOR(i, 0, maxCostSum+1) if(maxMemories[i] >= M)
+    l = 0;
+    r = 0;
+    cnt = 0;
+
+    while(r < s && l <= r)
     {
-        cout << i;
-        return 0;
+        if(sum == N) { cnt++; R(); }
+        else if(sum < N)
+        {
+            if(r < s-1) R();
+            else L();
+        }
+        else if(sum > N) L();
     }
+
+    cout << cnt;
+    
 }
