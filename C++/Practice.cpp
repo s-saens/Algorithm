83330582a9b7da38edd88ee5ffbe81cf13d3c52d
saens <songsh5775@gmail.com>
2022-06-10 15:20:42 +0900

    [C++] BJ 1916 최소비용구하기 - 틀린 풀이

--- C++/Practice.cpp
+++ C++/Practice.cpp
@@ -1,48 +1,79 @@
 #include <iostream>
 #include <queue>
+#include <vector>
 using namespace std;
-#define ll long long
+#define INF 2000000002;
+#define pq priority_queue<Bus, vector<Bus>, CompareBus>
 
-queue<pair<int, int> > st;
-
-int F(int w, int s, int e)
+int N, M, S, E;
+struct Bus
 {
-    if (w == 1)
+    int to, cost;
+    Bus(int t, int c)
     {
-        st.push(make_pair(s, e));
-        return 1;
+        to = t;
+        cost = c;
     }
-
-    int r = 6-s-e;
-    return F(w-1, s, r) + F(1, s, e) + F(w-1, r, e);
-}
-
-ll f(int w)
+};
+struct CompareBus
 {
-    if(w == 1) return 1;
-    return 2 * f(w-1) + 1;
-}
+    bool operator() (Bus& b1, Bus& b2)
+    {
+        return b1.cost > b2.cost;
+    }
+};
+struct City
+{
+    int minumumCost = INF; // from start
+    pq buses;
+};
+
+City* cities;
+pq q;
 
 int main()
 {
     ios::sync_with_stdio(false);
     cin.tie(NULL);
 
-    int N; cin >> N;
-
-    if(N > 20)
+    cin >> N;
+    if(N == 1)
     {
-        cout << f(N);
+        cout << 0;
         return 0;
     }
+    cin >> M;
+    cities = new City[N];
 
-    cout << F(N, 1, 3) << '\n';
-
-    while(!st.empty())
+    for (int i = 0; i < M; ++i)
     {
-        pair<int, int> t = st.front(); st.pop();
-        cout << t.first << ' ' << t.second << '\n';
+        int a, b, c; cin >> a >> b >> c; a--; b--;
+        cities[a].buses.push(Bus(b, c));
     }
+
+    cin >> S >> E; S--; E--;
+    q.push(Bus(S, 0));
+    cities[S].minumumCost = 0;
+
+    while(!q.empty())
+    {
+        Bus bus = q.top(); q.pop();
+        City* city = cities + bus.to;
+
+        while(!city->buses.empty())
+        {
+            Bus nextBus = city->buses.top(); city->buses.pop();
+            City* nextCity = cities + nextBus.to;
+            int w = city->minumumCost + nextBus.cost;
+
+            if(w >= nextCity->minumumCost) continue;
+
+            nextCity->minumumCost = w;
+            q.push(nextBus);
+        }
+    }
+
+    cout << cities[E].minumumCost;
 
     return 0;
 }
