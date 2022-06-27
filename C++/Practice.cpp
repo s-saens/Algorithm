d16fa4e9160d9a9fe20e6b84e4946ab7b49a2470
saens <songsh5775@gmail.com>
2022-06-27 18:56:53 +0900

    [C++] BJ 16637 괄호추가하기 - 재귀

--- C++/Practice.cpp
+++ C++/Practice.cpp
@@ -1,4 +1,42 @@
 #include <iostream>
-#include <queue>
+#include <algorithm>
 #define FOR(i,s,e) for(int i=s;i<e;++i)
-using namespace std;struct T{int i=0,s=0,w=0;};int main(){int r=0,e=0,a=1,p=0,s[11],d[11]={1};FOR(i,0,11){cin>>s[i];a*=s[i];}FOR(i,1,11)d[i]=s[i-1]*d[i-1];T t[a];queue<T> q;FOR(i,0,a){cin>>t[i].s;t[i].i=i;if(t[i].s==1)q.push(t[i]);else if(t[i].s==-1)e++;}while(!q.empty()){T f=q.front();q.pop();r++;p=f.w;FOR(i,0,22){int m=i%2,n=f.i+d[i/2]*(1-m*2),v=d[i/2]*s[i/2],x=f.i%v,y=n%v;if(n<0||n>=a||m&&y>=x||!m&&y<=x)continue;if(t[n].s==0){t[n].s=1;t[n].w=f.w+1;q.push(t[n]);}}}if(r==a-e&&r>0)cout<<p;else cout<<-1;return 0;}
+#define MIN -2147483648
+using namespace std;
+
+int N;
+string ex;
+int answer;
+
+int cal(int a, char op, int b)
+{
+    switch (op)
+    {
+        case '+': return a + b;
+        case '-': return a - b;
+        case '*': return a * b;
+    }
+}
+
+void F(int i, int lastResult)
+{
+    if(i >= N-1)
+    {
+        answer = max(answer, lastResult);
+        return;
+    }
+    if(i+4 < N) F(  i+4, cal( lastResult, ex[i+1], cal(ex[i+2]-'0', ex[i+3], ex[i+4]-'0') )  );
+    if(i+2 < N) F(  i+2, cal( lastResult, ex[i+1], ex[i+2]-'0')  );
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    answer = MIN;
+    cin >> N >> ex;
+    F(0, ex[0]-'0');
+
+    cout << answer;
+}
