59234ccd64e673f704d62e821f2fd89baf6f5ccf
saens <songsh5775@gmail.com>
2022-06-29 13:24:40 +0900

    [C++] BJ 13458 시험감독

--- C++/Practice.cpp
+++ C++/Practice.cpp
@@ -1,57 +1,34 @@
 #include <iostream>
-#include <algorithm>
 #define FOR(i,s,e) for(int i=s;i<e;++i)
-#define MIN -2147483648
+#define ll long long int
 using namespace std;
-
-int cnt = 0;
-int N;
-bool** isWall;
-int dX[3] = {1, 0, 1};
-int dY[3] = {0, 1, 1};
-
-/*
-dir
-0: →
-1: ↓
-2: ↘︎
-*/
-void DFS(int x, int y, int lastDir, int c)
-{
-    if (x == N-1 && y == N-1) { cnt++; return; }
-
-    FOR(i, 0, 3)
-    {
-        int nx = x + dX[i];
-        int ny = y + dY[i];
-        if((lastDir * i == 0 && lastDir + i == 1)
-        || (nx >= N)
-        || (ny >= N)
-        || (isWall[ny][nx])
-        ) continue;
-
-        if( i == 2 && (isWall[ny][x] || isWall[y][nx]) )
-        {
-            continue;
-        }
-
-        DFS(nx, ny, i, c+1);
-    }
-}
 
 int main()
 {
     ios::sync_with_stdio(false);
     cin.tie(NULL);
 
+    ll N, main, sub, cnt = 0;
     cin >> N;
-    isWall = new bool*[N];
-    FOR(i,0,N) isWall[i] = new bool[N];
 
+    ll testers[N];
 
-    FOR(y, 0, N) FOR(x, 0, N) cin >> isWall[y][x];
+    FOR(i, 0, N) cin >> testers[i];
 
-    DFS(1, 0, 0, 0);
+    cin >> main >> sub;
+
+    FOR(i, 0, N)
+    {
+        testers[i] -= main;
+        cnt++;
+        if(testers[i] > 0)
+        {
+            cnt += testers[i] / sub;
+            if(testers[i] % sub > 0) cnt++;
+        }
+    }
 
     cout << cnt;
+
+    return 0;
 }
