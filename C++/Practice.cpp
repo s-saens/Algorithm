75de9511727c2bed4e50f2838f14a65a47be847b
saens <songsh5775@gmail.com>
2022-07-12 18:56:04 +0900

    [C++] BJ 2206 벽부수고이동하기 - BFS
    1h

--- C++/Practice.cpp
+++ C++/Practice.cpp
@@ -1,45 +1,97 @@
 #include <iostream>
-#include <algorithm>
+#include <queue>
 #define FOR(i, s, e) for(int i=s ; i<e ; ++i)
-#define INF 20000000000
-#define ll long long
 using namespace std;
+
+int dx[4] = {0, 0, 1, -1};
+int dy[4] = {-1, 1, 0, 0};
+
+struct Snapshot
+{
+    int x, y;
+    int dist = 1;
+    bool destroy = false;
+
+    Snapshot(int _x, int _y)
+    {
+        x = _x;
+        y = _y;
+    }
+};
 
 int main()
 {
     ios::sync_with_stdio(false);
     cin.tie(NULL);
 
-    int N, M;
-    cin >> N >> M;
-    ll weights[N][N];
-    FOR(i, 0, N) FOR(j, 0, N)
+    int Y, X; cin >> Y >> X;
+
+    if(Y == 1 && X == 1)
     {
-        weights[i][j] = i == j ? 0 : INF;
+        cout << 1;
+        return 0;
     }
 
-    FOR(i, 0, M)
+    bool blocked[Y][X];
+    bool visited[Y][X];
+    bool visited_crashed[Y][X];
+
+    FOR(y, 0, Y)
     {
-        int f, t; ll w;
-        cin >> f >> t >> w;
-        f--; t--;
-
-        weights[f][t] = min(weights[f][t], w);
-    }
-
-    FOR(k, 0, N) FOR(i, 0, N) FOR(j, 0, N) weights[i][j] = min(weights[i][j], weights[i][k] + weights[k][j]);
-
-
-    FOR(i, 0, N)
-    {
-        FOR(j, 0, N)
+        string input;
+        cin >> input;
+        FOR(x, 0, X)
         {
-            if(weights[i][j] == INF) weights[i][j] = 0;
-            cout << weights[i][j] << ' ';
+            blocked[y][x] = input[x]-'0';
+            visited[y][x] = false;
+            visited_crashed[y][x] = false;
         }
-        cout << '\n';
     }
 
+    queue<Snapshot> q;
+    q.push(Snapshot(0, 0));
+
+    while(!q.empty())
+    {
+        Snapshot f = q.front(); q.pop();
+        if(!f.destroy) visited[f.y][f.x] = true;
+        if(f.destroy) visited_crashed[f.y][f.x] = true;
+
+        FOR(i, 0, 4)
+        {
+            int nx = f.x + dx[i];
+            int ny = f.y + dy[i];
+
+            if(ny < 0 || ny >= Y || nx < 0 || nx >= X) continue;
+
+            if(nx == X-1 && ny == Y-1)
+            {
+                cout << f.dist + 1;
+                return 0;
+            }
+
+            
+            if(!f.destroy && visited[ny][nx]) continue;
+            if(f.destroy && visited_crashed[ny][nx]) continue;
+
+            Snapshot snapshot = Snapshot(nx, ny);
+            snapshot.destroy = f.destroy;
+
+            if(blocked[ny][nx])
+            {
+                snapshot.destroy = true;
+                if(f.destroy) continue;
+            }
+            snapshot.dist = f.dist + 1;
+
+            if(!snapshot.destroy) visited[ny][nx] = true;
+            if(snapshot.destroy) visited_crashed[ny][nx] = true;
+
+            q.push(snapshot);
+        }
+    }
+
+    cout << -1;
 
     return 0;
 }
