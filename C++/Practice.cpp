b082b5f0f8125accc0b7a6756c1a7d863aceda27
saens <songsh5775@gmail.com>
2022-07-18 17:26:31 +0900

    [C++] BJ 7562 섬의개수 - DFS

--- C++/Practice.cpp
+++ C++/Practice.cpp
@@ -1,20 +1,26 @@
 #include <iostream>
 #include <cmath>
+#include <vector>
 #define FOR(i, s, e) for(int i=s ; i<e ; ++i)
 using namespace std;
 
-int cnt = 0, N, *xOnY;
+bool** island;
+bool** visited;
 
-bool isValid(int y)
-{
-    FOR(i, 0, y) if(xOnY[y] == xOnY[i] || y - i == abs(xOnY[y] - xOnY[i])) return false;
-    return true;
-}
+int X=1, Y=1;
+int dx[8] = {0, 0, -1, 1, 1, -1, 1, -1};
+int dy[8] = {-1, 1, 0, 0, 1, -1, -1, 1};
 
-void find(int y)
+void DFS(int x, int y)
 {
-    if(y == N) { cnt++; return; }
-    FOR(i, 0, N) { xOnY[y] = i; if(isValid(y)) find(y+1); }
+    visited[y][x] = 1;
+    FOR(i, 0, 8)
+    {
+        int nx = x + dx[i];
+        int ny = y + dy[i];
+        if(nx < 0 || ny < 0 || nx >= X || ny >= Y || !island[ny][nx] || visited[ny][nx]) continue;
+        DFS(nx, ny);
+    }
 }
 
 int main()
@@ -22,12 +28,38 @@ int main()
     ios::sync_with_stdio(false);
     cin.tie(NULL);
 
-    cin >> N;
-    xOnY = new int[N];
+    cin >> X >> Y;
+    vector<int> answers;
 
-    find(0);
+    while(X != 0 && Y != 0)
+    {
+        int cnt = 0;
+        island = new bool*[Y];
+        visited = new bool*[Y];
+        FOR(y, 0, Y)
+        {
+            island[y] = new bool[X];
+            visited[y] = new bool[X];
+            FOR(x, 0, X) visited[y][x] = 0;
+        }
 
-    cout << cnt;
+        FOR(y, 0, Y) FOR(x, 0, X) cin >> island[y][x];
+
+        FOR(y, 0, Y) FOR(x, 0, X)
+        {
+            if(island[y][x] && !visited[y][x])
+            {
+                DFS(x, y);
+                cnt++;
+            }
+        }
+
+        answers.push_back(cnt);
+
+        cin >> X >> Y;
+    }
+
+    FOR(i, 0, answers.size()) cout << answers[i] << '\n';
 
     return 0;
 }
