73d8e0830ffb3cc052e3a284718b5cad1b09fccc
saens <songsh5775@gmail.com>
2022-07-26 18:43:59 +0900

    [C++] BJ 16931 - Geometry / short code

--- C++/Practice.cpp
+++ C++/Practice.cpp
@@ -1,65 +1,19 @@
 #include <iostream>
-#include <queue>
-#define FOR(i,s,e) for(int i=s;i<e;++i)
-#define MAX 100002
+#include <cmath>
+#define FOR(i, e) for (int i = 0; i < e; ++i)
 using namespace std;
-
-struct Snapshot
-{
-    int x, t=0;
-    Snapshot(int _x, int _t)
-    {
-        x = _x; t = _t;
-    }
-};
-
-int N, K;
-bool visited[MAX] = { 0 };
-short dx[3] = {1, -1, 2};
-
-
 int main()
 {
-    ios::sync_with_stdio(false);
-    cin.tie(NULL);
-
-    cin >> N >> K;
-
-    if(N >= K)
+    int X, Y, s = 0; cin >> Y >> X;
+    int Z[Y][X];
+    FOR(y, Y)FOR(x, X)
     {
-        cout << N-K << '\n' << 1;
-        return 0;
+        int n; cin >> n;
+        Z[y][x] = n;
+        s += 4 * n + 2;
+        if (y > 0) s -= min(Z[y - 1][x], n) * 2;
+        if (x > 0) s -= min(Z[y][x - 1], n) * 2;
     }
-
-    int minTime = MAX;
-    int answerCnt = 0;
-
-    queue<Snapshot> q;
-    q.push(Snapshot(N, 0));
-
-    while(!q.empty())
-    {
-        Snapshot s = q.front(); q.pop();
-        if(answerCnt > 0 && minTime <= s.t) break;
-        visited[s.x] = 1;
-
-        FOR(i, 0, 3)
-        {
-            int nx = i<2 ? s.x + dx[i] : s.x * 2;
-            if(nx == K)
-            {
-                answerCnt++;
-                minTime = s.t + 1;
-                continue;
-            }
-
-            if(nx < 0 || nx >= MAX || visited[nx]) continue;
-
-            q.push(Snapshot(nx, s.t+1));
-        }
-    }
-
-    cout << minTime << '\n' << answerCnt;
-
+    cout << s;
     return 0;
 }
