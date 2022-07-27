40bd99c989cb288197af84557442a23ee25e523f
saens <songsh5775@gmail.com>
2022-07-27 10:49:24 +0900

    [C++] BJ 1002 터렛

--- C++/Practice.cpp
+++ C++/Practice.cpp
@@ -1,19 +1,41 @@
 #include <iostream>
 #include <cmath>
-#define FOR(i, e) for (int i = 0; i < e; ++i)
+#define FOR(i, e) for (int i=0; i<e; ++i)
 using namespace std;
+
+int _x1, _y1, r1, _x2, _y2, _r2;
+
+int Solve()
+{
+    if(_x1==_x2 && _y1==_y2 && r1==_r2) return -1;
+
+    int dx=_x2-_x1, dy=_y2-_y1, dd=dx*dx+dy*dy, rp=_r2+r1, rn=_r2-r1;
+    rp*=rp; rn*=rn;
+    if(rp == dd) return 1; // 외접
+    if(rp < dd) return 0; // 원 밖에서 안만남
+
+    double minR=(double)min(r1, _r2), maxR=(double)max(r1, _r2);
+    double d = sqrt(dd);
+    if(d + minR == maxR) return 1;
+    if(d + minR < maxR) return 0;
+
+    return 2;
+}
+
 int main()
 {
-    int X, Y, s = 0; cin >> Y >> X;
-    int Z[Y][X];
-    FOR(y, Y)FOR(x, X)
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int T; cin >> T;
+    short answers[T];
+    FOR(t, T)
     {
-        int n; cin >> n;
-        Z[y][x] = n;
-        s += 4 * n + 2;
-        if (y > 0) s -= min(Z[y - 1][x], n) * 2;
-        if (x > 0) s -= min(Z[y][x - 1], n) * 2;
+        cin >> _x1 >> _y1 >> r1 >> _x2 >> _y2 >> _r2;
+        answers[t] = Solve();
     }
-    cout << s;
+
+    FOR(t, T) cout << answers[t] << '\n';
+
     return 0;
 }
