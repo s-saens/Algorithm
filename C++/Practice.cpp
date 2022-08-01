00cfce93880fb7407dd7bbb1e998e53936a6a8dd
saens <songsh5775@gmail.com>
2022-08-01 22:35:16 +0900

    [C++] BJ 14507 숏코딩

--- C++/Practice.cpp
+++ C++/Practice.cpp
@@ -1,15 +1,3 @@
-#include <iostream>
-#define k(i) t[n].c[i]
-using namespace std;
-struct P{int c[2];};
-int main()
-{
-    long long N,K,n=0;
-    P t[200001];
-    cin>>N;for(int n;n<N;++n){cin>>k(0)>>k(1);k(0)--;k(1)--;}cin>>K;K--;
-    while(k(0)>-2||k(1)>-2)
-        if(k(0)==-2)n=k(1);
-        else if(k(1)==-2)n=k(0);
-        else n=k(K%2),K=K&1?K/2:K-K/2;
-    cout<<n+1;
-}
+#include<iostream>
+#define k(i)t[n].c[i]
+using namespace std;struct P{int c[2];};int main(){long long N,K,n=1;P t[200001];cin>>N;for(int n=1;n<=N;++n){cin>>k(0)>>k(1);}cin>>K;while(k(0)>-1||k(1)>-1)if(k(0)==-1)n=k(1);else if(k(1)==-1)n=k(0);else n=k(1-K&1),K=K&1?K/2+1:K/2;cout<<n;}
