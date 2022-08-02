2817d76a16a093aaa4b9e1da34f1082c670746a8
saens <songsh5775@gmail.com>
2022-08-02 10:51:54 +0900

    [C++] BJ 2533 사회망서비스(SNS) - DP

--- C++/Practice.cpp
+++ C++/Practice.cpp
@@ -1,3 +1,56 @@
-#include<iostream>
-#define k(i)t[n].c[i]
-using namespace std;struct P{int c[2];};int main(){long long N,K,n=1;P t[200001];cin>>N;for(int n=1;n<=N;++n){cin>>k(0)>>k(1);}cin>>K;while(k(0)>-1||k(1)>-1)if(k(0)==-1)n=k(1);else if(k(1)==-1)n=k(0);else n=k(1-K&1),K=K&1?K/2+1:K/2;cout<<n;}
+#include <iostream>
+#include <vector>
+#include <cmath>
+#define FOR(i,e)for(int i=0;i<e;++i)
+#define MAX 1000000
+using namespace std;
+
+// dp[x][1] = x가 일반인이면? 모든 친구들이 EA여야 함. = dp[친구][0] 들의 합
+// dp[x][0] = x가 EA면? = (dp[연결][0], dp[친구][1]) 중 작은거 들의 합
+
+struct Node
+{
+    bool v = 0;
+    vector<int> links;
+};
+
+int dp[MAX][2], N, u, v;
+Node tree[MAX];
+
+void Input()
+{
+    cin >> N;
+    FOR(i,N-1)
+    {
+        cin >> u >> v; u--; v--;
+        tree[u].links.push_back(v);
+        tree[v].links.push_back(u);
+    }
+}
+
+void Find(int x)
+{
+    tree[x].v = true;
+    dp[x][0] = 1;
+    FOR(i, tree[x].links.size())
+    {
+        int c = tree[x].links[i];
+        if(tree[c].v) continue;
+        Find(c);
+        dp[x][1] += dp[c][0];
+        dp[x][0] += min(dp[c][0], dp[c][1]);
+    }
+}
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    Input();
+    Find(1);
+
+    cout << min(dp[1][0], dp[1][1]);
+
+    return 0;
+}
