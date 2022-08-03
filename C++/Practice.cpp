6ce6554cc6b58de98c8be6fa72d5b2e193fee64e
saens <songsh5775@gmail.com>
2022-08-03 09:54:22 +0900

    [C++] 14725 개미굴 - Trie, map

--- C++/Practice.cpp
+++ C++/Practice.cpp
@@ -1,69 +1,59 @@
 #include <iostream>
-#include <cmath>
+#include <map>
+#include <vector>
 #define FOR(i,e)for(int i=0;i<e;++i)
-#define MAX 1001
 using namespace std;
 
-struct V2
+struct Node
 {
-    int x, y;
-
-    V2 operator-(V2 v)
-    {
-        V2 newV;
-        newV.x = x - v.x;
-        newV.y = y - v.y;
-        return newV;
-    }
-
-    short operator>>(V2 v) // 외적. +시계 -반시계
-    {
-        int c = (x * v.y) - (y * v.x);
-        if (c < 0) return -1;
-        if (c > 0) return 1;
-        else return 0;
-    }
+    int level = 0;
+    map<string, Node> children;
 };
+
+Node start;
+
+void Print(Node& node, string lastString)
+{
+    map<string, Node>::iterator itr;
+    if(lastString.length() > 0)
+    {
+        FOR(i, node.level) cout << "--";
+        cout << lastString << '\n';
+    }
+    for(itr = node.children.begin() ; itr != node.children.end() ; itr++)
+    {
+        Print(itr->second, itr->first);
+    }
+}
 
 int main()
 {
     ios::sync_with_stdio(0);
     cin.tie(0);
     
-    V2 p[3];
-    FOR(i,3) cin >> p[i].x >> p[i].y;
+    int N, K;
+    cin >> N;
 
-    short ccw = (p[1]-p[0]) >> (p[2]-p[0]);
-
-    double area = 0;
-    FOR(i, 3) area += p[i].x * (p[(i+1)%3].y - p[(i+2)%3].y);
-    area /= 2;
-
-    int N, cnt=0; cin >> N;
-    
     FOR(i, N)
     {
-        V2 t;
-        cin >> t.x >> t.y;
+        Node* now = &start;
 
-        bool yes = 1;
+        cin >> K;
 
-        FOR(j, 3)
+        FOR(j, K)
         {
-            V2 a=p[j], b=p[(j+1)%3];
-            V2 ab = b-a;
-            V2 at = t-a;
-
-            short c = ab >> at;
-
-            if (c != ccw && c != 0) yes = 0;
+            string input; cin >> input;
+            if(now->children.find(input) == now->children.end()) // 자식 중에 없으면 추가하기
+            {
+                Node newNode;
+                newNode.level = j;
+                now->children[input] = newNode;
+            }
+            now = &now->children[input];
         }
-
-        if(yes) cnt++;
     }
 
-    printf("%.1f\n%d", abs(area), cnt);
-
+    Print(start, "");
 
     return 0;
 }
