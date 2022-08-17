4e0777922d1d776b505140fe76a2f2c3e2cb77dc
saens <songsh5775@gmail.com>
2022-08-18 01:27:12 +0900

    [C++] BJ 2268 수들의합7 - Segtree
    1h
    아직도 실수가 너무 잦다.
    1. 비교연산자
    2. Modify시 adding value
    3. 구간 쿼리시 swap 했는지?

--- C++/Practice.cpp
+++ C++/Practice.cpp
@@ -3,136 +3,51 @@
 #include <cmath>
 #define FOR(i, e) for(int i=0; i<e ; ++i)
 #define ll long long int
+#define MAX 2000000
 using namespace std;
 
-struct Point
+ll N, M, numbers[MAX], segtree[4*MAX];
+
+ll SetSegtree(int index, int nowL, int nowR)
 {
-    int x, y;
-    Point() {}
-    Point(int _x, int _y)
+    if(nowL == nowR)
     {
-        x = _x;
-        y = _y;
+        segtree[index] = numbers[nowL];
+        return numbers[nowL];
     }
-    bool operator==(Point& p)
-    {
-        return x == p.x && y == p.y;
-    }
-};
 
-struct Area
+    int middle = (nowL + nowR) / 2;
+    int nextIndexR = 2 * (index + 1);
+    int nextIndexL = nextIndexR - 1;
+    segtree[index] = SetSegtree(nextIndexL, nowL, middle) + SetSegtree(nextIndexR, middle + 1, nowR);
+    return segtree[index];
+}
+
+ll Sum(int index, int nowL, int nowR, int targetL, int targetR)
 {
-    Point startPoint, endPoint;
-    Area(Point sp, Point ep)
-    {
-        startPoint = sp;
-        endPoint = ep;
-    }
-    bool operator >> (Area& p) // left(this) Contains right(p)
-    {
-        return  startPoint.x <= p.startPoint.x && p.endPoint.x <= endPoint.x
-            &&  startPoint.y <= p.startPoint.y && p.endPoint.y <= endPoint.y;
-    }
-    bool operator >> (Point& p) // area contains point
-    {
-        return  startPoint.x <= p.x && p.x <= endPoint.x
-            &&  startPoint.y <= p.y && p.y <= endPoint.y;
-    }
+    if(targetR < nowL || nowR < targetL) return 0;
+    if(targetL <= nowL && nowR <= targetR) return segtree[index];
 
-    bool operator &= (Area& p) // left(this) and right(p) shares nothing
-    {
-        return  startPoint.x > p.endPoint.x || endPoint.x < p.startPoint.x
-            ||  startPoint.y > p.endPoint.y || endPoint.y < p.startPoint.y;
-    }
-};
-
-ll N, M, numbers[1024][1024], segtree[1024 * 1024 * 8];
-
-ll SetSegtree(int index, Area nowArea)
-{
-
-    int
-    x1 = nowArea.startPoint.x,
-    y1 = nowArea.startPoint.y,
-    x2 = nowArea.endPoint.x,
-    y2 = nowArea.endPoint.y,
-    lenX = x2 - x1,
-    lenY = y2 - y1;
-
-    if(nowArea.startPoint == nowArea.endPoint)
-    {
-        segtree[index] = numbers[y1][x1];
-        return numbers[y1][x1];
-    }
-
-    int mx = (x1 + x2) / 2;
-    int my = (y1 + y2) / 2;
-    ll sum = 0;
-    int nextIndex[4];
-    FOR(i, 4) nextIndex[i] = index * 4 + (i + 1);
-    sum += SetSegtree(nextIndex[0], Area(Point(x1, y1), Point(mx, my)));
-    if(lenX > 0) sum += SetSegtree(nextIndex[1], Area(Point(mx+1, y1), Point(x2, my)));
-    if(lenY > 0) sum += SetSegtree(nextIndex[2], Area(Point(x1, my+1), Point(mx, y2)));
-    if(lenX > 0 && lenY > 0) sum += SetSegtree(nextIndex[3], Area(Point(mx+1, my+1), Point(x2, y2)));
-
-    segtree[index] = sum;
-
+    int middle = (nowL + nowR) / 2;
+    int nextIndexR = 2*(index + 1);
+    int nextIndexL = nextIndexR - 1;
+    ll sum = Sum(nextIndexL, nowL, middle, targetL, targetR) + Sum(nextIndexR, middle + 1, nowR, targetL, targetR);
     return sum;
 }
 
-ll FindSum(int index, Area nowArea, Area targetArea)
+void Modify(int index, int nowL, int nowR, int targetIndex, ll diff)
 {
-    if(targetArea &= nowArea) return 0;
-    if(targetArea >> nowArea) return segtree[index];
+    if(nowL > targetIndex || nowR < targetIndex) return;
 
-    int 
-    x1 = nowArea.startPoint.x,
-    y1 = nowArea.startPoint.y,
-    x2 = nowArea.endPoint.x,
-    y2 = nowArea.endPoint.y,
-    lenX = x2 - x1,
-    lenY = y2 - y1;
+    segtree[index] += diff;
 
-    int mx = (x1 + x2) / 2;
-    int my = (y1 + y2) / 2;
-    ll sum = 0;
-    int nextIndex[4];
-    FOR(i, 4) nextIndex[i] = index * 4 + (i + 1);
-    sum += FindSum(nextIndex[0], Area(Point(x1, y1), Point(mx, my)), targetArea);
-    if(lenX > 0) sum += FindSum(nextIndex[1], Area(Point(mx+1, y1), Point(x2, my)), targetArea);
-    if(lenY > 0) sum += FindSum(nextIndex[2], Area(Point(x1, my+1), Point(mx, y2)), targetArea);
-    if(lenX > 0 && lenY > 0) sum += FindSum(nextIndex[3], Area(Point(mx+1, my+1), Point(x2, y2)), targetArea);
+    if(nowL == nowR) return;
 
-    return sum;
-}
-
-void Modify(int index, Area nowArea, Point targetPoint, ll addingNumber)
-{
-    if(!(nowArea >> targetPoint)) return;
-
-    int
-    x1 = nowArea.startPoint.x,
-    y1 = nowArea.startPoint.y,
-    x2 = nowArea.endPoint.x,
-    y2 = nowArea.endPoint.y,
-    lenX = x2 - x1,
-    lenY = y2 - y1;
-
-    if(nowArea.startPoint == nowArea.endPoint)
-    {
-        segtree[index] += addingNumber;
-        return;
-    }
-
-    int mx = (x1 + x2) / 2;
-    int my = (y1 + y2) / 2;
-    int nextIndex[4];
-    FOR(i, 4) nextIndex[i] = index * 4 + (i + 1);
-    Modify(nextIndex[0], Area(Point(x1, y1), Point(mx, my)), targetPoint, addingNumber);
-    if(lenX > 0) Modify(nextIndex[1], Area(Point(mx+1, y1), Point(x2, my)), targetPoint, addingNumber);
-    if(lenY > 0) Modify(nextIndex[2], Area(Point(x1, my+1), Point(mx, y2)), targetPoint, addingNumber);
-    if(lenX > 0 && lenY > 0) Modify(nextIndex[3], Area(Point(mx+1, my+1), Point(x2, y2)), targetPoint, addingNumber);
-    segtree[index] += addingNumber;
+    int middle = (nowL + nowR) / 2;
+    int nextIndexR = 2 * (index + 1);
+    int nextIndexL = nextIndexR - 1;
+    if(targetIndex <= middle) Modify(nextIndexL, nowL, middle, targetIndex, diff);
+    else Modify(nextIndexR, middle + 1, nowR, targetIndex, diff);
 }
 
 int main()
@@ -141,33 +56,33 @@ int main()
     cin.tie(0);
 
     cin >> N >> M;
+
     vector<ll> answers;
 
-    FOR(y, N) FOR(x, N) cin >> numbers[y][x];
+    FOR(i, N) numbers[i] = 0;
+    SetSegtree(0, 0, N-1);
 
-    SetSegtree(0, Area(Point(0, 0), Point(N-1, N-1)));
-
-    FOR(m, M)
+    FOR(i, M)
     {
-        int w; cin >> w;
-        if(w == 1)
+        int op; cin >> op;
+        ll a, b; cin >> a >> b;
+        if(op == 0)
         {
-            int x1, y1, x2, y2; cin >> x1 >> y1 >> x2 >> y2; x1--; y1--; x2--; y2--;
-            if(x1 > x2) swap(x1, x2);
-            if(y1 > y2) swap(y1, y2);
-            answers.push_back(FindSum(0, Area(Point(0, 0), Point(N-1, N-1)), Area(Point(x1, y1), Point(x2, y2))));
+            a--; b--;
+            if(a > b) swap(a, b);
+            answers.push_back(Sum(0, 0, N-1, a, b));
         }
-        else
+        else if(op == 1)
         {
-            ll x, y, c; cin >> x >> y >> c; x--; y--;
-            ll adding = c - numbers[y][x];
-            numbers[y][x] = c;
-            Modify(0, Area(Point(0, 0), Point(N - 1, N - 1)), Point(x, y), adding);
+            a--;
+            ll adding = b - numbers[a];
+            numbers[a] = b;
+            Modify(0, 0, N-1, a, adding);
         }
     }
 
     int l = answers.size();
-    FOR(m, l) cout << answers[m] << '\n';
+    FOR(i, l) cout << answers[i] << '\n';
 
     return 0;
 }
