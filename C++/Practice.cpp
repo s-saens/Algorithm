97120f7cbb4a7e36bd1e4b4f832b39d6c1d2a4f8
saens <songsh5775@gmail.com>
2022-08-17 19:42:51 +0900

    [C++] BJ 11658 구간합구하기3 - QuadTree, 틀린풀이
    시간초과조차 안떴다. 이유가 뭘까 도대체...

--- C++/Practice.cpp
+++ C++/Practice.cpp
@@ -1,48 +1,173 @@
 #include <iostream>
+#include <vector>
 #include <cmath>
+#define FOR(i, e) for(int i=0; i<e ; ++i)
+#define ll long long int
 using namespace std;
+
+struct Point
+{
+    int x, y;
+    Point() {}
+    Point(int _x, int _y)
+    {
+        x = _x;
+        y = _y;
+    }
+    bool operator==(Point& p)
+    {
+        return x == p.x && y == p.y;
+    }
+};
+
+struct Area
+{
+    Point startPoint, endPoint;
+    Area(Point sp, Point ep)
+    {
+        startPoint = sp;
+        endPoint = ep;
+    }
+    bool operator >> (Area& p) // left(this) Contains right(p)
+    {
+        return  startPoint.x <= p.startPoint.x && p.endPoint.x <= endPoint.x
+            &&  startPoint.y <= p.startPoint.y && p.endPoint.y <= endPoint.y;
+    }
+    bool operator >> (Point& p) // area contains point
+    {
+        return  startPoint.x <= p.x && p.x <= endPoint.x
+            &&  startPoint.y <= p.y && p.y <= endPoint.y;
+    }
+
+    bool operator &= (Area& p) // left(this) and right(p) shares nothing
+    {
+        return  startPoint.x > p.endPoint.x || endPoint.x < p.startPoint.x
+            ||  startPoint.y > p.endPoint.y || endPoint.y < p.startPoint.y;
+    }
+};
+
+ll N, M, numbers[1024][1024], segtree[1024 * 1024 * 8];
+
+ll SetSegtree(int index, Area nowArea)
+{
+
+    int
+    x1 = nowArea.startPoint.x,
+    y1 = nowArea.startPoint.y,
+    x2 = nowArea.endPoint.x,
+    y2 = nowArea.endPoint.y,
+    lenX = x2 - x1,
+    lenY = y2 - y1;
+
+    if(nowArea.startPoint == nowArea.endPoint)
+    {
+        segtree[index] = numbers[y1][x1];
+        return numbers[y1][x1];
+    }
+
+    int mx = (x1 + x2) / 2;
+    int my = (y1 + y2) / 2;
+    ll sum = 0;
+    int nextIndex[4];
+    FOR(i, 4) nextIndex[i] = index * 4 + (i + 1);
+    sum += SetSegtree(nextIndex[0], Area(Point(x1, y1), Point(mx, my)));
+    if(lenX > 0) sum += SetSegtree(nextIndex[1], Area(Point(mx+1, y1), Point(x2, my)));
+    if(lenY > 0) sum += SetSegtree(nextIndex[2], Area(Point(x1, my+1), Point(mx, y2)));
+    if(lenX > 0 && lenY > 0) sum += SetSegtree(nextIndex[3], Area(Point(mx+1, my+1), Point(x2, y2)));
+
+    segtree[index] = sum;
+
+    return sum;
+}
+
+ll FindSum(int index, Area nowArea, Area targetArea)
+{
+    if(targetArea &= nowArea) return 0;
+    if(targetArea >> nowArea) return segtree[index];
+
+    int 
+    x1 = nowArea.startPoint.x,
+    y1 = nowArea.startPoint.y,
+    x2 = nowArea.endPoint.x,
+    y2 = nowArea.endPoint.y,
+    lenX = x2 - x1,
+    lenY = y2 - y1;
+
+    int mx = (x1 + x2) / 2;
+    int my = (y1 + y2) / 2;
+    ll sum = 0;
+    int nextIndex[4];
+    FOR(i, 4) nextIndex[i] = index * 4 + (i + 1);
+    sum += FindSum(nextIndex[0], Area(Point(x1, y1), Point(mx, my)), targetArea);
+    if(lenX > 0) sum += FindSum(nextIndex[1], Area(Point(mx+1, y1), Point(x2, my)), targetArea);
+    if(lenY > 0) sum += FindSum(nextIndex[2], Area(Point(x1, my+1), Point(mx, y2)), targetArea);
+    if(lenX > 0 && lenY > 0) sum += FindSum(nextIndex[3], Area(Point(mx+1, my+1), Point(x2, y2)), targetArea);
+
+    return sum;
+}
+
+void Modify(int index, Area nowArea, Point targetPoint, ll addingNumber)
+{
+    if(!(nowArea >> targetPoint)) return;
+
+    int
+    x1 = nowArea.startPoint.x,
+    y1 = nowArea.startPoint.y,
+    x2 = nowArea.endPoint.x,
+    y2 = nowArea.endPoint.y,
+    lenX = x2 - x1,
+    lenY = y2 - y1;
+
+    if(nowArea.startPoint == nowArea.endPoint)
+    {
+        segtree[index] += addingNumber;
+        return;
+    }
+
+    int mx = (x1 + x2) / 2;
+    int my = (y1 + y2) / 2;
+    int nextIndex[4];
+    FOR(i, 4) nextIndex[i] = index * 4 + (i + 1);
+    Modify(nextIndex[0], Area(Point(x1, y1), Point(mx, my)), targetPoint, addingNumber);
+    if(lenX > 0) Modify(nextIndex[1], Area(Point(mx+1, y1), Point(x2, my)), targetPoint, addingNumber);
+    if(lenY > 0) Modify(nextIndex[2], Area(Point(x1, my+1), Point(mx, y2)), targetPoint, addingNumber);
+    if(lenX > 0 && lenY > 0) Modify(nextIndex[3], Area(Point(mx+1, my+1), Point(x2, y2)), targetPoint, addingNumber);
+    segtree[index] += addingNumber;
+}
 
 int main()
 {
     ios::sync_with_stdio(0);
     cin.tie(0);
 
-    int N; cin >> N;
+    cin >> N >> M;
+    vector<ll> answers;
 
-    int multiplier = 9, d = 1;
-    while(N-multiplier > 0)
+    FOR(y, N) FOR(x, N) cin >> numbers[y][x];
+
+    SetSegtree(0, Area(Point(0, 0), Point(N-1, N-1)));
+
+    FOR(m, M)
     {
-        N -= multiplier;
-        d++;
-        multiplier = (int)pow(10, d-1) * 9 * d;
+        int w; cin >> w;
+        if(w == 1)
+        {
+            int x1, y1, x2, y2; cin >> x1 >> y1 >> x2 >> y2; x1--; y1--; x2--; y2--;
+            if(x1 > x2) swap(x1, x2);
+            if(y1 > y2) swap(y1, y2);
+            answers.push_back(FindSum(0, Area(Point(0, 0), Point(N-1, N-1)), Area(Point(x1, y1), Point(x2, y2))));
+        }
+        else
+        {
+            ll x, y, c; cin >> x >> y >> c; x--; y--;
+            ll adding = c - numbers[y][x];
+            numbers[y][x] = c;
+            Modify(0, Area(Point(0, 0), Point(N - 1, N - 1)), Point(x, y), adding);
+        }
     }
 
-    N--;
-    int l = (int)pow(10, d-1);
-    int k = l + N/d;
-
-    string number = to_string(k);
-    int index = N%d;
-
-    cout << number[index];
+    int l = answers.size();
+    FOR(m, l) cout << answers[m] << '\n';
 
     return 0;
 }
-
-/*
-
-한자리수 : 1부터 9까지 : 9개 * 1
-두자리수 : 10부터 99까지 : 90개 * 2
-세자리수 : 100부터 999까지 : 900개 * 3
-d자리수 : 10^(d-1)부터 10^d까지 : 9 * 10^(d-1)개 * d
-  -> multiplier = 9 * 10^(n-1) * d
-
-N이 0에 가장 가까워질때까지 multiplier만큼 값을 빼면서 d값, 즉 몇자리 수인지 알아낸다.
-나중N은, 처음N보다 작은, 처음N에서 가장 가까운 경곗값(l = 10^(d))으로부터 몇 번째의 인덱스를 더 가야하는지를 나타내는 값이다.
-단, 이때 1번째가 아니라 0번째부터 세어야 하므로 N에 1을 빼준다. (10을 생각해보면, 나중N이 1인데, 이 때 더 가야하는 인덱스 수는 0개이다.)
-d자리 수에 대해서, l부터 시작해서 인덱스가 d번 증가할 때마다 수는 1씩 증가하므로 나중N을 d로 나눈 값은, l에서 인덱스에 도착했을 때의 전체 수(k)까지 몇이 증가했는지를 의미한다.
-즉, k = l + N/d이다.
-또한 N%d는 d자리 숫자 k에서 몇번째를 탐색해야 할지를 알려준다. (index)
-따라서 k를 string으로 변환한 후 index로 접근하면 도달한 숫자를 알 수 있다.
-
-*/
