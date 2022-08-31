861d40c0d4e9b5235df14a6600ef9e2c293807af
saens <songsh5775@gmail.com>
2022-08-31 13:31:55 +0900

    [C++] BJ 2437 저울 - Greedy
    어렵다!

--- C++/Practice.cpp
+++ C++/Practice.cpp
@@ -1,26 +1,7 @@
 #include <iostream>
-#include <map>
-#include <set>
-#include <cmath>
 #include <algorithm>
 #define FOR(i,e)for(int i=0;i<e;++i)
 using namespace std;
-
-struct Number
-{
-    char c = 'A';
-    int w = 0;
-    Number() {}
-    Number(char _c, int _w)
-    {
-        c = _c, w = _w;
-    }
-};
-
-bool CompareNumber (const Number& n1, const Number& n2)
-{
-    return n1.w > n2.w;
-}
 
 int main()
 {
@@ -28,32 +9,19 @@ int main()
     cin.tie(0);
 
     int N; cin >> N;
-    Number numbers[26]; // numbers[0] = A
+    int numbers[N];
+    
+    FOR(i, N) cin >> numbers[i];
+    sort(numbers, numbers+N);
+
+    int lastR = 1;
 
     FOR(i, N)
     {
-        string s; cin >> s;
-        int len = s.length();
-        int k = pow(10, len-1);
-        FOR(j, len)
-        {
-            numbers[s[j]-'A'].c = s[j];
-            numbers[s[j]-'A'].w += k;
-            k /= 10;
-        }
+        int nowL = numbers[i];
+        if (nowL > lastR) break;
+        lastR += numbers[i];
     }
-    sort(numbers, numbers + 26, CompareNumber);
-
-    int answer = 0;
-    int mul = 9;
-    FOR(i, 26)
-    {
-        if(numbers[i].w <= 0) continue;
-        answer += numbers[i].w * mul;
-        mul--;
-    }
-
-    cout << answer;
-
+    cout << lastR;
     return 0;
 }
