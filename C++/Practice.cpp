4f886dbd672417d618c9589b2645d6ee3356e90e
saens <songsh5775@gmail.com>
2022-08-31 14:49:11 +0900

    [C++] BJ 13164 행복유치원 - Greedy

--- C++/Practice.cpp
+++ C++/Practice.cpp
@@ -8,20 +8,23 @@ int main()
     ios::sync_with_stdio(0);
     cin.tie(0);
 
-    int N; cin >> N;
-    int numbers[N];
-    
-    FOR(i, N) cin >> numbers[i];
-    sort(numbers, numbers+N);
+    int N, K; cin >> N >> K;
+    int h[N], d[N-1];
 
-    int lastR = 1;
+    int totalCost = 0;
 
-    FOR(i, N)
+    FOR(i, N) cin >> h[i];
+    FOR(i, N-1)
     {
-        int nowL = numbers[i];
-        if (nowL > lastR) break;
-        lastR += numbers[i];
+        d[i] = h[i+1] - h[i];
+        totalCost += d[i];
     }
-    cout << lastR;
+
+    sort(d, d+N-1);
+
+    FOR(i, K-1) totalCost -= d[N-2-i];
+
+    cout << totalCost;
+
     return 0;
 }
