5b7028ec704d8795fc529305f8c76a5be2227dee
saens <songsh5775@gmail.com>
2022-09-07 01:04:06 +0900

    [Python] BJ 1016 제곱ㄴㄴ수 - 시간초과

--- Python/Practice.py
+++ Python/Practice.py
@@ -1,13 +1,21 @@
-n = 1000 - int(input())
-now = 500
-d = 5
-cnt = 0
-while n > 0:
-    if n >= now:
-        n -= now
-        cnt += 1
-    else:
-        now /= d
-        d = 2 if d == 5 else 5
+min, max = input().split()
+min = int(min)
+max = int(max)
+
+cnt = max - min + 1
+
+visited = []
+for i in range(max+1): visited.append(False)
+
+for i in range(1, max):
+    if visited[i] == False:
+        j = 2
+        while i*j*j < max+1:
+            m = i*j*j
+            if visited[m] == False and min <= m and m <= max:
+                cnt -= 1
+
+            visited[m] = True
+            j += 1
 
 print(cnt)
