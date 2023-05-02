b4d18a9a4cab7cb0f86877d0c33c09e65c527905
saens <songsh5775@gmail.com>
2023-05-02 14:13:31 +0900

    [C++] BJ 2504 괄호의값 - Stack, DP

--- C++/Practice.cpp
+++ C++/Practice.cpp
@@ -1,36 +1,104 @@
 #include <iostream>
 #include <algorithm>
-#include <queue>
+#include <vector>
+#include <stack>
 
 #define FOR(i,s,e) for(int i=s ; i<e ; ++i)
+#define ll long long
 using namespace std;
 
-int Y, X;
-int** blocks;
-queue<int> q; // BFS 큐 - 1차원 위치 저장
+string input;
+int len;
+bool bad = false;
 
-int dx[4] = {0, 0, 1, -1}, dy[4] = {-1, 1, 0, 0};
-
-int bfs()
+ll f(int l, int r)
 {
-    int vcnt = 0;
+    if (l > r)
+        return 1;
 
-    while(!q.empty())
+    vector<int> starts;
+    vector<int> ends;
+    int cnt = 0;
+
+    FOR(i, l, r + 1)
     {
-        vcnt++;
-        int pos1d = q.front(); q.pop();
-        int x = pos1d % X; int y = pos1d / X;
-
-        FOR(i, 0, 4)
+        switch (input[i])
         {
-            int nx = x + dx[i]; int ny = y + dy[i];
-            if(nx < 0 || nx >= X || ny < 0 || ny >= Y || blocks[ny][nx] != 0) continue;
-            q.push(nx + X*ny);
-            blocks[ny][nx] = -1; // 방문 표시
+        case '(':
+        case '[':
+            if (cnt == 0)
+                starts.push_back(i);
+            cnt++;
+            break;
+        case ')':
+        case ']':
+            cnt--;
+            if (cnt == 0)
+                ends.push_back(i);
+            break;
         }
     }
 
-    return vcnt;
+    ll ret = 0;
+    int slen = starts.size();
+
+    if (ends.size() != slen)
+        return 0;
+
+    FOR(i, 0, slen)
+    {
+        int s = starts[i], e = ends[i];
+        ll k = f(s + 1, e - 1);
+
+        if (k == 0)
+            return 0;
+
+        if (input[s] == '(')
+            ret += k * 2;
+        else if (input[s] == '[')
+            ret += k * 3;
+        else
+            return 0;
+    }
+
+    return ret;
+}
+
+bool check()
+{
+    if (len % 2 == 1)
+        return false;
+
+    stack<char> s;
+
+    FOR(i, 0, len)
+    {
+        char c = input[i];
+        switch (c)
+        {
+        case '(':
+            s.push(c);
+            break;
+        case '[':
+            s.push(c);
+            break;
+        case ')':
+            if (s.empty() || s.top() != '(')
+                return false;
+            s.pop();
+            break;
+        case ']':
+            if (s.empty() || s.top() != '[')
+                return false;
+            s.pop();
+            break;
+        }
+    }
+
+    if (!s.empty())
+        return false;
+
+    return true;
 }
 
 int main()
@@ -38,61 +106,13 @@ int main()
     ios::sync_with_stdio(false);
     cin.tie(NULL);
 
-    cin >> Y >> X;
-    int size1d = X * Y;
+    cin >> input;
+    len = input.length();
 
-    blocks = new int *[Y];
-    FOR(y, 0, Y) blocks[y] = new int[X];
-
-    int wallCnt = 3; // 세워야 할 벽 세 개를 미리 포함시키기
-    vector<int> viruses; // 바이러스 위치 1차원으로 저장
-
-    FOR(y, 0, Y) FOR(x, 0, X)
-    {
-        cin >> blocks[y][x];
-        switch (blocks[y][x])
-        {
-            case 1: wallCnt++; break; // 벽
-            case 2: viruses.push_back(x + X*y); break; // 바이러스
-        }
-    }
-
-    int virusCnt = viruses.size();
-
-    int minimumViruses = 65;
-
-    // 벽 위치 조합. i, j, k는 1차원 좌표.
-    FOR(i, 0, size1d)
-    {
-        int x1 = i % X; int y1 = i / X; // i를 2차원 좌표로 변환
-        if(blocks[y1][x1] != 0) continue; // 빈공간 아니면 패스
-        blocks[y1][x1] = 1; // 벽 1 세우기
-
-        FOR(j, i+1, size1d)
-        {
-            int x2 = j % X; int y2 = j / X; // j를 2차원 좌표로 변환
-            if(blocks[y2][x2] != 0) continue; // 빈공간 아니면 패스
-            blocks[y2][x2] = 1; // 벽 2 세우기
-
-            FOR(k, j+1, size1d)
-            {
-                int x3 = k % X; int y3 = k / X; // k를 2차원 좌표로 변환
-                if(blocks[y3][x3] != 0) continue; // 빈공간 아니면 패스
-                blocks[y3][x3] = 1; // 벽 3 세우기
-
-                    // BFS
-                    FOR(p, 0, virusCnt) q.push(viruses[p]); // 바이러스를 큐에 추가
-                    minimumViruses = min(minimumViruses, bfs()); // BFS 수행
-                    FOR(y, 0, Y) FOR(x, 0, X) if(blocks[y][x] < 0) blocks[y][x] = 0; // 방문표시 없애기
-
-                blocks[y3][x3] = 0; // 벽 3 없애기
-            }
-            blocks[y2][x2] = 0; // 벽 2 없애기
-        }
-        blocks[y1][x1] = 0; // 벽 1 없애기
-    }
-
-    cout << size1d - minimumViruses - wallCnt;
+    if (check())
+        cout << f(0, len - 1);
+    else
+        cout << 0;
 
     return 0;
 }
--- C++/tempCodeRunnerFile.cpp
+++ C++/tempCodeRunnerFile.cpp
@@ -1,59 +1,117 @@
 #include <iostream>
-#include <cmath>
 #include <algorithm>
+#include <vector>
+#include <stack>
+
 #define FOR(i,s,e) for(int i=s ; i<e ; ++i)
 #define ll long long
 using namespace std;
+
+string input;
+int len;
+bool bad = false;
+
+ll f(int l, int r)
+{
+    if (l >= r)
+        return 1;
+
+    vector<int> starts;
+    vector<int> ends;
+    int cnt = 0;
+
+    FOR(i, l, r + 1)
+    {
+        switch (input[i])
+        {
+        case '(':
+        case '[':
+            if (cnt == 0)
+                starts.push_back(i);
+            cnt++;
+            break;
+        case ')':
+        case ']':
+            cnt--;
+            if (cnt == 0)
+                ends.push_back(i);
+            break;
+        }
+    }
+
+    ll ret = 0;
+    int slen = starts.size();
+
+    if (ends.size() != slen)
+        return -1;
+
+    FOR(i, 0, slen)
+    {
+        int s = starts[i], e = ends[i];
+        ll k = f(s + 1, e - 1);
+
+        if (k < 0)
+            return -1;
+
+        if (input[s] == '(')
+            ret += k * 2;
+        else if (input[s] == '[')
+            ret += k * 3;
+        else
+            return -1;
+    }
+
+    return ret;
+}
+
+bool check()
+{
+    int cnt2 = 0, cnt3 = 0;
+    stack<char> s;
+    FOR(i, 0, len)
+    {
+        char c = input[i];
+        switch (c)
+        {
+        case '(':
+        case '[':
+            s.push(c);
+            break;
+        case ')':
+            if (s.empty() || s.top() != '(')
+                return false;
+            s.pop();
+            break;
+        case ']':
+            if (s.empty() || s.top() != '[')
+                return false;
+            s.pop();
+            break;
+        }
+    }
+
+    return true;
+}
 
 int main()
 {
     ios::sync_with_stdio(false);
     cin.tie(NULL);
-    
-    int N;
-    cin >> N;
-    ll numbers[N];
-    int zeroCnt = 0;
 
-    FOR(i, 0, N)
+    cin >> input;
+    len = input.length();
+
+    if (!check())
     {
-        cin >> numbers[i];
-        if(numbers[i] == 0) zeroCnt++;
-    }
-    sort(numbers, numbers+N);
-
-    int j = 0;
-    int borderIndex = 0;
-
-    while(j < N-1 && numbers[j] < 0 && numbers[j+1] < 0)
-    {
-        numbers[j+1] *= numbers[j];
-        numbers[j] = 0;
-        borderIndex = j+1;
-        j+=2;
+        cout << 0;
+        return 0;
     }
 
-    j = N-1;
-    while(j > borderIndex+1 && numbers[j] > 1 && numbers[j-1] > 1)
-    {
-        numbers[j-1] *= numbers[j];
-        numbers[j] = 0;
-        j-=2;
-    }
-
-    ll sum = 0;
-    FOR(i, 0, N)
-    {
-        // 음수끼리 못 곱해지고 남았는데 0도 있는 경우, 0과 곱하기.
-        if(numbers[i] < 0 && zeroCnt > 0)
-        {
-            numbers[i] = 0;
-            zeroCnt--;
-        }
-        sum += numbers[i];
-    }
-
-    cout << sum;
+    int answer = f(0, len - 1);
+    if (answer > 0)
+        cout << answer;
+    else
+        cout << 0;
 
     return 0;
 }
