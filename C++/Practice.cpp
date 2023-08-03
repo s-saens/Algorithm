e2a3abfd9f013ca843b7b3ce64d97b2f06287e5e
saens <songsh5775@gmail.com>
2023-08-03 11:28:00 +0900

    infix to suffix

--- C++/Practice.cpp
+++ C++/Practice.cpp
@@ -1,104 +1,56 @@
 #include <iostream>
 #include <algorithm>
-#include <vector>
-#include <stack>
 
-#define FOR(i,s,e) for(int i=s ; i<e ; ++i)
-#define ll long long
 using namespace std;
 
-string input;
-int len;
-bool bad = false;
-
-ll f(int l, int r)
+string to_suffix(string e)
 {
-    if (l > r)
-        return 1;
-
-    vector<int> starts;
-    vector<int> ends;
-    int cnt = 0;
-
-    FOR(i, l, r + 1)
+    string result = "";
+    string op = "";
+    for(int i=0 ; i<e.length() ; i++)
     {
-        switch (input[i])
+        switch (e[i])
         {
-        case '(':
-        case '[':
-            if (cnt == 0)
-                starts.push_back(i);
-            cnt++;
-            break;
-        case ')':
-        case ']':
-            cnt--;
-            if (cnt == 0)
-                ends.push_back(i);
-            break;
+            case '(':
+                op += e[i];
+                break;
+            case ')':
+                while(op.back() != '(')
+                {
+                    result += op.back();
+                    op.pop_back();
+                }
+                op.pop_back();
+                break;
+            case '*':
+            case '/':
+                while(op.length() > 0 && op.back() == '*' || op.back() == '/')
+                {
+                    result += op.back();
+                    op.pop_back();
+                }
+                op += e[i];
+                break;
+            case '+':
+            case '-':
+                while(op.length() > 0 && op.back() != '(')
+                {
+                    result += op.back();
+                    op.pop_back();
+                }
+                op += e[i];
+                break;
+            default:
+                result += e[i];
+                break;
         }
     }
-
-    ll ret = 0;
-    int slen = starts.size();
-
-    if (ends.size() != slen)
-        return 0;
-
-    FOR(i, 0, slen)
+    while(op.length() > 0)
     {
-        int s = starts[i], e = ends[i];
-        ll k = f(s + 1, e - 1);
-
-        if (k == 0)
-            return 0;
-
-        if (input[s] == '(')
-            ret += k * 2;
-        else if (input[s] == '[')
-            ret += k * 3;
-        else
-            return 0;
+        result += op.back();
+        op.pop_back();
     }
-
-    return ret;
-}
-
-bool check()
-{
-    if (len % 2 == 1)
-        return false;
-
-    stack<char> s;
-
-    FOR(i, 0, len)
-    {
-        char c = input[i];
-        switch (c)
-        {
-        case '(':
-            s.push(c);
-            break;
-        case '[':
-            s.push(c);
-            break;
-        case ')':
-            if (s.empty() || s.top() != '(')
-                return false;
-            s.pop();
-            break;
-        case ']':
-            if (s.empty() || s.top() != '[')
-                return false;
-            s.pop();
-            break;
-        }
-    }
-
-    if (!s.empty())
-        return false;
-
-    return true;
+    return result;
 }
 
 int main()
@@ -106,13 +58,9 @@ int main()
     ios::sync_with_stdio(false);
     cin.tie(NULL);
 
-    cin >> input;
-    len = input.length();
+    string e; cin >> e;
 
-    if (check())
-        cout << f(0, len - 1);
-    else
-        cout << 0;
+    cout << to_suffix(e);
 
     return 0;
 }
