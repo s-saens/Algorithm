3d9c203a9037c60e7d07e1341c1f672f0a8eedfc
saens <songsh5775@gmail.com>
2023-10-08 19:15:58 +0900

    [C++] BJ 1967 트리의지름 - BFS

--- C++/Practice.cpp
+++ C++/Practice.cpp
@@ -1,43 +1,54 @@
 #include <iostream>
 #include <algorithm>
+#include <vector>
 #define FOR(i, s, e) for(int i=s ; i<e; ++i)
 
 using namespace std;
 
+struct Link
+{
+    Link(int t, int l)
+    {
+        to = t;
+        length = l;
+    }
+
+    int to;
+    int length;
+};
+
+struct Node
+{
+    vector<Link> links;
+};
+
+Node* nodes;
+bool* visited;
 int N;
 
-int arr[101][101];
-bool flooded[101][101];
-bool visited[101][101];
+int dfs(int nIndex)
+{
+    int len = 0;
+    visited[nIndex] = true;
 
-int dx[4] = {-1, 1, 0, 0};
-int dy[4] = {0, 0, -1, 1};
+    vector<Link> &links = nodes[nIndex].links;
 
+    FOR(i, 0, links.size())
+    {
+        Link l = links[i];
+        if(!visited[l.to])
+        {
+            len = max(len, dfs(l.to) + l.length);
+            visited[l.to] = true;
+        }
+    }
+
+    return len;
+}
 
 void init()
 {
-    FOR(i, 0, N) FOR(j, 0, N)
-    {
-        flooded[i][j] = false;
-        visited[i][j] = false;
-    }
-}
-
-void dfs(int y, int x)
-{
-    visited[y][x] = true;
-
-    FOR(i, 0, 4)
-    {
-        int ny = y + dy[i];
-        int nx = x + dx[i];
-
-        if(ny < 0 || ny >= N || nx < 0 || nx >= N) continue;
-        if(visited[ny][nx]) continue;
-        if(flooded[ny][nx]) continue;
-
-        dfs(ny, nx);
-    }
+    FOR(i, 0, N) visited[i] = false;
 }
 
 int main()
@@ -46,40 +57,31 @@ int main()
     cin.tie(NULL);
 
     cin >> N;
+    nodes = new Node[N];
+    visited = new bool[N];
 
-    int maxH = 0, minH = 101;
-
-    FOR(y, 0, N) FOR(x, 0, N)
+    FOR(i, 0, N-1)
     {
-        cin >> arr[y][x];
-        int h = arr[y][x];
-        maxH = max(maxH, h);
-        minH = min(minH, h);
+        int s, t, l; cin >> s >> t >> l;
+        nodes[s-1].links.push_back(Link(t-1, l));
+        nodes[t-1].links.push_back(Link(s-1, l));
     }
-
+    
     int answer = 0;
 
-    FOR(h, minH-1, maxH)
+    vector<int> leafNodes;
+
+    FOR(i, 0, N) if(nodes[i].links.size() == 1) leafNodes.push_back(i);
+
+    FOR(i, 0, leafNodes.size())
     {
-        int cnt = 0;
+        int ni = leafNodes[i];
         init();
-
-        // 일단 각각 물에 잠가
-        FOR(y, 0, N) FOR(x, 0, N) if(arr[y][x] <= h) flooded[y][x] = true;
-
-        FOR(y, 0, N) FOR(x, 0, N)
-        {
-            if(visited[y][x]) continue;
-            if(flooded[y][x]) continue;
-
-            dfs(y, x);
-            cnt++;
-        }
-
-        answer = max(answer, cnt);
+        
+        answer = max(answer, dfs(ni));
     }
 
-    cout << answer;
+    cout << answer << endl;
 
     return 0;
 }
