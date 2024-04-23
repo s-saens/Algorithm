28a0b63aeed0ea5ba5e62b6998cb138a2b95b232
saens <songsh5775@gmail.com>
2024-04-23 13:09:36 +0900

    [C++] BJ 20529 가장가까운세사람의심리적거리 - 비둘기집원리

--- C++/Practice.cpp
+++ C++/Practice.cpp
@@ -1,60 +1,65 @@
 #include <iostream>
+#include <algorithm>
 #define FOR(i, s, e) for (int i = s; i < e; ++i)
 using namespace std;
 
-bool k[128][128];
-
-int N, W = 0, B = 0;
-
-void f(int x1, int y1, int w, int h)
+int dist(string a, string b)
 {
-  bool allSame = true;
-  bool first = k[y1][x1];
-  FOR(y, y1, y1 + h)
-  FOR(x, x1, x1 + w)
-  {
-    if (k[y][x] != first)
+    int cnt = 0;
+    FOR(i, 0, 4)
     {
-      allSame = false;
-      break;
+        if (a[i] != b[i])
+            cnt++;
     }
-  }
+    return cnt;
+}
 
-  if (!allSame)
-  {
-    int nw = w / 2, nh = h / 2;
-    f(x1, y1, nw, nh);
-    f(x1 + nw, y1, nw, nh);
-    f(x1 + nw, y1 + nh, nw, nh);
-    f(x1, y1 + nh, nw, nh);
-  }
-  else
-  {
-    if (first) // blue
-    {
-      B++;
-    }
-    else // white
-    {
-      W++;
-    }
-  }
+int calc(string a, string b, string c)
+{
+    int sum = 0;
+    sum += dist(a, b);
+    sum += dist(a, c);
+    sum += dist(c, b);
+    return sum;
 }
 
 int main()
 {
-  cin >> N;
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
 
-  FOR(y, 0, N)
-  FOR(x, 0, N)
-  {
-    cin >> k[y][x];
-  }
+    int T;
+    cin >> T;
+    while (T--)
+    {
+        int N;
+        cin >> N;
+        string mbtis[N];
 
-  f(0, 0, N, N);
+        FOR(i, 0, N)
+        {
+            cin >> mbtis[i];
+        }
 
-  cout << W << '\n'
-       << B;
+        int answer = 999999;
 
-  return 0;
+        if (N > 32)
+        {
+            answer = 0;
+        }
+        else
+        {
+            FOR(i, 0, N - 2)
+            FOR(j, i + 1, N - 1)
+            FOR(k, j + 1, N)
+            {
+                answer = min(answer, calc(mbtis[i], mbtis[j], mbtis[k]));
+            }
+        }
+
+        cout << answer << '\n';
+    }
+
+    return 0;
 }
