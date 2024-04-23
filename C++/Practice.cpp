669b4db1ef79e52190c9af2ee181d2c85656a3e8
saens <songsh5775@gmail.com>
2024-04-23 12:11:34 +0900

    [C++] Programmers 여행경로 - Backtracking

--- C++/Practice.cpp
+++ C++/Practice.cpp
@@ -1,50 +1,83 @@
+#include <string>
+#include <vector>
+#include <unordered_map>
 #include <iostream>
-#include <queue>
-#include <stack>
 #include <algorithm>
+
 using namespace std;
+typedef unordered_map<string, vector<string>> M;
+typedef vector<vector<string>> T;
+
+void init(M &ticketMap, T &tickets, int &cnt)
+{
+  for (auto t : tickets)
+  {
+    string key = t[0], value = t[1];
+    if (ticketMap.find(key) == ticketMap.end())
+    {
+      ticketMap[key] = vector<string>();
+    }
+    ticketMap[key].push_back(value);
+    cnt++;
+  }
+
+  for (auto &t : ticketMap)
+  {
+    sort(t.second.begin(), t.second.end());
+  }
+}
+
+vector<string> answer;
+
+bool dfs(string now, M ticketMap, int cnt)
+{
+  answer.push_back(now);
+  if (cnt <= 0)
+  {
+    return true;
+  }
+  if (ticketMap[now].size() <= 0)
+  {
+    answer.pop_back();
+    return false;
+  }
+
+  bool ret = false;
+
+  for (int i = 0; i < ticketMap[now].size(); ++i)
+  {
+    M newMap = ticketMap;
+    string next = ticketMap[now][i];
+    newMap[now].erase(newMap[now].begin() + i);
+    ret = dfs(next, newMap, cnt - 1);
+    if (ret)
+      break;
+  }
+
+  if (!ret)
+  {
+    answer.pop_back();
+  }
+  return ret;
+}
+
+vector<string> solution(T tickets)
+{
+  M ticketMap;
+  int cnt = 0;
+  init(ticketMap, tickets, cnt);
+
+  dfs("ICN", ticketMap, cnt);
+
+  return answer;
+}
 
 int main()
 {
-    int N; cin >> N;
-    queue<int> q;
-    stack<int> s;
-
-    int M = 0;
-
-    while(N--) { int n; cin >> n; M = max(M, n); q.push(n); }
-
-    int a = 1;
-    while(!q.empty())
-    {
-        int n = q.front();
-
-        if(n == a)
-        {
-            q.pop();
-            a++;
-        }
-        else if(!s.empty() && s.top() == a)
-        {
-            s.pop();
-            a++;
-        }
-        else
-        {
-            q.pop();
-            s.push(n);
-        }
-    }
-
-    while(!s.empty())
-    {
-        int n = s.top();
-        if(n != a) break;
-        s.pop();
-        a++;
-    }
-
-    cout << (s.empty() ? "Nice" : "Sad");
-
-    return 0;
+  vector<vector<string>> v = {{"ICN", "AAA"}, {"ICN", "CCC"}, {"CCC", "DDD"}, {"AAA", "BBB"}, {"AAA", "BBB"}, {"DDD", "ICN"}, {"BBB", "AAA"}};
+  for (auto a : solution(v))
+  {
+    cout << a << ',';
+  }
+  return 0;
 }
