68103cda58b51e71bb98c82637d914c532a5d98a
saens <songsh5775@gmail.com>
2024-09-27 20:03:13 +0900

    [C++] BJ 1485 정사각형 - Geometry

--- C++/Practice.cpp
+++ C++/Practice.cpp
@@ -1,44 +1,49 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <algorithm>
+#define ll long long
+using namespace std;
+
+struct Vector2
+{
+    ll x, y;
+
+    ll operator*(const Vector2 &other)
+    {
+        return this->x * other.x + this->y * other.y;
+    }
+
+    ll sqrLength(const Vector2 &other)
+    {
+        ll dx = this->x - other.x, dy = this->y - other.y;
+        return dx * dx + dy * dy;
+    }
+};
 
 int main()
 {
-    int r, p, c = 0, h = 0, a = 1, s[11], d[11] = {1}, g, i;
-    for (i = 0; i < 11; ++i)
-    {
-        std::cin >> s[i];
-        a *= s[i];
-        if (i)
-            d[i] = s[i - 1] * d[i - 1];
-    }
 
-    int t[a], q[a];
-    for (i = 0; i < a; ++i)
+    int T;
+    cin >> T;
+
+    while (T--)
     {
-        std::cin >> g;
-        t[i] = g;
-        if (g>0) q[h++] = i;
-        if (g < 0) r++;
-    }
-    while (c < h)
-    {
-        g = q[c++];
-        r++;
-        p = t[g] - 1;
-        for (i = 0; i < 22; ++i)
+        Vector2 v[4];
+
+        for (int i = 0; i < 4; ++i)
         {
-            int k = i / 2, m = i % 2 ? -1 : 1,
-                n = g + m * d[k],
-                v = d[k] * s[k],
-                x = g % v, y = n % v;
-
-            if (n >= 0 && n < a && m * y > m * x && !t[n])
-            {
-                q[h++] = n;
-                t[n] = t[g] + 1;
-            }
+            cin >> v[i].x >> v[i].y;
         }
+
+        ll d[6];
+        for (int i = 0; i < 4; ++i)
+            d[i] = v[i].sqrLength(v[(i + 1) % 4]);
+        d[4] = v[0].sqrLength(v[2]);
+        d[5] = v[1].sqrLength(v[3]);
+
+        sort(d, d + 6);
+
+        cout << (d[0] == d[1] && d[1] == d[2] && d[2] == d[3] && d[4] == d[5] ? 1 : 0) << '\n';
     }
-    if (r < a)
-        p = -1;
-    std::cout << p;
+
+    return 0;
 }
