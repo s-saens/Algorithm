ea5b8103b3b2aa09e89afb9cf12f78bc6445a234
saens <songsh5775@gmail.com>
2024-11-19 16:37:46 +0900

    [C++] BJ 1655 가운데를말해요 - PQ*2

--- C++/Practice.cpp
+++ C++/Practice.cpp
@@ -1,49 +1,72 @@
 #include <iostream>
-#include <algorithm>
-#define ll long long
+#include <queue>
+#include <vector>
+#define s1 q1.size()
+#define s2 q2.size()
+#define t1 q1.top()
+#define t2 q2.top()
+
 using namespace std;
 
-struct Vector2
+priority_queue<int> q1; // maxHeap
+priority_queue<int, vector<int>, greater<int>> q2; // minHeap
+
+int push_and_return_mid(int k)
 {
-    ll x, y;
-
-    ll operator*(const Vector2 &other)
+    if(q1.empty())
     {
-        return this->x * other.x + this->y * other.y;
+        q1.push(k);
+        return k;
+    }
+    else if(q2.empty())
+    {
+        if(k < t1)
+        {
+            q2.push(t1);
+            q1.pop(); q1.push(k);
+
+            return k;
+        }
+        q2.push(k);
+        return t1;
     }
 
-    ll sqrLength(const Vector2 &other)
+    if(s1 == s2)
     {
-        ll dx = this->x - other.x, dy = this->y - other.y;
-        return dx * dx + dy * dy;
+        if(k <= t2) q1.push(k);
+        else q2.push(k);
     }
-};
+    else if(s1 < s2)
+    {
+        if(k > t2)
+        {
+            q1.push(t2);
+            q2.pop(); q2.push(k);
+        }
+        else q1.push(k);
+    }
+    else
+    {
+        if(k <= t1)
+        {
+            q2.push(t1);
+            q1.pop(); q1.push(k);
+        }
+        else q2.push(k);
+    }
+
+	return s1 < s2 ? t2 : t1;
+}
 
 int main()
 {
+	int N; cin >> N;
+    vector<int> answers;
+	for(int i=0 ; i<N ; ++i)
+	{
+		int a; cin >> a;
+        answers.push_back(push_and_return_mid(a));
+	}
 
-    int T;
-    cin >> T;
-
-    while (T--)
-    {
-        Vector2 v[4];
-
-        for (int i = 0; i < 4; ++i)
-        {
-            cin >> v[i].x >> v[i].y;
-        }
-
-        ll d[6];
-        for (int i = 0; i < 4; ++i)
-            d[i] = v[i].sqrLength(v[(i + 1) % 4]);
-        d[4] = v[0].sqrLength(v[2]);
-        d[5] = v[1].sqrLength(v[3]);
-
-        sort(d, d + 6);
-
-        cout << (d[0] == d[1] && d[1] == d[2] && d[2] == d[3] && d[4] == d[5] ? 1 : 0) << '\n';
-    }
-
-    return 0;
+    for(int i=0 ; i<N ; ++i) cout << answers[i] << '\n';
 }
