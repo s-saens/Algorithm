b82675a81a596102b0466dcbee129d2d8386a06c
saens <songsh5775@gmail.com>
2025-01-06 22:44:42 +0800

    [C++] BJ 1080 행렬 - Greedy

--- C++/Practice.cpp
+++ C++/Practice.cpp
@@ -1,53 +1,53 @@
 #include <iostream>
 #define FOR(i,s,e) for(int i=s ; i<e; ++i)
-
+#define K 51
 using namespace std;
+
+int Y, X, answer = 0;
+bool A[K][K], B[K][K];
+
+void flip(int x, int y)
+{
+    answer++;
+    FOR(i, 0, 3) FOR(j, 0, 3)
+    {
+        A[y + i][x + j] = !A[y + i][x + j];
+    }
+}
+
+void set(bool a)
+{
+    FOR(y, 0, Y)
+    {
+        string k; cin >> k;
+        FOR(x, 0, X) if(a) A[y][x] = k[x] - '0'; else B[y][x] = k[x] - '0';
+    }
+}
 
 int main()
 {
-    int l = 0, o = 0, v = 0, e = 0;
-    string name; cin >> name;
-    FOR(i, 0, name.size())
+    cin >> Y >> X;
+
+    set(true);
+    set(false);
+
+    if(Y < 3 || X < 3)
     {
-        switch(name[i])
-        {
-            case 'L': l++; break;
-            case 'O': o++; break;
-            case 'V': v++; break;
-            case 'E': e++; break;
-        }
+        FOR(y,0,Y) FOR(x,0,X) if(A[y][x]!=B[y][x]) answer = -1;
+        cout << answer;
+        return 0;
     }
 
-    int maximum = -1;
-    string answer = "";
-
-    int N; cin >> N;
-    FOR(i, 0, N)
+    FOR(y, 0, Y) FOR(x, 0, X)
     {
-        int L=l, O=o, V=v, E=e;
-        string teamName; cin >> teamName;
-
-        FOR(i, 0, teamName.size())
+        if(A[y][x] != B[y][x]) 
         {
-            switch(teamName[i])
+            if (x < X-2 && y < Y-2) flip(x,y);
+            else
             {
-                case 'L': L++; break;
-                case 'O': O++; break;
-                case 'V': V++; break;
-                case 'E': E++; break;
+                cout << -1;
+                return 0;
             }
-        }
-
-        int k = ((L + O) * (L + V) * (L + E) * (O + V) * (O + E) * (V + E)) % 100;
-
-        if (k > maximum)
-        {
-            maximum = k;
-            answer = teamName;
-        }
-        else if (k == maximum && answer > teamName)
-        {
-            answer = teamName;
         }
     }
 
