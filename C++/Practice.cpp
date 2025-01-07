c021b19d820dd52215c6f1c623af7846f3ddff50
saens <songsh5775@gmail.com>
2025-01-07 21:17:27 +0800

    [C++] BJ 11655 ROT13

--- C++/Practice.cpp
+++ C++/Practice.cpp
@@ -4,56 +4,30 @@
 #define FOR(i,s,e) for(int i=s ; i<e; ++i)
 using namespace std;
 
-char sign(ll a)
-{
-    return a == 0 ? '0' : a < 0 ? '-' : '+';
-}
+string s;
 
-bool sameSign(ll a, ll b)
+void change(char &c)
 {
-    return sign(a) == sign(b);
+    if(c >= 'a' && c <= 'z')
+    {
+        c = (c - 'a' + 13) % 26 + 'a';
+    }
+    else if (c >= 'A' && c <= 'Z')
+    {
+        c = (c - 'A' + 13) % 26 + 'A';
+    }
 }
 
 int main()
 {
-    int T = 3;
-    while(T--)
+    getline(cin, s);
+
+    for(auto &c : s)
     {
-        int N; cin >> N;
-        ll num[N];
-        FOR(i, 0, N) cin >> num[i];
-
-        sort(num, num+N);
-
-        int l=0, r=N-1;
-
-        char lastSign = sign(num[l]);
-        bool broken = false;
-
-        while (l < r && !sameSign(num[l], num[r]))
-        {
-            ll sum = num[l] + num[r];
-            if(sum < 0)
-            {
-                num[l] = sum;
-                r--;
-            }
-            else if (sum > 0)
-            {
-                num[r] = sum;
-                l++;
-            }
-            else
-            {
-                r--; l++;
-            }
-
-            lastSign = sign(sum);
-        }
-
-        if(l == r) lastSign = sign(num[l]);
-
-        cout << lastSign << '\n';
+        change(c);
     }
+
+    cout << s;
+
     return 0;
 }
