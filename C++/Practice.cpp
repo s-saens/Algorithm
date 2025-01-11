dfaeb2597a5d6c23572ba0a1b50408772a8b25db
saens <songsh5775@gmail.com>
2025-01-11 09:35:22 +0800

    [C++] BJ 4378 트ㅏㅊ; - string

--- C++/Practice.cpp
+++ C++/Practice.cpp
@@ -1,39 +1,41 @@
 #include <iostream>
-#include <algorithm>
+#include <map>
+#include <vector>
+#include <sstream>
+#define D(a, b) m.insert({a,b});
 #define FOR(i,s,e) for(int i=s ; i<e ; ++i)
 using namespace std;
 
-int *num, *sum;
 
 int main()
 {
-    int N; cin >> N;
-    num = new int[N];
-    sum = new int[N];
+    map<char, char> m;
 
-    FOR(i,0,N)
-    {
-        cin >> num[i];
-        sum[i] = num[i];
-        if (i > 0) sum[i] += sum[i-1];
-    }
-    int l=0, r=N-1, m=0, M=0;
-    
-    while(++m < r)
-    {
-        // S**S**D
-        int a = (sum[m] - num[l] - num[m]) + (sum[r] - sum[m]) * 2;
-        // S**D**S
-        int b = sum[r] - num[l] - num[r] + num[m];
-        // D**S**S
-        int c = (sum[r] - sum[m] - num[r]) + (sum[m] - num[m]) * 2;
+    D('1','`')D('2','1')D('3','2')D('4','3')D('5','4')D('6','5')D('7','6')D('8','7')D('9','8')D('0','9')D('-','0')D('=','-')
+    D('W','Q')D('E','W')D('R','E')D('T','R')D('Y','T')D('U','Y')D('I','U')D('O','I')D('P','O')D('[','P')D(']','[')D('\\',']')
+    D('S','A')D('D','S')D('F','D')D('G','F')D('H','G')D('J','H')D('K','J')D('L','K')D(';','L')D('\'',';')
+    D('X','Z')D('C','X')D('V','C')D('B','V')D('N','B')D('M','N')D(',','M')D('.',',')D('/','.')
+    D(' ', ' ')
 
-        M = max(a, M);
-        M = max(b, M);
-        M = max(c, M);
+    string s;
+    vector<string> answers;
+
+    while(getline(cin, s))
+    {
+        string k;
+        FOR(i,0,s.length())
+        {
+            s[i] = m[s[i]];
+            k += s[i];
+        }
+
+        answers.push_back(k);
     }
 
-    cout << M;
+    FOR(i,0,answers.size())
+    {
+        cout << answers[i] << '\n';
+    }
 
     return 0;
 }
