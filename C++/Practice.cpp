6b1cf9a8a6c2d0f889a3fe75b3ee262434a04c58
saens <songsh5775@gmail.com>
2025-01-12 16:08:34 +0800

    [C++] BJ 11437 LCA - Tree,DFS,LCA

--- C++/Practice.cpp
+++ C++/Practice.cpp
@@ -1,34 +1,72 @@
 #include <iostream>
-#include <unordered_set>
+#include <vector>
 #define FOR(i,s,e) for(int i=s ; i<e ; ++i)
 using namespace std;
 
-// i의 부모노드는 i/2
+int N, M, *p, *d;
+bool *v;
+vector<int> *links;
+
+void input()
+{
+    FOR(i, 1, N)
+    {
+        int a, b; cin >> a >> b;
+        links[a].push_back(b);
+        links[b].push_back(a);
+    }
+
+    FOR(i, 0, N+1) p[i] = -1, d[i] = 0, v[i] = false;
+}
+
+void updateDepth(int i, int depth)
+{
+    v[i] = true;
+    for(int n : links[i])
+    {
+        if(v[n]) continue;
+
+        p[n] = i;
+        d[n] = depth + 1;
+
+        updateDepth(n, depth + 1);
+    }
+}
+
+int lca(int a, int b)
+{
+    while(d[a] != d[b])
+    {
+        if(d[a] > d[b]) a = p[a];
+        else b = p[b];
+    }
+    
+    while(a != b)
+    {
+        a = p[a];
+        b = p[b];
+    }
+
+    return a;
+}
+
 int main()
 {
-    int T; cin >> T;
-    
-    while (T--)
+    ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+    cin >> N;
+    v = new bool[N+1]; p = new int[N+1]; d = new int[N+1], links = new vector<int>[N+1];
+
+    input();
+
+    updateDepth(1, 0);
+
+    cin >> M;
+    while(M--)
     {
-        unordered_set<int> s;
-
         int a, b; cin >> a >> b;
-
-        while(a > 0)
-        {
-            s.insert(a);
-            a /= 2;
-        }
-
-        while (b > 0)
-        {
-            if(s.count(b) > 0)
-            {
-                cout << b * 10 << '\n';
-                break;
-            }
-            b /= 2;
-        }
+        cout << lca(a, b) << '\n';
     }
+    
     return 0;
 }
